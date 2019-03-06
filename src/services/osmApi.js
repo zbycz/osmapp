b9f01d313503742364be1332fe1b3a1c3aa7733f
Pavel Zbytovský <zbytovsky@gmail.com>
2019-03-06 01:54:26 +0100

    osmApi: compute class and subclass from tags

--- src/services/osmApi.js
+++ src/services/osmApi.js
@@ -27,6 +27,251 @@ const getGeometry = {
   }),
 };
 
+const subclassKeys = [
+  'aerialway',
+  'amenity',
+  'barrier',
+  'highway',
+  'historic',
+  'landuse',
+  'leisure',
+  'railway',
+  'shop',
+  'sport',
+  'tourism',
+  'waterway',
+];
+const subclassToClassRules = [
+  {
+    subclass: [
+      'accessories',
+      'antiques',
+      'beauty',
+      'bed',
+      'boutique',
+      'camera',
+      'carpet',
+      'charity',
+      'chemist',
+      'chocolate',
+      'coffee',
+      'computer',
+      'confectionery',
+      'convenience',
+      'copyshop',
+      'cosmetics',
+      'garden_centre',
+      'doityourself',
+      'erotic',
+      'electronics',
+      'fabric',
+      'florist',
+      'frozen_food',
+      'furniture',
+      'video_games',
+      'video',
+      'general',
+      'gift',
+      'hardware',
+      'hearing_aids',
+      'hifi',
+      'ice_cream',
+      'interior_decoration',
+      'jewelry',
+      'kiosk',
+      'lamps',
+      'mall',
+      'massage',
+      'motorcycle',
+      'mobile_phone',
+      'newsagent',
+      'optician',
+      'outdoor',
+      'perfumery',
+      'perfume',
+      'pet',
+      'photo',
+      'second_hand',
+      'shoes',
+      'sports',
+      'stationery',
+      'tailor',
+      'tattoo',
+      'ticket',
+      'tobacco',
+      'toys',
+      'travel_agency',
+      'watches',
+      'weapons',
+      'wholesale',
+    ],
+    resultClass: 'shop',
+  },
+  {
+    subclass: ['townhall', 'public_building', 'courthouse', 'community_centre'],
+    resultClass: 'town_hall',
+  },
+  {
+    subclass: ['golf', 'golf_course', 'miniature_golf'],
+    resultClass: 'golf',
+  },
+  {
+    subclass: ['fast_food', 'food_court'],
+    resultClass: 'fast_food',
+  },
+  {
+    subclass: ['park', 'bbq'],
+    resultClass: 'park',
+  },
+  {
+    subclass: ['bus_stop', 'bus_station'],
+    resultClass: 'bus',
+  },
+  {
+    mappingKey: 'railway',
+    subclass: ['station'],
+    resultClass: 'railway',
+  },
+  {
+    subclass: ['halt', 'tram_stop', 'subway'],
+    resultClass: 'railway',
+  },
+  {
+    mappingKey: 'aerialway',
+    subclass: ['station'],
+    resultClass: 'aerialway',
+  },
+  {
+    subclass: ['subway_entrance', 'train_station_entrance'],
+    resultClass: 'entrance',
+  },
+  {
+    subclass: ['camp_site', 'caravan_site'],
+    resultClass: 'campsite',
+  },
+  {
+    subclass: ['laundry', 'dry_cleaning'],
+    resultClass: 'laundry',
+  },
+  {
+    subclass: [
+      'supermarket',
+      'deli',
+      'delicatessen',
+      'department_store',
+      'greengrocer',
+      'marketplace',
+    ],
+    resultClass: 'grocery',
+  },
+  {
+    subclass: ['books', 'library'],
+    resultClass: 'library',
+  },
+  {
+    subclass: ['university', 'college'],
+    resultClass: 'college',
+  },
+  {
+    subclass: [
+      'hotel',
+      'motel',
+      'bed_and_breakfast',
+      'guest_house',
+      'hostel',
+      'chalet',
+      'alpine_hut',
+      'camp_site',
+    ],
+    resultClass: 'lodging',
+  },
+  {
+    subclass: ['chocolate', 'confectionery'],
+    resultClass: 'ice_cream',
+  },
+  {
+    subclass: ['post_box', 'post_office'],
+    resultClass: 'post',
+  },
+  {
+    subclass: ['cafe'],
+    resultClass: 'cafe',
+  },
+  {
+    subclass: ['school', 'kindergarten'],
+    resultClass: 'school',
+  },
+  {
+    subclass: ['alcohol', 'beverages', 'wine'],
+    resultClass: 'alcohol_shop',
+  },
+  {
+    subclass: ['bar', 'nightclub'],
+    resultClass: 'bar',
+  },
+  {
+    subclass: ['marina', 'dock'],
+    resultClass: 'harbor',
+  },
+  {
+    subclass: ['car', 'car_repair', 'taxi'],
+    resultClass: 'car',
+  },
+  {
+    subclass: ['hospital', 'nursing_home', 'clinic'],
+    resultClass: 'hospital',
+  },
+  {
+    subclass: ['grave_yard', 'cemetery'],
+    resultClass: 'cemetery',
+  },
+  {
+    subclass: ['attraction', 'viewpoint'],
+    resultClass: 'attraction',
+  },
+  {
+    subclass: ['biergarten', 'pub'],
+    resultClass: 'beer',
+  },
+  {
+    subclass: ['music', 'musical_instrument'],
+    resultClass: 'music',
+  },
+  {
+    subclass: ['american_football', 'stadium', 'soccer'],
+    resultClass: 'stadium',
+  },
+  {
+    subclass: ['art', 'artwork', 'gallery', 'arts_centre'],
+    resultClass: 'art_gallery',
+  },
+  {
+    subclass: ['bag', 'clothes'],
+    resultClass: 'clothing_store',
+  },
+  {
+    subclass: ['swimming_area', 'swimming'],
+    resultClass: 'swimming',
+  },
+  {
+    subclass: ['castle', 'ruins'],
+    resultClass: 'castle',
+  },
+];
+
+const getPoiClass = tags => {
+  const mappingKey = subclassKeys.find(x => x in tags);
+  const subclass = tags[mappingKey];
+  const resultRule = subclassToClassRules.find(rule => {
+    const mappingKeyMatchedOrNone =
+      !rule.mappingKey || rule.mappingKey === mappingKey;
+    const subclassMatched = rule.subclass.includes(subclass);
+    return mappingKeyMatchedOrNone && subclassMatched;
+  });
+  const cls = resultRule ? resultRule.resultClass : subclass;
+  return { class: cls, subclass };
+};
+
 export const osmToGeojson = async osmXmlStr => {
   const osmXml = await parseXmlString(osmXmlStr);
 
@@ -48,6 +293,7 @@ export const osmToGeojson = async osmXmlStr => {
     geometry: getGeometry[type](osmXml, item),
     osmMeta,
     tags,
+    properties: getPoiClass(tags),
   };
 };
 
