7199bab869d8122ccc2b3e28bf4af22e1d6fdfa0
Pavel Zbytovský <zbytovsky@gmail.com>
2019-12-13 21:17:52 +0100

    general: fix nextjs + material-ui after upgrade

--- pages/_app.js
+++ pages/_app.js
@@ -1,56 +1,35 @@
-// @flow
-
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import JssProvider from 'react-jss/lib/JssProvider';
-
-import getPageContext from '../src/helpers/getPageContext';
+import theme from '../src/helpers/theme';
 import GlobalStyle from '../src/helpers/GlobalStyle';
 
-class MyApp extends App {
-  constructor() {
-    super();
-    this.pageContext = getPageContext();
-  }
-
+export default class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side'); // eslint-disable-line no-undef
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
     }
   }
 
   render() {
     const { Component, pageProps } = this.props;
+
     return (
-      <Container>
+      <React.Fragment>
         <Head>
           <title>osmcz-v2</title>
         </Head>
-        {/* Wrap every page in Jss and Theme providers */}
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
-          {/* MuiThemeProvider makes the theme available down the React tree thanks to React context. */}
-          <MuiThemeProvider
-            theme={this.pageContext.theme}
-            sheetsManager={this.pageContext.sheetsManager}
-          >
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {/* Pass pageContext to the _document though the renderPage enhancer to render collected styles on server-side. */}
-            <Component pageContext={this.pageContext} {...pageProps} />
-          </MuiThemeProvider>
-        </JssProvider>
-        <GlobalStyle />
-      </Container>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+          <GlobalStyle />
+        </ThemeProvider>
+      </React.Fragment>
     );
   }
 }
-
-export default MyApp;
