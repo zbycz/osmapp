f98efb110ae787101fb79c13ffb61e6fa71c1d67
Pavel Zbytovský <zbytovsky@gmail.com>
2020-02-15 12:11:25 +0100

    FeatureImage: show mapillary after wiki img

--- src/services/images.js
+++ src/services/images.js
@@ -1,8 +1,8 @@
-import { fetchText, removeFetchCache } from './helpers';
+import { fetchText, getShortId, removeFetchCache } from './helpers';
 
-const getMapillaryUrl = async feature => {
-  const lonlat = feature.center.map(x => x.toFixed(5)).join(',');
-  const url = `https://a.mapillary.com/v3/images?client_id=TTdNZ2w5eTF6MEtCNUV3OWNhVER2dzpjMjdiZGE1MWJmYzljMmJi&lookat=${lonlat}&closeto=${lonlat}&per_page=2`;
+const getMapillaryUrl = async center => {
+  const lonlat = center.map(x => x.toFixed(5)).join(',');
+  const url = `https://a.mapillary.com/v3/images?client_id=TTdNZ2w5eTF6MEtCNUV3OWNhVER2dzpjMjdiZGE1MWJmYzljMmJi&lookat=${lonlat}&closeto=${lonlat}`;
   const data = await fetchText(url);
 
   // {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"ca":71.80811,"camera_make":"Apple","camera_model":"iPhone6,2","captured_at":"2015-05-08T06:02:41.227Z","key":"rPU1sldzMCVIMN2XmjDf2A","pano":false,"sequence_key":"-zanzZ2HpdOhkw-uG166Pg","user_key":"M7Mgl9y1z0KB5Ew9caTDvw","username":"zbycz"},"geometry":{"type":"Point","coordinates":[14.390517,50.100268]}}]}
@@ -14,25 +14,37 @@ const getMapillaryUrl = async feature => {
 
   const image = features[0];
   const { key } = image.properties;
+  console.log('mplr', key);
   return `https://images.mapillary.com/${key}/thumb-640.jpg`;
 };
 
-export const getFeatureImageUrl = async feature => {
-  const wikiUrl = getWikiApiUrl(feature.tags);
-  console.log(wikiUrl);
+let mapillary, mapillaryId;
 
+export const getFeatureImage = async feature => {
+  if (feature.nonOsmObject) {
+    return await getMapillaryUrl(feature.center);
+  }
+
+  // first pass is skeleton
+  const osmid = getShortId(feature.osmMeta);
+  if (feature.skeleton) {
+    mapillary = getMapillaryUrl(feature.center);
+    mapillaryId = osmid;
+    return;
+  }
+
+  if (mapillaryId !== osmid) {
+    mapillary = undefined;
+  }
+
+  // second pass is full object, can fetch wikipage by tags
+  const wikiUrl = getWikiApiUrl(feature.tags);
   if (wikiUrl) {
     const image = await getImage(wikiUrl);
-    console.log(image);
     return image.thumb;
   }
 
-  try {
-    return await getMapillaryUrl(feature);
-  } catch (e) {
-    console.log(e);
-    return undefined;
-  }
+  return mapillary ?? (await getMapillaryUrl(feature.center));
 };
 
 // From https://github.com/osmcz/osmcz/blob/0d3eaaa/js/poi-popup.js - MIT
