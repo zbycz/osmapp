38a0b65b7814c089527382a705b79bf1a1527ebc
Pavel Zbytovsky <Pavel.Zbytovsky@ncr.com>
2020-02-16 13:02:32 +0100

    Map: iD edit link

--- src/components/utils/MapStateContext.js
+++ src/components/utils/MapStateContext.js
@@ -6,14 +6,14 @@ export const MapStateContext = React.createContext(undefined);
 
 export const MapStateProvider = ({ children, initialMapState }) => {
   const [view, setView] = React.useState(initialMapState);
-  const [_viewForMap, setViewForMap] = React.useState(initialMapState);
-  const _setView = React.useCallback(view => {
+  const [_viewForMap, _setViewForMap] = React.useState(initialMapState);
+  const setBothViews = React.useCallback(view => {
     setView(view);
-    setViewForMap(view);
+    _setViewForMap(view);
   }, []);
   const mapState = {
-    view, // always up-to-date
-    setView,
+    view, // always up-to-date (for use in react)
+    setView: setBothViews,
     _viewForMap, // updated only when map has to be updated
     _setViewFromMap: setView,
   };
