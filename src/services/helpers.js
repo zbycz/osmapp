d89338467bb6212cf66bfe60f7a2e84e54d6eb42
Pavel Zbytovský <zbytovsky@gmail.com>
2020-02-18 18:34:55 +0100

    index: default location from IP

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -123,3 +123,21 @@ export const getCenter = feature => {
 };
 
 export const prod = process.env.NODE_ENV === 'production';
+
+const defaultView = [4, 50, 14];
+const isLocalhost = ip => ['127.0.0.1', '::1'].includes(ip);
+
+export const getViewFromIP = async ({ req }) => {
+  const rmtIp = req.connection.remoteAddress;
+  const fwdIp = (req.headers['x-forwarded-for'] || '').split(',')[0].trim(); // ngnix: proxy_set_header X-Forwarded-For $remote_addr;
+  const ip = rmtIp && !isLocalhost(rmtIp) ? rmtIp : fwdIp;
+  const url = `http://api.ipstack.com/${ip}?access_key=169a541e2e9936a03b0b9e355dd29ff3&format=1`;
+
+  try {
+    const { latitude: lat, longitude: lon } = await fetchJson(url);
+    return lat && lon ? [7, lat, lon] : defaultView;
+  } catch (e) {
+    console.warn('getViewFromIP', e);
+    return defaultView;
+  }
+};
