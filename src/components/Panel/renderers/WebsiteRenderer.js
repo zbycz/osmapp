a53d8212ffbc4a8c76283972cf6e9e808b9888d3
Pavel Zbytovský <zbytovsky@gmail.com>
2021-03-14 12:35:59 +0100

    Panel: fix WebsiteRenderer regexp

--- src/components/Panel/renderers/WebsiteRenderer.js
+++ src/components/Panel/renderers/WebsiteRenderer.js
@@ -6,13 +6,13 @@ import Language from '@material-ui/icons/Language';
 const protocol = /^\w+:\/\//;
 const fixHttp = url => (url.match(protocol) ? url : `http://${url}`);
 const displayForm = url =>
-  url.replace(protocol, '').replace(/([^\/]+)\/$/, '$1');
+  url.replace(protocol, '').replace(/([^/]+)\/$/, '$1');
 
 export const WebsiteRenderer = ({ v }) => (
-         <>
-           <Language fontSize="small" />
-           <a href={fixHttp(v)}>{displayForm(v)}</a>
-         </>
-       );
+  <>
+    <Language fontSize="small" />
+    <a href={fixHttp(v)}>{displayForm(v)}</a>
+  </>
+);
 
 export default WebsiteRenderer;
