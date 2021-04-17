7e57e4254540d884d59da05056bce749d1cd0718
Pavel Zbytovský <zbytovsky@gmail.com>
2021-04-17 14:44:43 +0200

    EditDialog: insert OSM note

--- src/components/FeaturePanel/EditDialog/EditDialog.tsx
+++ src/components/FeaturePanel/EditDialog/EditDialog.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import useTheme from '@material-ui/core/styles/useTheme';
+import DialogContent from '@material-ui/core/DialogContent';
 import { useToggleState } from '../../helpers';
 import { Feature, FeatureTags } from '../../../services/types';
+import { createNote } from './createNote';
+import { insertOsmNote } from '../../../services/osmApi';
 import { MajorKeysEditor } from './MajorKeysEditor';
-import { OtherTagsEditor } from './OtherTagsEditor';
 import {
   ChangeLocationEditor,
   ContributionInfoBox,
@@ -17,6 +18,8 @@ import {
   OtherOptionsHeading,
   PlaceCancelledToggle,
 } from './components';
+import { OtherTagsEditor } from './OtherTagsEditor';
+import { SuccessContent } from './SuccessContent';
 
 const useIsFullScreen = () => {
   const theme = useTheme();
@@ -31,10 +34,6 @@ const useTagsState = (
   return [tags, setTag];
 };
 
-const createNote = () => {
-  alert('TODO');
-};
-
 interface Props {
   feature: Feature;
   open: boolean;
@@ -43,16 +42,27 @@ interface Props {
 }
 
 export const EditDialog = ({ feature, open, handleClose, focusTag }: Props) => {
-  const { properties } = feature;
   const fullScreen = useIsFullScreen();
   const [cancelled, toggleCancelled] = useToggleState(false);
   const [location, setLocation] = useState('');
   const [note, setNote] = useState('');
   const [tags, setTag] = useTagsState(feature.tags);
+  const [insertedNote, setInsertedNote] = useState<
+    false | { text: string; url: string }
+  >(false);
 
-  const saveDialog = () => {
-    createNote(feature, tags, cancelled, location, note);
-    handleClose();
+  const saveDialog = async () => {
+    const text = createNote(feature, tags, cancelled, location, note);
+    if (text == null) {
+      // eslint-disable-next-line no-alert
+      alert('Proveďte, prosím, požadované změny.');
+      return;
+    }
+
+    setInsertedNote({
+      text,
+      url: await insertOsmNote(feature.center, text),
+    });
   };
 
   return (
@@ -63,25 +73,37 @@ export const EditDialog = ({ feature, open, handleClose, focusTag }: Props) => {
       aria-labelledby="edit-dialog-title"
     >
       <DialogTitle id="edit-dialog-title">
-        Navrhnout úpravu: {tags.name || properties.subclass}
+        Navrhnout úpravu: {feature.tags.name || feature.properties.subclass}
       </DialogTitle>
-      <DialogContent dividers>
-        <MajorKeysEditor tags={tags} setTag={setTag} focusTag={focusTag} />
-        <OtherOptionsHeading />
-        <OtherTagsEditor tags={tags} setTag={setTag} focusTag={focusTag} />
-        <PlaceCancelledToggle cancelled={cancelled} toggle={toggleCancelled} />
-        <ChangeLocationEditor location={location} setLocation={setLocation} />
-        <ContributionInfoBox />
-        <NoteField note={note} setNote={setNote} />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Zrušit
-        </Button>
-        <Button onClick={saveDialog} color="primary" variant="contained">
-          Uložit
-        </Button>
-      </DialogActions>
+      {insertedNote ? (
+        <SuccessContent insertedNote={insertedNote} handleClose={handleClose} />
+      ) : (
+        <>
+          <DialogContent dividers>
+            <MajorKeysEditor tags={tags} setTag={setTag} focusTag={focusTag} />
+            <OtherOptionsHeading />
+            <OtherTagsEditor tags={tags} setTag={setTag} focusTag={focusTag} />
+            <PlaceCancelledToggle
+              cancelled={cancelled}
+              toggle={toggleCancelled}
+            />
+            <ChangeLocationEditor
+              location={location}
+              setLocation={setLocation}
+            />
+            <ContributionInfoBox />
+            <NoteField note={note} setNote={setNote} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Zrušit
+            </Button>
+            <Button onClick={saveDialog} color="primary" variant="contained">
+              Uložit
+            </Button>
+          </DialogActions>
+        </>
+      )}
     </Dialog>
   );
 };
---
+++ src/components/FeaturePanel/EditDialog/createNote.ts
@@ -0,0 +1,65 @@
+import { Feature, FeatureTags } from '../../../services/types';
+import { getUrlOsmId } from '../../../services/helpers';
+
+export const createNote = (
+  feature: Feature,
+  newTags: FeatureTags,
+  placeCancelled: boolean,
+  location: string,
+  note: string,
+) => {
+  const isAdded = ([k, v]) => v && !feature.tags[k];
+  const isRemoved = ([k, v]) => v && !newTags[k];
+  const addedTags = Object.entries(newTags).filter(isAdded);
+  const removedTags = Object.entries(feature.tags).filter(isRemoved);
+  const changedTags = Object.entries(newTags).filter(
+    ([k, v]) => !isAdded([k, v]) && v && v !== feature.tags[k],
+  );
+  const changeOrAddedTags = [...addedTags, ...changedTags];
+
+  if (
+    !changeOrAddedTags.length &&
+    !removedTags.length &&
+    !placeCancelled &&
+    !location &&
+    !note
+  ) {
+    return null;
+  }
+
+  const noteText = [];
+  noteText.push(getUrlOsmId(feature.osmMeta));
+  if (placeCancelled) {
+    noteText.push('! Place was marked permanently closed.');
+  }
+
+  if (note) {
+    noteText.push('');
+    noteText.push(note);
+  }
+
+  if (location) {
+    noteText.push('');
+    noteText.push('Suggested location change:');
+    noteText.push(location);
+  }
+
+  if (changeOrAddedTags.length) {
+    noteText.push('');
+    noteText.push('Suggested changes:');
+    noteText.push(changeOrAddedTags.map(([k, v]) => `${k}=${v}`).join('\n'));
+  }
+
+  if (removedTags.length) {
+    noteText.push('');
+    noteText.push(`Removed tags:`);
+    noteText.push(removedTags.map(([k]) => k).join(', '));
+  }
+
+  noteText.push('\n');
+  noteText.push(
+    `Submitted from https://osmapp.org/${getUrlOsmId(feature.osmMeta)}`,
+  );
+
+  return noteText.join('\n');
+};
