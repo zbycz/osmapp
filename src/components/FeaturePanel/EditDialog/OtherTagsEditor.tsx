9d7caca5902fe696caa00844f872a0367f254951
Pavel Zbytovský <zbytovsky@gmail.com>
2021-04-17 15:18:33 +0200

    EditDialog: option to add new tags

--- src/components/FeaturePanel/EditDialog/OtherTagsEditor.tsx
+++ src/components/FeaturePanel/EditDialog/OtherTagsEditor.tsx
@@ -3,6 +3,7 @@ import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
 import { isString } from '../../helpers';
 import { majorKeys } from './MajorKeysEditor';
 
@@ -17,7 +18,67 @@ const Table = styled.table`
     vertical-align: center;
     padding-left: 0;
   }
+
+  .MuiInputBase-root {
+    font-size: 13px;
+  }
 `;
+
+const NewTagRow = ({ setTag }) => {
+  const [newKey, setNewKey] = useState('');
+  const [newValue, setNewValue] = useState('');
+
+  const handleAdd = () => {
+    if (newKey && newValue) {
+      setTag(newKey, newValue);
+      setNewKey('');
+      setNewValue('');
+    }
+  };
+
+  return (
+    <tr>
+      <th>
+        <TextField
+          value={newKey}
+          onChange={(e) => setNewKey(e.target.value)}
+          variant="outlined"
+          size="small"
+          placeholder="new key"
+        />
+      </th>
+      <td>
+        <TextField
+          value={newValue}
+          onChange={(e) => setNewValue(e.target.value)}
+          variant="outlined"
+          size="small"
+        />{' '}
+        <Button variant="contained" disableElevation onClick={handleAdd}>
+          Add
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+const KeyValueRow = ({ k, v, setTag, focusTag }) => (
+  <tr key={k}>
+    <th>{k}</th>
+    <td>
+      <TextField
+        name={k}
+        value={v}
+        onChange={(e) => setTag(e.target.name, e.target.value)}
+        fullWidth
+        variant="outlined"
+        size="small"
+        autoFocus={focusTag === k}
+        placeholder="will be deleted"
+      />
+    </td>
+  </tr>
+);
 
 export const OtherTagsEditor = ({ tags, setTag, focusTag }) => {
   const focusTagsSection =
@@ -30,6 +91,12 @@ export const OtherTagsEditor = ({ tags, setTag, focusTag }) => {
       setShowTags(true);
     }
   }, [focusTagsSection]);
+
+  const rows = Object.entries(tags)
+    .filter(([k]) => !majorKeys.includes(k))
+    .map(([k, v]) => (
+      <KeyValueRow k={k} v={v} setTag={setTag} focusTag={focusTag} />
+    ));
 
   return (
     <>
@@ -45,24 +112,8 @@ export const OtherTagsEditor = ({ tags, setTag, focusTag }) => {
       {showTags && (
         <Table>
           <tbody>
-            {Object.entries(tags)
-              .filter(([k]) => !majorKeys.includes(k))
-              .map(([k, v]) => (
-                <tr key={k}>
-                  <th>{k}</th>
-                  <td>
-                    <TextField
-                      value={v}
-                      variant="outlined"
-                      size="small"
-                      name={k}
-                      onChange={(e) => setTag(e.target.name, e.target.value)}
-                      fullWidth
-                      autoFocus={focusTag === k}
-                    />
-                  </td>
-                </tr>
-              ))}
+            {rows}
+            <NewTagRow setTag={setTag} />
           </tbody>
         </Table>
       )}
