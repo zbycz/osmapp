3bc31948e7ba249561071c1fdeb8efc4a7f57ef7
arrival-spring <77166354+arrival-spring@users.noreply.github.com>
2021-09-02 22:15:17 +0100

    Create DescriptionRenderer.tsx

---
+++ src/components/FeaturePanel/renderers/DescriptionRenderer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Info from '@material-ui/icons/Info';
+
+const DescriptionRenderer = ({ v }) => (
+  <>
+    <Info fontSize="small" />
+    ${v}
+  </>
+);
+
+export default DescriptionRenderer;
