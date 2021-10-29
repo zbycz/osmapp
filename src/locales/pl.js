9f7e9454f4270dac70b83b92d6a8c571d90c8e13
Piotr Strebski <strebski@gmail.com>
2021-10-29 19:52:11 +0200

    general: add pl language and translations (#77)
    * Create pl.js
    
    Fixed and extended
    
    * add polish lang to config
    
    Co-authored-by: Pavel Zbytovský <zbytovsky@gmail.com>

---
+++ src/locales/pl.js
@@ -0,0 +1,154 @@
+// identifier should be in the form: foldername.messageid.
+
+export default {
+  loading: 'Wczytywanie',
+  error: 'Błąd',
+  close_panel: 'Zamknij panel',
+  webgl_error: `Ups. Ta mapa wymaga technologii WebGL.<br /><br />Jeśli masz zgodne urządzenie, spróbuj użyć ostatniej wersji przeglądarki Chrome.`,
+
+  'install.button': 'Zainstaluj aplikację',
+  'install.tabs_aria_label': 'Wybierz swoją platformę',
+  'install.ios_intro': 'Otwórz osmapp.org w <strong>przeglądarce Safari</strong>',
+  'install.ios_share': 'Dotknij <strong>ikonę Udostępniania</strong>',
+  'install.ios_add': 'Dotknij <strong>Dodaj do ekranu domowego</strong>',
+  'install.android_intro': 'Otwórz osmapp.org w <strong>przeglądarce Chrome</strong>',
+  'install.android_share': 'Dotknij <strong>menu trzech kropek</strong>',
+  'install.android_add': 'Dotknij <strong>Zainstaluj aplikację</strong>',
+  'install.desktop_intro': 'Otwórz osmapp.org w <strong>Chrome</strong>, <strong>FirefoxOS</strong> lub <strong>Opera</strong>',
+  'install.desktop_install': 'Kliknij w <strong>przycisk instalacji</strong>',
+  'install.outro': 'To wszystko! Zobacz OsmAPP na swoim domowym ekranie.',
+  'install.note': 'Uwaga: ta aplikacja korzysta z technologii PWA – zapewnia szybką instalację i nie wymaga Google Play ani App Store.',
+
+  'homepage.subtitle': 'Uniwersalna aplikacja OpenStreetMap',
+  'homepage.how_to_start': 'Zacznij od wpisania zapytania w polu wyszukiwania.\nAlbo też kliknij dowolny obiekt na mapie.',
+  'homepage.examples.eg': 'np.',
+  'homepage.examples.charles_bridge_statues': 'Pomniki na Moście Karola',
+  'homepage.screenshot_alt': 'Zrzut ekranu OsmAPP',
+  'homepage.about_osm': `Wszystkie dane mapy pochodzą z
+      <a href="https://osm.org">OpenStreetMap</a>, mapy tworzonej przez
+      miliony współtwórców — podobnie jak Wikipedia. Możesz odnaleźć
+      przycisk <em>Edycji</em> w każdym elemencie mapy.`,
+  'homepage.heading_about_osmapp': 'O projekcie OsmAPP',
+  'homepage.about_osmapp': `Ta aplikacja powinna oferować wygodny interfejs do codziennego korzystania z <i>OpenStreetMap</i>,
+     włączając w to możliwości edycji. <br/>Obecnie zawiera różne warstwy mapy, edycję POI oraz podstawową wyszukiwarkę.
+     Funkcje takie jak nawigacja czy ulubione miejsca - są na etapie planów.`,
+  'homepage.github_link': `Możesz zasugerować nowe możliwości poprzez <a href="https://github.com/zbycz/osmapp" rel='noopener' target='_blank'>Github</a>.`,
+  'homepage.special_thanks_heading': `Specjalne podziękopwania dla`,
+  'homepage.special_thanks': `<ul>
+        <li><a href="https://www.mapillary.com/" rel='noopener' target='_blank'>Mapillary</a>,
+              <a href="https://openstreetmap.cz/fody" rel='noopener' target='_blank'>Fody</a>,
+              <a href="https://www.wikipedia.org/" rel='noopener' target='_blank'>Wikipedia</a> – za zdjęcia 🖼
+        <li><a href="https://www.openstreetmap.org/" rel='noopener' target='_blank'>OpenStreetMap</a> – za najlepsze na świecie mapy 🌎
+      </ul>
+  `,
+  'homepage.maptiler': '<strong>MapTiler</strong> – za niesamowite mapy wektorowe <br>i za wsparcie dla tego projektu ❤️ ',
+
+  'searchbox.placeholder': 'Przeszukaj OpenStreetMap',
+
+  'featurepanel.no_name': 'Brak nazwy',
+  'featurepanel.share_button': 'Udostępnij',
+  'featurepanel.save_button': 'Zapisz do ulubionych',
+  'featurepanel.directions_button': 'Kierunki',
+  'featurepanel.error': 'Error __code__ while fetching feature from OpenStreetMap',
+  'featurepanel.error_unknown': 'Unknown error while fetching feature from OpenStreetMap.',
+  'featurepanel.error_network': "Can't get the feature, check your network cable.",
+  'featurepanel.error_deleted': 'This object is marked as deleted in OpenStreetMap.',
+  'featurepanel.history_button': 'Historia »',
+  'featurepanel.other_info_heading': 'Więcej informacji',
+  'featurepanel.edit_button_title': 'Edytuj w bazie danych OpenStreetMap',
+  'featurepanel.edit_button': 'Edytuj miejsce',
+  'featurepanel.add_place_button': 'Dodaj miejsce',
+  'featurepanel.undelete_button': 'Cofnij usunięcie',
+  'featurepanel.feature_description_nonosm': 'Map feature __type__',
+  'featurepanel.feature_description_osm': '__type__ in OpenStreetMap database',
+  'featurepanel.feature_description_point': 'Współrzędne mapy',
+  'featurepanel.show_objects_around': 'Wyświetl obiekty w okolicy',
+  'featurepanel.uncertain_image': 'To jest najbliższy obraz widoku ulicy. Może pokazywać inny obiekt.',
+  'featurepanel.inline_edit_title': 'Edycja',
+  'featurepanel.objects_around': 'Obiekty w okolicy',
+
+  'opening_hours.open': 'Otwarte: __todayTime__',
+  'opening_hours.now_closed_but_today': 'Obecnie zamknięte, dziś: __todayTime__',
+  'opening_hours.today_closed': 'Dzisiaj zamknięte',
+  'opening_hours.days_su_mo_tu_we_th_fr_sa': 'niedziela|poniedziałek|wtorek|środa|czwartek|piątek|sobota',
+
+  'map.github_title': 'GitHub',
+  'map.language_title': 'Zmień język',
+  'map.osm_copyright_tooltip': '(c) Współtwórcy OpenStreetMap.org<br> – wolne dane mapowe świata 👌',
+  'map.maptiler_copyright_tooltip': '(c) MapTiler.com ❤️ <br> – wektorowe kafelki, hosting, mapa zewnętrzna<br>Wielkie podziękowania za wsparcie tego projektu! 🙂 ',
+  'map.more_button': 'więcej',
+  'map.more_button_title': 'Więcej opcji…',
+  'map.edit_link': 'Edytuj ten obszar w edytorze iD',
+  'map.about_link': 'O tej aplikacji',
+
+  'editdialog.add_heading': 'Dodaj do OpenStreetMap',
+  'editdialog.undelete_heading': 'Dodaj ponownie do OpenStreetMap',
+  'editdialog.edit_heading': 'Edycja:',
+  'editdialog.suggest_heading': 'Suggest an edit:',
+  'editdialog.feature_type_select': 'Choose type',
+  'editdialog.options_heading': 'Opcje',
+  'editdialog.cancel_button': 'Anuluj',
+  'editdialog.save_button_edit': 'Zapisz w OSM',
+  'editdialog.save_button_delete': 'Usuń',
+  'editdialog.save_button_note': 'Dodaj uwagę',
+  'editdialog.changes_needed': 'Please, make some changes.',
+  'editdialog.loggedInMessage': 'You are logged in as <b>__osmUser__</b>, changes will be saved immediately.',
+  'editdialog.logout': 'wyloguj',
+  'editdialog.anonymousMessage1': 'An <b>anonymous</b> note will be added to the map.<br />If you',
+  'editdialog.anonymousMessage2_login': 'log in to OpenStreetMap',
+  'editdialog.anonymousMessage3': ', your changes will be immediate.',
+  'editdialog.add_major_tag': 'Dodaj',
+  'editdialog.location_checkbox': 'Zmień położenie',
+  'editdialog.location_placeholder': 'eg. across the street',
+  'editdialog.location_editor_to_be_added': 'The position cannot be edited here yet, you can do so in the <a href="__link__">iD editor</a>.',
+  'editdialog.place_cancelled': 'Place permanently closed (delete)',
+  'editdialog.comment': 'Komentarz (opcjonalnie)',
+  'editdialog.comment_placeholder': 'link to the source of information etc.',
+  'editdialog.info_edit': `Your edit will be immediately saved to the OpenStreetMap. Please,
+         enter only information from your own or verified sources. It is prohibited
+         to copy copyrighted data (e.g. Google Maps). <a href="https://wiki.openstreetmap.org/wiki/How_We_Map">More info</a>`,
+  'editdialog.info_note': `Your suggestion will be processed by OpenStreetMap volunteers. Here
+         you can add an additional note or describe position adjustment etc.
+         It is also appropriate to support your contribution with a link to a source of information (web,
+         photo, etc.).`,
+  'editdialog.other_tags': 'Other properties – tags',
+  'editdialog.other_tags.new_key': 'new key',
+  'editdialog.other_tags.add': 'Add another tag',
+  'editdialog.other_tags.will_be_deleted': 'will be deleted',
+  'editdialog.other_tags.info': `The Tags describe properties
+        of each map feature in agreed format. Here you will find the complete
+        <a href="https://wiki.openstreetmap.org/wiki/Map_Features">overview of all tags in OpenStreetMap</a>.`,
+
+  'editsuccess.close_button': 'Zamknij',
+  'editsuccess.note.heading': 'Thank you for your suggestion!',
+  'editsuccess.note.subheading': 'OpenStreetMap volunteers will process it over time.',
+  'editsuccess.note.body': 'It usually takes few days. However, in places with no active community, it can take a very long time.',
+  'editsuccess.note.urlLabel': 'You can add information or follow updates here:',
+  'editsuccess.note.textLabel': 'Note text',
+  'editsuccess.edit.heading': 'Thank you for your edit!',
+  'editsuccess.edit.subheading': 'Your change is starting to appear on maps around the world.',
+  'editsuccess.edit.body': `It is already stored in the OSM database. You will see it on the "OSM Mapnik" map in a few minutes.
+         The OsmAPP map and various other applications are refreshed about once a month.
+          <br/><br/>If this is a mistake, you can manually revert the values and save it again.`,
+  'editsuccess.edit.urlLabel': `Twoje zmiany:`,
+  'editsuccess.edit.textLabel': 'Komentarz',
+
+  'tags.name': 'Nazwa',
+  'tags.website': 'Strona www',
+  'tags.phone': 'Telefon',
+  'tags.opening_hours': 'Godziny otwarcia',
+
+  'layerswitcher.button': 'Mapy',
+  'layerswitcher.heading': 'Warstwy mapy',
+  'layerswitcher.intro': 'Thanks to the fact that OpenStreetMap offers source data, anyone can produce different variants of the map.',
+  'layerswitcher.add_layer_button': 'Add custom layer',
+  'layerswitcher.add_layer_prompt': 'Add custom TMS layer – needs correct CORS:\n(beta feature)',
+
+  'layers.basic': 'Podstawowa',
+  'layers.outdoor': 'Na zewnątrz',
+  'layers.mtb': 'MTB',
+  'layers.snow': 'Śnieg',
+  'layers.mapnik': 'OSM Mapnik',
+  'layers.sat': 'Satelita (p<14)',
+  'layers.bike': 'Rower',
+};
