3ea8d9b9dec163a39a51cc9607a68ee91753bbf5
Alexander Menk <alex@addismap.com>
2022-01-16 19:41:33 +0100

    Add German Translation

---
+++ src/locales/de.js
@@ -0,0 +1,156 @@
+// identifier should be in the form: foldername.messageid.
+
+export default {
+  loading: 'Lade',
+  error: 'Fehler',
+  close_panel: 'Panel schließen',
+  webgl_error: `Ups. Diese Karte setzt die WebGL Technologie voraus .<br /><br />Wenn Du ein kompatibles Gerät hast, versuch die letzte Version des Chrome Browsers zu verwenden.`,
+
+  'install.button': 'App installieren',
+  'install.tabs_aria_label': 'Platform wählen',
+  'install.ios_intro': 'osmapp.org im <strong>Safari Browser</strong> öffnen',
+  'install.ios_share': 'Drücke das <strong>Teilen Icon</strong>',
+  'install.ios_add': 'Drücke <strong>Zum Startbildschirm zufügen</strong>',
+  'install.android_intro': 'Öffner osmapp.org im <strong>Chrome Browser</strong>',
+  'install.android_share': 'Drücke das <strong>Drei-Punkte Menü</strong>',
+  'install.android_add': 'Drücke <strong>App installieren</strong>',
+  'install.desktop_intro': 'Öffne osmapp.org im <strong>Chrome</strong>, <strong>FirefoxOS</strong> oder <strong>Opera</strong>',
+  'install.desktop_install': 'Drücke die Schaltfläche <strong>Installieren</strong>',
+  'install.outro': "Das war's! OsmAPP sollte auf deinem Startbildschirm erscheinen.",
+  'install.note': 'Hinweis: Diese App verwendet PWA Technologie und bietete eine schnelle Installation ohne Google Play oder App Store.',
+
+  'homepage.subtitle': 'Eine universelle OpenStreetMap app',
+  'homepage.how_to_start': 'Tippe einen Begriff in die Suchbox.\nOder klicke auf einen Ort auf der Karte.',
+  'homepage.examples.eg': 'z.B.',
+  'homepage.examples.charles_bridge_statues': 'Statuen auf der Karlsbrücke',
+  'homepage.screenshot_alt': 'Screenshot der OsmAPP',
+  'homepage.about_osm': `Alle Kartendaten sind von
+      <a href="https://osm.org">OpenStreetMap</a>, eine Karte von Millionen von Mitwirkenden
+      — ähnlich wie bei Wikipedia. Du findest die Schaltfläche
+      <em>Bearbeiten</em> bei jedem Kartenelement.`,
+  'homepage.heading_about_osmapp': 'Über OsmAPP',
+  'homepage.about_osmapp': `Diese Anwendung soll eine komfortable Oberfläche für die tägliche Nutzung von <i>OpenStreetMap</i>,
+     samt Bearbeitungsfunktionen, bieten. <br/> Derzeit umfasst sie verschiedene Kartenebenen, POI-Bearbeitung und eine einfache Suchmaschine.
+     Funktionen wie Navigation und eine Favoritenfunktion sind in Planung`,
+  'homepage.github_link': `Du kannst neue Feature auf <a href="https://github.com/zbycz/osmapp" rel='noopener' target='_blank'>vorschlagen</a>.`,
+  'homepage.special_thanks_heading': `Danke an`,
+  'homepage.special_thanks': `<ul>
+        <li><a href="https://www.mapillary.com/" rel='noopener' target='_blank'>Mapillary</a>,
+              <a href="https://openstreetmap.cz/fody" rel='noopener' target='_blank'>Fody</a>,
+              <a href="https://www.wikipedia.org/" rel='noopener' target='_blank'>Wikipedia</a> – für die Bilder 🖼
+        <li><a href="https://www.openstreetmap.org/" rel='noopener' target='_blank'>OpenStreetMap</a> – für die beste Weltkarte 🌎
+      </ul>
+  `,
+  'homepage.maptiler': '<strong>MapTiler</strong> – für die großartigen Vektor Karten <br>und die Unterstützung dieses Projekts  ❤️ ',
+
+  'searchbox.placeholder': 'Durchsuche OpenStreetMap',
+
+  'featurepanel.no_name': 'Kein Name',
+  'featurepanel.share_button': 'Teilen',
+  'featurepanel.save_button': 'Zu Favoriten hinzufügen',
+  'featurepanel.directions_button': 'Navigation',
+  'featurepanel.error': 'Fehler __code__ während des Abrufs des Objekts von OpenStreetMap',
+  'featurepanel.error_unknown': 'Unbekannter Fehler beim Abruf des Objekts von OpenStreetMap.',
+  'featurepanel.error_network': 'Kann das Objekt nicht abrufen, bitte überprüfe die Netzwerkverbindung.',
+  'featurepanel.error_deleted': 'Das Objekt wurde in OpenStreetMap als gelöscht markiert.',
+  'featurepanel.history_button': 'Versionsverlauf »',
+  'featurepanel.other_info_heading': 'Weiter Informationen',
+  'featurepanel.edit_button_title': 'In OpenStreetMap Datenbank bearbeiten',
+  'featurepanel.edit_button': 'Ort bearbeiten',
+  'featurepanel.add_place_button': 'Ort hinzufügen',
+  'featurepanel.undelete_button': 'Löschen rückgängig machen',
+  'featurepanel.feature_description_nonosm': 'Kartenobjekt __type__',
+  'featurepanel.feature_description_osm': '__type__ in der OpenStreetMap Datenbank',
+  'featurepanel.feature_description_point': 'Karten Koordinaten',
+  'featurepanel.show_objects_around': 'Zeige Orte in der Nähe',
+  'featurepanel.uncertain_image': 'Dies ist das nächste Street View Bild. Es kann ein anders Objekt zeigen.',
+  'featurepanel.inline_edit_title': 'Bearbeiten',
+  'featurepanel.objects_around': 'Objekt in der Nähe',
+
+  'opening_hours.open': 'Geöffnet: __todayTime__',
+  'opening_hours.now_closed_but_today': 'Geschlossen, heute: __todayTime__',
+  'opening_hours.today_closed': 'Heute geschlossen',
+  'opening_hours.days_su_mo_tu_we_th_fr_sa': 'Sonntag|Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag',
+
+  'map.github_title': 'GitHub',
+  'map.language_title': 'Sprache wechseln',
+  'map.osm_copyright_tooltip': '(c) OpenStreetMap.org Mitwirkende<br> – freie Karte der Erde 👌',
+  'map.maptiler_copyright_tooltip':
+    '(c) MapTiler.com ❤️ <br> – Vektor Tiles, Hosting, Outdoor Karte<br>Vielen Dank für die Unterstützung dieses Projekts! 🙂 ',
+  'map.more_button': 'mehr',
+  'map.more_button_title': 'Mehr Optionen…',
+  'map.edit_link': 'Diesen Breich im iD Editor bearbeiten',
+  'map.about_link': 'Über diese App',
+
+  'editdialog.add_heading': 'Zu OpenStreetMap hinzfügen',
+  'editdialog.undelete_heading': 'Wieder zu OpenStreetMap hinzufügen',
+  'editdialog.edit_heading': 'Bearbeiten:',
+  'editdialog.suggest_heading': 'Änderung vorschlagen:',
+  'editdialog.feature_type_select': 'Typ auswählen',
+  'editdialog.options_heading': 'Optionen',
+  'editdialog.cancel_button': 'Abbrechen',
+  'editdialog.save_button_edit': 'In OSM speichern',
+  'editdialog.save_button_delete': 'Löschen',
+  'editdialog.save_button_note': 'Notiz hinzufügen',
+  'editdialog.changes_needed': 'Bitte nimm ein paar Änderungen vor.',
+  'editdialog.loggedInMessage': 'Du bist als <b>__osmUser__</b> eingeloggt, Änderungen werden sofort gespeichert.',
+  'editdialog.logout': 'logout',
+  'editdialog.anonymousMessage1': 'Ein <b>anonyme</b>Notiz wird der Karte hinzugefügt.<br />Wenn du dich',
+  'editdialog.anonymousMessage2_login': 'auf OpenStreetMap einloggt',
+  'editdialog.anonymousMessage3': ', werden deine Änderungen direkt gespeichert.',
+  'editdialog.add_major_tag': 'Hinzufügen',
+  'editdialog.location_checkbox': 'Position ändern',
+  'editdialog.location_placeholder': 'z.B. gegenüber auf der Straße',
+  'editdialog.location_editor_to_be_added':
+    'Die Position kann hier noch nicht bearbeitet werden, dies ist aber im <a href="__link__">iD editor</a> möglich.',
+  'editdialog.place_cancelled': 'Endgültig geschlossen (Löschen)',
+  'editdialog.comment': 'Kommentar (optional)',
+  'editdialog.comment_placeholder': 'Link zur Quelle der Informationen usw.',
+  'editdialog.info_edit': `Deine Änderunge werden direkt in OpenStreetMap gespeichert. Bitte füge nur Informationen
+         von deinen eigenen oder geprüften Quellen hinzu. Es ist nicht erlaubt, Urheberrechtlich geschützte Informationen
+         (z.B. von Google Maps) zu kopieren. <a href="https://wiki.openstreetmap.org/wiki/How_We_Map">weitere Informationen</a>`,
+  'editdialog.info_note': `Dein Vorschlag wird von Freiwilligen des OpenStreetMap Projektes bearbeitet. Hier
+         kannst du weitere Hinweise oder Beschreibungen der neuen Position usw. hinzufügen.
+         Es ist auch in Ordnung, deinen Beitrag mit einem Link zur Quelle (web,
+         Foto, etc.) zu versehen.`,
+  'editdialog.other_tags': 'Weitere Eigenschaften – Tags',
+  'editdialog.other_tags.new_key': 'Neuer Schlüssel',
+  'editdialog.other_tags.add': 'Weiterer Tag',
+  'editdialog.other_tags.will_be_deleted': 'wird gelöscht',
+  'editdialog.other_tags.info': `Ein Tag beschreibt Eigenschaften
+        eines Ortes auf der Karte in einem einheitlichen Format. Hier findest du die
+        <a href="https://wiki.openstreetmap.org/wiki/Map_Features">komplette Übersicht aller Tags in OpenStreetMap</a>.`,
+
+  'editsuccess.close_button': 'Schließen',
+  'editsuccess.note.heading': 'Danke für deinen Hinweis!',
+  'editsuccess.note.subheading': 'Freiwillige des OpenStreetMap Projekts werden diesen mit der Zeit bearbeiten.',
+  'editsuccess.note.body': 'Normalerweise dauert dies ein paar Tage, allerdings kann es an Orten ohne aktive Community auch sehr lange dauern.',
+  'editsuccess.note.urlLabel': 'Du kannst hier weitere Informationen hinzufügen und Aktualisierungen verfolgen:',
+  'editsuccess.note.textLabel': 'Notiz',
+  'editsuccess.edit.heading': 'Danke für deine Bearbeitung!',
+  'editsuccess.edit.subheading': 'Deine Änderung erscheint auf Karten auf der ganzen Welt.',
+  'editsuccess.edit.body': `Die Änderung ist bereits in der OSM Datenbank gespeichert. Du wirst dieses auf der "OSM Mapnik" Karte in wenigen Minuten sehen können.
+         Die OsmAPP Karte und verschiedene anderen Karten werden ca. einmal im Monat aktualisiert.
+          <br/><br/>Wenn dies ein Versehen war, kannst du die Werte manuell rückgängig machen und erneut speichern.`,
+  'editsuccess.edit.urlLabel': `Deine Änderungen:`,
+  'editsuccess.edit.textLabel': 'Kommentar',
+
+  'tags.name': 'Name',
+  'tags.website': 'Web',
+  'tags.phone': 'Telefon',
+  'tags.opening_hours': 'Öffnungszeiten',
+
+  'layerswitcher.button': 'Karten',
+  'layerswitcher.heading': 'Kartenebenen',
+  'layerswitcher.intro': 'Dank der Tatsache, dass OpenStreetMap Quelldaten anbietet, kann jeder verschiedene Varianten der Karte erstellen.',
+  'layerswitcher.add_layer_button': 'Eigene Ebene hinzufügen',
+  'layerswitcher.add_layer_prompt': 'Eigene TMS Ebene hinzufügen – benötigt entsprechende CORS Header:\n(Beta)',
+
+  'layers.basic': 'Standard',
+  'layers.outdoor': 'Outdoor',
+  'layers.mtb': 'MTB',
+  'layers.snow': 'Schnee',
+  'layers.mapnik': 'OSM Mapnik',
+  'layers.sat': 'Satellit (z<14)',
+  'layers.bike': 'Fahrrad',
+};
