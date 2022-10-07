74bacf19721c623d6daa06bb20894b479b136244
Pavel Zbytovský <zbytovsky@gmail.com>
2022-10-07 23:02:55 +0200

    fix broken formula + add debug view

---
+++ src/services/images/__tests__/getMapillaryImage.test.ts
@@ -0,0 +1,31 @@
+import { Position } from "../../types";
+import { getBearing } from "../getMapillaryImage";
+
+const point: Position = [0, 0];
+const pointAbove: Position = [0, 1];
+const point45: Position = [1, 1];
+const pointRight: Position = [1, 0];
+const pointBelow: Position = [0, -1]; // 180
+const point225: Position = [-1, -1]; // 180+45
+const pointLeft: Position = [-1, 0];
+
+const praha: Position = [14.3, 50.1];  //lon, lat  --> x,y
+const liberec: Position = [15.05, 50.75];
+
+describe('getBearing', () => {
+  it('should work for node', async () => {
+    //      pA  p45
+    //      | /
+    // pL - p - pR
+    //    / |
+    // p225 pB
+    expect(getBearing(point, pointAbove)).toEqual(0);
+    expect(getBearing(point, point45)).toEqual(45);
+    expect(getBearing(point, pointRight)).toEqual(90);
+    expect(getBearing(point, pointBelow)).toEqual(180); // 180
+    expect(getBearing(point, point225)).toEqual(225);
+    expect(getBearing(point, pointLeft)).toEqual(270);
+
+    expect(Math.round(getBearing(praha, liberec))).toEqual(49);
+  })
+});
--- src/services/images/getMapillaryImage.ts
+++ src/services/images/getMapillaryImage.ts
@@ -1,44 +1,63 @@
 import { fetchJson } from '../fetch';
 import { Image, Position } from '../types';
+import { getGlobalMap } from "../mapStorage";
+import maplibregl from "maplibre-gl";
 
-const deltaAngle = (a: number, b: number): number =>
+const subtractAngle = (a: number, b: number): number =>
   Math.min(Math.abs(a - b), a - b + 360);
 
-const bearing = (a1: number, a2: number, b1: number, b2: number): number => {
-  const rad2deg = 57.2957795130823209;
-  if (a1 === b1 && a2 === b2) return Infinity;
-  let theta = Math.atan2(b1 - a1, a2 - b2);
-  if (theta < 0) theta += Math.PI * 2;
-  return rad2deg * theta;
+export const getBearing = ([aX, aY]: Position, [bX, bY]: Position): number => {
+  const angle = (Math.atan2(bX - aX, bY - aY) * 180) / Math.PI;
+  return angle < 0 ? angle + 360 : angle;
 };
 
-const getMapillaryImageRaw = async (pPoi: Position): Promise<Image> => {
-  const bbox = `${pPoi[0] - 0.0004},${pPoi[1] - 0.0004},${pPoi[0] + 0.0004},${
-    pPoi[1] + 0.0004
-  }`;
+const getMapillaryImageRaw = async (poiCoords: Position): Promise<Image> => {
+  const bbox = `${poiCoords[0] - 0.0004},${poiCoords[1] - 0.0004},${
+    poiCoords[0] + 0.0004
+  },${poiCoords[1] + 0.0004}`;
+
+  // https://www.mapillary.com/developer/api-documentation/#image
+  // left, bottom, right, top (or minLon, minLat, maxLon, maxLat)
+  // consider computed_compass_angle - it is zero for many images, so we would have to fallback to compass_angle
   const url = `https://graph.mapillary.com/images?access_token=MLY|4742193415884187|44e43b57d0211d8283a7ca1c3e6a63f2&fields=compass_angle,computed_geometry,captured_at,thumb_1024_url&bbox=${bbox}`;
   const { data } = await fetchJson(url);
 
-  let bestImg = null;
-
-  for (let i = 0; i < data.length; i += 1) {
-    const pImg = data[i].computed_geometry.coordinates;
-    const bear = bearing(pImg[0], pImg[1], pPoi[0], pPoi[1]);
-    data[i].angleToPoi = deltaAngle(bear, data[i].compass_angle);
-    if (
-      data[i].angleToPoi < 50 &&
-      (bestImg == null || bestImg.angleToPoi > data[i].angleToPoi)
-    ) {
-      bestImg = data[i];
-    }
+  if (!data.length) {
+    return undefined;
   }
 
-  if (bestImg === null) return undefined;
+  const itemsWithDeviation = data.map((item) => {
+    const photoCoords = item.computed_geometry.coordinates;
+    const angleFromPhotoToPoi = getBearing(photoCoords, poiCoords);
+    const deviationFromStraightSight = subtractAngle(
+      angleFromPhotoToPoi,
+      item.compass_angle,
+    );
+
+    new maplibregl.Marker({ rotation: item.compass_angle })
+      .setLngLat(photoCoords)
+      .addTo(getGlobalMap());
+
+    return { ...item, angleFromPhotoToPoi, deviationFromStraightSight };
+  });
+
+  console.log({ itemsWithDeviation });
+
+  const closest = itemsWithDeviation.reduce((acc, item) =>
+    acc.deviationFromStraightSight < item.deviationFromStraightSight
+      ? acc
+      : item,
+  );
+
+  new maplibregl.Marker({ rotation: closest.compass_angle, color: '#eb5757' })
+    .setLngLat(closest.computed_geometry.coordinates)
+    .addTo(getGlobalMap());
 
   return {
     source: 'Mapillary',
-    link: `https://www.mapillary.com/app/?focus=photo&pKey=${bestImg.id}`,
-    thumb: bestImg.thumb_1024_url,
+    link: `https://www.mapillary.com/app/?focus=photo&pKey=${closest.id}`,
+    thumb: closest.thumb_1024_url,
+    timestamp: new Date(closest.captured_at).toLocaleString()
   };
 };
 
