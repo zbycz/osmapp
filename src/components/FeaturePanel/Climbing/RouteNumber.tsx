ce54c54eddd51798c291412b082c210787dcce16
Jan Václavík <jvaclavik@gmail.com>
2023-09-17 22:32:51 +0200

    Fix route number in case of small photo

--- src/components/FeaturePanel/Climbing/RouteNumber.tsx
+++ src/components/FeaturePanel/Climbing/RouteNumber.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { ClimbingEditorContext } from './contexts/climbingEditorContext';
 
 type Props = {
   onRouteSelect: (routeNumber: number) => void;
@@ -31,6 +32,12 @@ export const RouteNumber = ({
   const OUTLINE_WIDTH = 2;
   const HOVER_WIDTH = 10;
 
+  const { imageSize } = useContext(ClimbingEditorContext);
+  const newY = // this shifts Y coordinate in case of too small photo
+    y + RECT_Y_OFFSET + RECT_HEIGHT > imageSize.height
+      ? imageSize.height - RECT_HEIGHT - OUTLINE_WIDTH
+      : y + RECT_Y_OFFSET;
+
   const commonProps = {
     onClick: (e) => {
       onRouteSelect(routeNumber);
@@ -44,7 +51,7 @@ export const RouteNumber = ({
     <>
       <HoverableRouteName
         x={x - RECT_WIDTH / 2 - HOVER_WIDTH / 2}
-        y={y + RECT_Y_OFFSET - HOVER_WIDTH / 2}
+        y={newY - HOVER_WIDTH / 2}
         width={RECT_WIDTH + HOVER_WIDTH}
         height={RECT_HEIGHT + HOVER_WIDTH}
         rx="10"
@@ -54,7 +61,7 @@ export const RouteNumber = ({
 
       <RouteNameOutline
         x={x - RECT_WIDTH / 2 - OUTLINE_WIDTH / 2}
-        y={y + RECT_Y_OFFSET - OUTLINE_WIDTH / 2}
+        y={newY - OUTLINE_WIDTH / 2}
         width={RECT_WIDTH + OUTLINE_WIDTH}
         height={RECT_HEIGHT + OUTLINE_WIDTH}
         rx="10"
@@ -63,7 +70,7 @@ export const RouteNumber = ({
       />
       <RouteNameBox
         x={x - RECT_WIDTH / 2}
-        y={y + RECT_Y_OFFSET}
+        y={newY}
         width={RECT_WIDTH}
         height={RECT_HEIGHT}
         rx="10"
@@ -72,7 +79,7 @@ export const RouteNumber = ({
       />
       <text
         x={x}
-        y={y + RECT_Y_OFFSET + 15}
+        y={newY + 15}
         fill={isSelected ? 'white' : '#666'}
         textAnchor="middle"
         fontWeight="bold"
