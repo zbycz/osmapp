eba0d403e76815262cd03c7fcabf17900a91e416
Pavel Zbytovský <zbytovsky@gmail.com>
2023-09-18 21:16:54 +0200

    fix delta

--- src/components/Map/behaviour/useIntegerZoomInRaster.tsx
+++ src/components/Map/behaviour/useIntegerZoomInRaster.tsx
@@ -7,7 +7,7 @@ export const useIntegerZoomInRaster = useAddMapEvent((map, activeLayers) => ({
       activeLayers[0] !== 'basic' && activeLayers[0] !== 'outdoor';
 
     const delta = parseFloat(window.location.search?.substring(1)) || 0.1;
-    const zoomIsInVicinity = Math.abs(map.getZoom() % 1) < delta;
+    const zoomIsInVicinity = Math.abs(map.getZoom() % 1) < delta || Math.abs(map.getZoom() % 1) > 1 - delta;
     if (isRaster && map.getZoom() % 1 !== 0 && zoomIsInVicinity) {
       map.setZoom(Math.round(map.getZoom()));
     }
