6fdb1a8be18419d3a79d45b541046fadc7da07be
Pavel Zbytovský <zbytovsky@gmail.com>
2023-10-16 20:37:36 +0200

    Map: round zoom when switching to raster #103

--- src/components/Map/behaviour/useUpdateStyle.tsx
+++ src/components/Map/behaviour/useUpdateStyle.tsx
@@ -12,12 +12,13 @@ export const getRasterStyle = (key) => {
 
 export const useUpdateStyle = useMapEffect((map, activeLayers) => {
   const key = activeLayers[0] ?? DEFAULT_MAP;
+
   map.setMaxZoom(osmappLayers[key]?.maxzoom ?? 24); // TODO find a way how to zoom bing further (now it stops at 19)
-  map.setStyle(
-    key === 'basic'
-      ? basicStyle
-      : key === 'outdoor'
-      ? outdoorStyle
-      : getRasterStyle(key),
-  );
+
+  if (key === 'basic' || key === 'outdoor') {
+    map.setStyle(key === 'basic' ? basicStyle : outdoorStyle);
+  } else {
+    map.setStyle(getRasterStyle(key));
+    map.setZoom(Math.round(map.getZoom()));
+  }
 });
