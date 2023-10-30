7ef51452cbf37acf37bf7a2ad0bcd8440a52bf58
Pavel Zbytovský <zbytovsky@gmail.com>
2023-10-30 09:09:09 +0100

    fix no tags but prettier

--- src/components/FeaturePanel/Properties/Properties.tsx
+++ src/components/FeaturePanel/Properties/Properties.tsx
@@ -21,7 +21,9 @@ export const Properties = ({ showTags }) => {
       )}
       {showTags && (
         <>
-          {!!Object.keys(feature.tags).length && <Subheading>{t("featurepanel.all_tags_heading")}</Subheading>}
+          {!!Object.keys(feature.tags).length && (
+            <Subheading>{t('featurepanel.all_tags_heading')}</Subheading>
+          )}
           <Wrapper>
             <Table>
               <tbody>
