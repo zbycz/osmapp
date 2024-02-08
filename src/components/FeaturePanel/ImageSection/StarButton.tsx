67cd5d162d78bbca7b51d0e3d797d8b359116565
Pavel Zbytovský <zbytovsky@gmail.com>
2024-02-08 09:36:12 +0100

    fix ssr hydrate warning

--- src/components/FeaturePanel/ImageSection/StarButton.tsx
+++ src/components/FeaturePanel/ImageSection/StarButton.tsx
@@ -1,26 +1,45 @@
 import Star from '@material-ui/icons/Star';
 import StarBorder from '@material-ui/icons/StarBorder';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { t } from '../../../services/intl';
 import { useStarsContext } from '../../utils/StarsContext';
 import { StyledActionButton } from './utils';
 
+const useIsClient = () => {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  return isClient;
+};
+
+const StarButtonPure = ({ isStarred, toggleStar }) => (
+  <StyledActionButton onClick={toggleStar}>
+    {isStarred ? (
+      <Star
+        htmlColor="#fff"
+        titleAccess={t('featurepanel.favorites_save_button')}
+      />
+    ) : (
+      <StarBorder
+        htmlColor="#fff"
+        titleAccess={t('featurepanel.favorites_unsave_button')}
+      />
+    )}
+  </StyledActionButton>
+);
+
 export const StarButton = () => {
   const { toggleStar, isStarred } = useStarsContext();
+  const isClient = useIsClient(); // SSR doesn't know localStorage, so it would end with hydrationWarning
 
   return (
-    <StyledActionButton onClick={toggleStar}>
-      {isStarred ? (
-        <Star
-          htmlColor="#fff"
-          titleAccess={t('featurepanel.favorites_save_button')}
-        />
+    <>
+      {isClient ? (
+        <StarButtonPure isStarred={isStarred} toggleStar={toggleStar} />
       ) : (
-        <StarBorder
-          htmlColor="#fff"
-          titleAccess={t('featurepanel.favorites_unsave_button')}
-        />
+        <StarButtonPure isStarred={false} onClick={() => {}} />
       )}
-    </StyledActionButton>
+    </>
   );
 };
