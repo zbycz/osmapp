6c1acb0738aa0f8c4d71496f408eb1548b618ad9
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-02-10 11:42:35 +0100

    Implement that the panorama view is actually usable

--- src/components/FeaturePanel/ImageSection/FeatureImage.tsx
+++ src/components/FeaturePanel/ImageSection/FeatureImage.tsx
@@ -11,30 +11,19 @@ import { Photo } from './Photo';
 
 const Wrapper = styled.div`
   position: relative;
-  background: #ddd;
+  background: #ddd
+    linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0) 70%,
+      rgba(0, 0, 0, 0.09) 76%,
+      #5b5b5b
+    );
   height: 238px;
   min-height: 238px; /* otherwise it shrinks b/c of flex*/
   ${({ uncertainImage }) =>
     uncertainImage
       ? 'box-shadow: inset 0 0 100px rgba(255,255,255,0.3); filter: contrast(0.6) brightness(1.2);'
       : ''}
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-image: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0) 70%,
-      rgba(0, 0, 0, 0.15) 76%,
-      #000000
-    );
-    // background-image: linear-gradient(to bottom right,#002f4b,#dc4225);
-    opacity: 0.6;
-  }
 `;
 
 const Bottom = styled.div`
@@ -102,7 +91,7 @@ export const FeatureImage = ({ feature, ico, children }: Props) => {
     );
   }, [feature]);
 
-  const { source, link, thumb, username, portrait } = image ?? {};
+  const { source, link, username, portrait } = image ?? {};
   const uncertainImage = source === 'Mapillary';
   const uncertainTitle = uncertainImage
     ? `\n${t('featurepanel.uncertain_image')}`
@@ -110,9 +99,8 @@ export const FeatureImage = ({ feature, ico, children }: Props) => {
   const attribution = `${source}${username ? ` / ${username}` : ''}`;
 
   return (
-    <Wrapper link={thumb} uncertainImage={uncertainImage} portrait={portrait}>
+    <Wrapper uncertainImage={uncertainImage}>
       {image && image !== LOADING && <Photo image={image} />}
-
       {(image === undefined || image === LOADING) && (
         <IconWrapper>
           <img src={`/icons/${ico}_11.svg`} alt={ico} title={ico} />
