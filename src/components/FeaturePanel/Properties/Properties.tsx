840c857151cae64138e0d6fb5e154917b44c33ef
Pavel Zbytovský <zbytovsky@gmail.com>
2024-03-03 07:50:03 +0100

    FeaturePanel: add ErrorBoundary for Properties

--- src/components/FeaturePanel/Properties/Properties.tsx
+++ src/components/FeaturePanel/Properties/Properties.tsx
@@ -7,31 +7,84 @@ import { useFeatureContext } from '../../utils/FeatureContext';
 import { Subheading } from '../helpers/Subheading';
 import { Wrapper } from './Wrapper';
 import { Table } from './Table';
+import { getUrlOsmId } from '../../../services/helpers';
+import { Alert } from '@material-ui/lab';
+import { captureException } from '../../../helpers/sentry';
 
-export const Properties = ({ showTags }) => {
+class ErrorBoundary extends React.Component<
+  { fallback: React.ReactNode },
+  { hasError: boolean }
+> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    captureException(error, errorInfo);
+
+    console.error({ error, errorInfo }); // eslint-disable-line no-console
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // const tryAgain = () => this.setState({ hasError: false });
+
+      return (
+        <div>
+          <Alert
+            variant="outlined"
+            severity="info"
+            style={{ marginBottom: '2em' }}
+          >
+            There was an error displaying fields. Showing tags instead.
+          </Alert>
+
+          {this.props.fallback || null}
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+const OnlyTagsTable = () => {
   const { feature } = useFeatureContext();
 
   return (
     <>
+      {!!Object.keys(feature.tags).length && (
+        <Subheading>{t('featurepanel.all_tags_heading')}</Subheading>
+      )}
+      <Wrapper>
+        <Table>
+          <tbody>
+            <TagsTableInner tags={feature.tags} center={feature.center} />
+          </tbody>
+        </Table>
+      </Wrapper>
+    </>
+  );
+};
+
+export const Properties = ({ showTags }) => {
+  const { feature } = useFeatureContext();
+
+  const key = feature && getUrlOsmId(feature.osmMeta);
+
+  return (
+    <>
+      {showTags && <OnlyTagsTable />}
       {!showTags && (
-        <>
+        <ErrorBoundary key={key} fallback={<OnlyTagsTable />}>
           <FeaturedTags featuredTags={feature.schema?.featuredTags} />
           <IdSchemeFields />
-        </>
-      )}
-      {showTags && (
-        <>
-          {!!Object.keys(feature.tags).length && (
-            <Subheading>{t('featurepanel.all_tags_heading')}</Subheading>
-          )}
-          <Wrapper>
-            <Table>
-              <tbody>
-                <TagsTableInner tags={feature.tags} center={feature.center} />
-              </tbody>
-            </Table>
-          </Wrapper>
-        </>
+        </ErrorBoundary>
       )}
     </>
   );
