1d114d59751fd624217ce4e71f804948bf0da891
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-06-20 21:26:20 +0200

    Implement max zoom

--- src/components/Map/BrowserMap.tsx
+++ src/components/Map/BrowserMap.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { useAddMapEvent, useMapEffect, useMobileMode } from '../helpers';
-import { useMapStateContext } from '../utils/MapStateContext';
+import { Layer, useMapStateContext } from '../utils/MapStateContext';
 import { useFeatureContext } from '../utils/FeatureContext';
 import { useFeatureMarker } from './behaviour/useFeatureMarker';
 import { useOnMapClicked } from './behaviour/useOnMapClicked';
@@ -11,6 +11,7 @@ import { useInitMap } from './behaviour/useInitMap';
 import { Translation } from '../../services/intl';
 import { useToggleTerrainControl } from './behaviour/useToggleTerrainControl';
 import { isWebglSupported } from './helpers';
+import { usePersistedState } from '../utils/usePersistedState';
 
 const useOnMapLoaded = useAddMapEvent((map, onMapLoaded) => ({
   eventType: 'load',
@@ -33,6 +34,8 @@ const NotSupportedMessage = () => (
 // TODO https://cdn.klokantech.com/openmaptiles-language/v1.0/openmaptiles-language.js + use localized name in FeaturePanel
 
 const BrowserMap = ({ onMapLoaded }) => {
+  const [userLayers] = usePersistedState<Layer[]>('userLayers', []);
+
   if (!isWebglSupported()) {
     onMapLoaded();
     return <NotSupportedMessage />;
@@ -49,7 +52,7 @@ const BrowserMap = ({ onMapLoaded }) => {
   useUpdateViewOnMove(map, setViewFromMap, setBbox);
   useToggleTerrainControl(map);
   useUpdateMap(map, viewForMap);
-  useUpdateStyle(map, activeLayers);
+  useUpdateStyle(map, activeLayers, userLayers);
 
   return <div ref={mapRef} style={{ height: '100%', width: '100%' }} />;
 };
--- src/components/Map/behaviour/useUpdateStyle.tsx
+++ src/components/Map/behaviour/useUpdateStyle.tsx
@@ -10,6 +10,7 @@ import { makinaAfricaStyle } from '../styles/makinaAfricaStyle';
 import { climbingLayers } from '../styles/layers/climbingLayers';
 import { EMPTY_GEOJSON_SOURCE } from '../consts';
 import { fetchCrags } from '../../../services/fetchCrags';
+import { Layer } from '../../utils/MapStateContext';
 
 export const getRasterStyle = (key) => {
   const url = osmappLayers[key]?.url ?? key; // if `key` not found, it contains tiles URL
@@ -30,39 +31,44 @@ const getBaseStyle = (key) => {
   return getRasterStyle(key);
 };
 
-export const useUpdateStyle = useMapEffect((map: Map, activeLayers) => {
-  const [basemap, ...overlays] = activeLayers;
+export const useUpdateStyle = useMapEffect(
+  (map: Map, activeLayers, userLayers: Layer[]) => {
+    const [basemap, ...overlays] = activeLayers;
 
-  const key = basemap ?? DEFAULT_MAP;
+    const key = basemap ?? DEFAULT_MAP;
 
-  map.setMaxZoom(osmappLayers[key]?.maxzoom ?? 24); // TODO find a way how to zoom bing further (now it stops at 19)
+    const osmappLayerMaxZoom = osmappLayers[key]?.maxzoom;
+    const userLayerMaxZoom = userLayers.find(({ url }) => url === key)?.maxzoom;
 
-  const style = cloneDeep(getBaseStyle(key));
-  overlays.forEach((overlayKey) => {
-    const overlay = osmappLayers[overlayKey];
+    map.setMaxZoom(osmappLayerMaxZoom ?? userLayerMaxZoom ?? 24); // TODO find a way how to zoom bing further (now it stops at 19)
 
-    if (overlay?.type === 'overlay') {
-      const raster = getRasterStyle(overlayKey);
-      style.sources[overlayKey] = raster.sources[overlayKey];
-      style.layers.push(raster.layers[0]);
+    const style = cloneDeep(getBaseStyle(key));
+    overlays.forEach((overlayKey) => {
+      const overlay = osmappLayers[overlayKey];
 
-      // TODO maxzoom 19 only for snow overlay
-    }
+      if (overlay?.type === 'overlay') {
+        const raster = getRasterStyle(overlayKey);
+        style.sources[overlayKey] = raster.sources[overlayKey];
+        style.layers.push(raster.layers[0]);
 
-    if (overlay?.type === 'overlayClimbing') {
-      style.sources.climbing = EMPTY_GEOJSON_SOURCE;
-      style.layers.push(...climbingLayers); // must be also in `layersWithOsmId` because of hover effect
-      fetchCrags().then(
-        (geojson) => {
-          const geojsonSource = map.getSource('climbing') as GeoJSONSource;
-          geojsonSource?.setData(geojson); // TODO can be undefined at first map render
-        },
-        (error) => {
-          console.warn('Climbing Layer failed to fetch.', error); // eslint-disable-line no-console
-        },
-      );
-    }
-  });
+        // TODO maxzoom 19 only for snow overlay
+      }
 
-  map.setStyle(style, { diff: true });
-});
+      if (overlay?.type === 'overlayClimbing') {
+        style.sources.climbing = EMPTY_GEOJSON_SOURCE;
+        style.layers.push(...climbingLayers); // must be also in `layersWithOsmId` because of hover effect
+        fetchCrags().then(
+          (geojson) => {
+            const geojsonSource = map.getSource('climbing') as GeoJSONSource;
+            geojsonSource?.setData(geojson); // TODO can be undefined at first map render
+          },
+          (error) => {
+            console.warn('Climbing Layer failed to fetch.', error); // eslint-disable-line no-console
+          },
+        );
+      }
+    });
+
+    map.setStyle(style, { diff: true });
+  },
+);
