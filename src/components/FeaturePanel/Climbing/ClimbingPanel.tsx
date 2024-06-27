9a9ae90782135016551528726fba749a65f36c4d
Pavel Zbytovský <zbytovsky@gmail.com>
2024-06-27 10:53:00 +0200

    climbing: replace photo with ImageSlider

--- src/components/FeaturePanel/Climbing/ClimbingPanel.tsx
+++ src/components/FeaturePanel/Climbing/ClimbingPanel.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import Router from 'next/router';
-import ZoomInIcon from '@mui/icons-material/ZoomIn';
-import { Button, CircularProgress } from '@mui/material';
-import { useClimbingContext } from './contexts/ClimbingContext';
+import { Box } from '@mui/material';
 import { PanelScrollbars, PanelWrapper } from '../../utils/PanelHelpers';
-import { RoutesLayer } from './Editor/RoutesLayer';
 import { RouteList } from './RouteList/RouteList';
-import { FullscreenIconContainer, ShowFullscreen } from './ShowFullscreen';
 import { useFeatureContext } from '../../utils/FeatureContext';
 import { getLabel } from '../../../helpers/featureLabel';
-import { getCommonsImageUrl } from '../../../services/images/getWikiImage';
-import { getOsmappLink } from '../../../services/helpers';
 import { StarButton } from '../ImageSection/StarButton';
 import { OsmError } from '../OsmError';
 import { Properties } from '../Properties/Properties';
@@ -20,34 +13,12 @@ import { ImageSlider } from '../ImagePane/ImageSlider';
 import { ClimbingParentLink } from '../ParentLink';
 import { RouteDistribution } from './RouteDistribution';
 import { YellowedBadge } from './YellowedBadge';
-import { getWikimediaCommonsKeys, removeFilePrefix } from './utils/photo';
 import { SuggestEdit } from '../SuggestEdit';
-
-const ThumbnailContainer = styled.div<{ height: number }>`
-  width: 100%;
-  height: ${({ height }) => height}px;
-  position: relative;
-  &:hover ${FullscreenIconContainer} {
-    visibility: visible;
-    -webkit-backdrop-filter: blur(3px);
-    backdrop-filter: blur(3px);
-    background-color: rgba(0, 0, 0, 0.5);
-    cursor: pointer;
-  }
-`;
 
 const HeadingContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
-
-const DetailButtonContainer = styled.div`
-  margin: 8px;
-  @supports (-webkit-touch-callout: none) {
-    /* CSS specific to iOS devices */
-    margin-bottom: 28px;
-  }
 `;
 
 const Heading = styled.div`
@@ -56,76 +27,14 @@ const Heading = styled.div`
   line-height: 0.98;
 `;
 
-const Thumbnail = styled.img<{ $isLoading: boolean }>`
-  width: 100%;
-  position: absolute;
-  visibility: ${({ $isLoading }) => ($isLoading ? 'hidden' : 'visible')};
-`;
-
-const LoadingContainer = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  align-items: center;
-`;
-
 export const ClimbingPanel = ({ footer, showTagsTable }) => {
   const { feature } = useFeatureContext();
   const label = getLabel(feature);
 
-  const {
-    loadPhotoRelatedData,
-    imageSize,
-    photoPath,
-    photoRef,
-    preparePhotosAndSet,
-  } = useClimbingContext();
-
-  const onFullScreenClick = () => {
-    Router.push(`${getOsmappLink(feature)}/climbing${window.location.hash}`);
-  };
-  const cragPhotos = getWikimediaCommonsKeys(feature.tags)
-    .map((key) => feature.tags[key])
-    .map(removeFilePrefix);
-  preparePhotosAndSet(cragPhotos);
-
-  const imageUrl = getCommonsImageUrl(`File:${photoPath}`, 500);
-
-  const onPhotoLoad = () => {
-    loadPhotoRelatedData();
-  };
-
-  const isPhotoLoading = imageSize.height === 0;
   return (
     <>
       <PanelWrapper>
         <PanelScrollbars>
-          {photoPath && imageUrl && (
-            <ThumbnailContainer
-              height={isPhotoLoading ? 200 : imageSize.height}
-            >
-              {isPhotoLoading && (
-                <LoadingContainer>
-                  <CircularProgress />
-                </LoadingContainer>
-              )}
-
-              <Thumbnail
-                src={imageUrl}
-                ref={photoRef}
-                onLoad={onPhotoLoad}
-                $isLoading={isPhotoLoading}
-              />
-
-              {!isPhotoLoading && <RoutesLayer onClick={() => null} />}
-              <ShowFullscreen onClick={onFullScreenClick} />
-            </ThumbnailContainer>
-          )}
-
           <ClimbingParentLink />
 
           <HeadingContainer>
@@ -138,9 +47,12 @@ export const ClimbingPanel = ({ footer, showTagsTable }) => {
 
           <OsmError />
 
+          <Box mt={2}>
+            <ImageSlider />
+          </Box>
+
           <RouteDistribution />
           <RouteList />
-          <ImageSlider />
 
           <div style={{ padding: '35px 15px 5px' }}>
             <Properties showTags={showTagsTable} />
@@ -151,18 +63,6 @@ export const ClimbingPanel = ({ footer, showTagsTable }) => {
           {/* @TODO unite with parent panel */}
           <div style={{ padding: '20px 15px 0 15px' }}>{footer}</div>
         </PanelScrollbars>
-        <DetailButtonContainer>
-          <Button
-            color="primary"
-            size="large"
-            startIcon={<ZoomInIcon fontSize="inherit" />}
-            onClick={onFullScreenClick}
-            fullWidth
-            variant="contained"
-          >
-            Show crag detail
-          </Button>
-        </DetailButtonContainer>
       </PanelWrapper>
     </>
   );
