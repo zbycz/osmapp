3e6d274e040e21d7277e27db3b9161c9059ba7ee
Jan Václavík <jvaclavik@gmail.com>
2024-07-24 23:27:58 +0200

    MyTicks: Add page to Drawer (#421)

---
+++ src/components/utils/MobilePageDrawer.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Drawer } from './Drawer';
+import { PanelWrapper } from './PanelHelpers';
+import { useMobileMode } from '../helpers';
+
+export const DRAWER_PREVIEW_HEIGHT = 86;
+export const DRAWER_TOP_OFFSET = 8;
+
+type MobilePageDrawerProps = {
+  children: React.ReactNode;
+  onClose?: (e: any, open: boolean) => void;
+  className: string;
+  collapsedHeight?: number;
+  topOffset?: number;
+};
+
+export const MobilePageDrawer = ({
+  children,
+  onClose,
+  className,
+  collapsedHeight = DRAWER_PREVIEW_HEIGHT,
+  topOffset = DRAWER_TOP_OFFSET,
+}: MobilePageDrawerProps) => {
+  const isMobileMode = useMobileMode();
+
+  return isMobileMode ? (
+    <Drawer
+      topOffset={topOffset}
+      className={className}
+      collapsedHeight={collapsedHeight}
+      onTransitionEnd={onClose}
+      defaultOpen
+    >
+      {children}
+    </Drawer>
+  ) : (
+    <PanelWrapper>{children}</PanelWrapper>
+  );
+};
