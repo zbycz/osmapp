854a1ff048e2380294c3a49a1a3764398d4a61f6
Pavel Zbytovský <zbytovsky@gmail.com>
2024-07-28 09:48:32 +0200

    fix vercel lib issue 4

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 });
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 const languages = {
   de: 'Deutsch',
   cs: 'Česky',
