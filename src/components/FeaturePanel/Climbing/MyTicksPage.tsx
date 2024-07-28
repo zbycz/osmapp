8629b544289ac9d9438631fdf9c3baa16b08d093
Jan Václavík <jvaclavik@gmail.com>
2024-07-28 23:19:38 +0200

    climbing: Add my ticks heatmap

--- src/components/FeaturePanel/Climbing/MyTicksPage.tsx
+++ src/components/FeaturePanel/Climbing/MyTicksPage.tsx
@@ -8,7 +8,7 @@ import {
   TableCell,
   TableContainer,
   Paper,
-  useTheme,
+  Typography,
 } from '@mui/material';
 import maplibregl from 'maplibre-gl';
 import { t } from '../../../services/intl';
@@ -21,8 +21,8 @@ import {
 } from '../../../services/overpassSearch';
 import { getApiId, getShortId } from '../../../services/helpers';
 import {
-  getDifficulty,
-  getDifficultyColor,
+  // getDifficulty,
+  // getDifficultyColor,
   getRouteGrade,
 } from './utils/grades/routeGrade';
 import { ClosePanelButton } from '../../utils/ClosePanelButton';
@@ -43,13 +43,31 @@ type Marker = {
 };
 
 export const MyTicksPage = () => {
-  const theme = useTheme();
+  // const theme = useTheme();
   const [myTicksData, setMyTicksData] = useState({});
+  const [heatmapData, setHeatmapData] = useState(null);
   const [featureMarkers, setFeatureMarkers] = useState<Array<Marker>>([]);
   const allTicks = getAllTicks();
   const { userSettings } = useUserSettingsContext();
   const map = getGlobalMap();
 
+  const generateGeojson = (features) => {
+    const geojson = {
+      type: 'FeatureCollection',
+      features: features.map((feature) => ({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: feature.center,
+        },
+        properties: {
+          title: feature.tags.name,
+          description: feature.tags.description,
+        },
+      })),
+    };
+    return geojson;
+  };
   const getOverpassData = async () => {
     const queryTicks = allTicks
       .map(({ osmId }) => {
@@ -71,6 +89,8 @@ export const MyTicksPage = () => {
       };
     }, {});
     setMyTicksData(data);
+
+    setHeatmapData(generateGeojson(features));
   };
 
   const deleteMarkers = () => {
@@ -89,114 +109,110 @@ export const MyTicksPage = () => {
   }, []);
 
   useEffect(() => {
-    const newMarkers = allTicks
-      .map((tick) => {
-        const tickFeature = myTicksData[tick.osmId];
-        const routeDifficulty = getDifficulty(tickFeature?.tags);
-        const colorByDifficulty = getDifficultyColor(routeDifficulty, theme);
-        const featureMarker = {
-          color: colorByDifficulty,
-          draggable: false,
-        };
+    // const showMarkers = () => {
+    //   const newMarkers = allTicks
+    //     .map((tick) => {
+    //       const tickFeature = myTicksData[tick.osmId];
+    //       const routeDifficulty = getDifficulty(tickFeature?.tags);
+    //       const colorByDifficulty = getDifficultyColor(routeDifficulty, theme);
+    //       const featureMarker = {
+    //         color: colorByDifficulty,
+    //         draggable: false,
+    //       };
+    //
+    //       if (map && myTicksData[tick.osmId]?.center) {
+    //         const markerRef = new maplibregl.Marker(featureMarker)
+    //           .setLngLat(tickFeature.center)
+    //           .addTo(map);
+    //
+    //         return { ref: markerRef, feature: tickFeature };
+    //       }
+    //       return null;
+    //     })
+    //     .filter((marker) => marker !== null);
+    //
+    //   setFeatureMarkers(newMarkers);
+    // };
 
-        if (map && myTicksData[tick.osmId]?.center) {
-          const markerRef = new maplibregl.Marker(featureMarker)
-            .setLngLat(tickFeature.center)
-            .addTo(map);
-
-          return { ref: markerRef, feature: tickFeature };
-        }
-        return null;
-      })
-      .filter((marker) => marker !== null);
-
-    setFeatureMarkers(newMarkers);
+    // showMarkers();
 
     const enableHeatmap = () => {
       map.on('load', () => {
-        // Add a geojson point source.
-        // Heatmap layers also work with a vector tile source.
         map.addSource('earthquakes', {
           type: 'geojson',
-          data: 'https://maplibre.org/maplibre-gl-js/docs/assets/earthquakes.geojson',
+          data: heatmapData,
         });
 
-        map.addLayer(
-          {
-            id: 'earthquakes-heat',
-            type: 'heatmap',
-            source: 'earthquakes',
-            maxzoom: 9,
-            paint: {
-              // Increase the heatmap weight based on frequency and property magnitude
-              'heatmap-weight': [
-                'interpolate',
-                ['linear'],
-                ['get', 'mag'],
-                0,
-                0,
-                6,
-                1,
-              ],
-              // Increase the heatmap color weight weight by zoom level
-              // heatmap-intensity is a multiplier on top of heatmap-weight
-              'heatmap-intensity': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                0,
-                1,
-                9,
-                3,
-              ],
-              // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-              // Begin color ramp at 0-stop with a 0-transparency color
-              // to create a blur-like effect.
-              'heatmap-color': [
-                'interpolate',
-                ['linear'],
-                ['heatmap-density'],
-                0,
-                'rgba(33,102,172,0)',
-                0.2,
-                'rgb(103,169,207)',
-                0.4,
-                'rgb(209,229,240)',
-                0.6,
-                'rgb(253,219,199)',
-                0.8,
-                'rgb(239,138,98)',
-                1,
-                'rgb(178,24,43)',
-              ],
-              // Adjust the heatmap radius by zoom level
-              'heatmap-radius': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                0,
-                2,
-                9,
-                20,
-              ],
-              // Transition from heatmap to circle layer by zoom level
-              'heatmap-opacity': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                7,
-                1,
-                9,
-                0,
-              ],
-            },
+        map.addLayer({
+          id: 'earthquakes-heat',
+          type: 'heatmap',
+          source: 'earthquakes',
+          paint: {
+            // Zvýraznění heatmapy podle intenzity
+            'heatmap-weight': [
+              'interpolate',
+              ['linear'],
+              ['get', 'mag'],
+              0,
+              0,
+              6,
+              1,
+            ],
+            // Nastavení barevné škály heatmapy
+            'heatmap-color': [
+              'interpolate',
+              ['linear'],
+              ['heatmap-density'],
+              0,
+              'rgba(33,102,172,0)',
+              0.2,
+              'rgb(103,169,207)',
+              0.4,
+              'rgb(209,229,240)',
+              0.6,
+              'rgb(253,219,199)',
+              0.8,
+              'rgb(239,138,98)',
+              1,
+              'rgb(178,24,43)',
+            ],
+            // Nastavení intenzity heatmapy
+            'heatmap-intensity': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              0,
+              1,
+              9,
+              3,
+            ],
+            // Nastavení poloměru heatmapových bodů
+            'heatmap-radius': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              0,
+              2,
+              9,
+              15,
+            ],
+            // Snížení opacity heatmapy ve vyšších zoomech
+            // 'heatmap-opacity': 0.8,
+            // 'heatmap-opacity': [
+            //   'interpolate',
+            //   ['linear'],
+            //   ['zoom'],
+            //   13,
+            //   1,
+            //   15,
+            //   0,
+            // ],
           },
-          'waterway',
-        );
+        });
       });
     };
 
-    if (map) {
+    if (map && heatmapData) {
       enableHeatmap();
     }
   }, [myTicksData, map]);
@@ -215,41 +231,58 @@ export const MyTicksPage = () => {
             <PanelSidePadding>
               <h1>{t('my_ticks.title')}</h1>
             </PanelSidePadding>
-            <TableContainer component={Paper}>
-              <Table size="small">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>{t('my_ticks.route_name')}</TableCell>
-                    <TableCell>{t('my_ticks.route_grade')}</TableCell>
-                    <TableCell>{t('my_ticks.route_style')}</TableCell>
-                    <TableCell>{t('my_ticks.route_date')}</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {allTicks.map((tick, index) => {
-                    const tickFeature = myTicksData[tick.osmId];
-                    const name = tickFeature?.tags?.name;
-                    const grade = getRouteGrade(
-                      tickFeature?.tags,
-                      userSettings['climbing.gradeSystem'],
-                    );
+            {allTicks.length === 0 ? (
+              <PanelSidePadding>
+                <Typography variant="body1" gutterBottom>
+                  You have no ticks so far…
+                </Typography>
 
-                    return (
-                      <TickRow
-                        key={`${tick.osmId}-${tick.date}`}
-                        name={name}
-                        grade={grade}
-                        gradeSystem={userSettings['climbing.gradeSystem']}
-                        tick={tick}
-                        index={index}
-                        isNameVisible
-                        isReadOnly
-                      />
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
+                <Typography
+                  variant="caption"
+                  display="block"
+                  gutterBottom
+                  color="secondary"
+                >
+                  Try to add one on the crag
+                </Typography>
+              </PanelSidePadding>
+            ) : (
+              <TableContainer component={Paper}>
+                <Table size="small">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>{t('my_ticks.route_name')}</TableCell>
+                      <TableCell>{t('my_ticks.route_grade')}</TableCell>
+                      <TableCell>{t('my_ticks.route_style')}</TableCell>
+                      <TableCell>{t('my_ticks.route_date')}</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {allTicks.map((tick, index) => {
+                      const tickFeature = myTicksData[tick.osmId];
+                      const name = tickFeature?.tags?.name;
+                      const grade = getRouteGrade(
+                        tickFeature?.tags,
+                        userSettings['climbing.gradeSystem'],
+                      );
+
+                      return (
+                        <TickRow
+                          key={`${tick.osmId}-${tick.date}`}
+                          name={name}
+                          grade={grade}
+                          gradeSystem={userSettings['climbing.gradeSystem']}
+                          tick={tick}
+                          index={index}
+                          isNameVisible
+                          isReadOnly
+                        />
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            )}
           </PanelScrollbars>
         </PanelContent>
       </MobilePageDrawer>
