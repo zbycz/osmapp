f60b4998cf30f75ff831a3e80418c3e5a9379164
Jan Václavík <jvaclavik@gmail.com>
2024-07-30 20:05:37 +0200

    MyTicks: Refactor TickRow (#429)

--- src/components/FeaturePanel/Climbing/TickRow.tsx
+++ src/components/FeaturePanel/Climbing/TickRow.tsx
@@ -9,43 +9,24 @@ import {
   TableRow,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import Router from 'next/router';
-import styled from 'styled-components';
 import {
   onTickDelete,
   onTickUpdate,
   tickStyles,
 } from '../../../services/ticks';
-import { getApiId, getUrlOsmId } from '../../../services/helpers';
 import { DEFAULT_DATA_FORMAT } from '../../../config';
 import { RouteDifficultyBadge } from './RouteDifficultyBadge';
 import { Tick } from './types';
 import { GradeSystem } from './utils/grades/gradeData';
 
-const Name = styled.a`
-  flex: 1;
-`;
-
 type TickRowProps = {
-  name?: string;
   grade?: GradeSystem;
   gradeSystem?: string;
   tick: Tick;
   index: number;
-  isNameVisible?: boolean;
-  isReadOnly?: boolean;
 };
 
-export const TickRow = ({
-  name,
-  grade,
-  gradeSystem,
-  tick,
-  index,
-  isNameVisible = false,
-  isReadOnly = false,
-}: TickRowProps) => {
-  const apiId = getApiId(tick.osmId);
+export const TickRow = ({ grade, gradeSystem, tick, index }: TickRowProps) => {
   const deleteTick = (deteledIndex) => {
     onTickDelete({ osmId: tick.osmId, index: deteledIndex });
   };
@@ -58,10 +39,6 @@ export const TickRow = ({
     });
   };
 
-  const onNameClick = () => {
-    Router.push(`/${getUrlOsmId(apiId)}`);
-  };
-
   const formattedDate = tick.date ? format(tick.date, DEFAULT_DATA_FORMAT) : '';
 
   const routeDifficulty = {
@@ -71,11 +48,6 @@ export const TickRow = ({
 
   return (
     <TableRow>
-      {isNameVisible && (
-        <TableCell>
-          <Name onClick={onNameClick}>{name || tick.osmId}</Name>
-        </TableCell>
-      )}
       {grade && gradeSystem && (
         <TableCell>
           <RouteDifficultyBadge routeDifficulty={routeDifficulty} />
@@ -83,30 +55,24 @@ export const TickRow = ({
       )}
       <TableCell>
         <FormControl size="small">
-          {isReadOnly ? (
-            tick.style
-          ) : (
-            <Select
-              value={tick.style}
-              size="small"
-              variant="standard"
-              onChange={(e) => onTickStyleChange(e, index)}
-            >
-              {tickStyles.map((tickStyle) => (
-                <MenuItem value={tickStyle.key}>{tickStyle.name}</MenuItem>
-              ))}
-            </Select>
-          )}
+          <Select
+            value={tick.style}
+            size="small"
+            variant="standard"
+            onChange={(e) => onTickStyleChange(e, index)}
+          >
+            {tickStyles.map((tickStyle) => (
+              <MenuItem value={tickStyle.key}>{tickStyle.name}</MenuItem>
+            ))}
+          </Select>
         </FormControl>
       </TableCell>
       <TableCell>{formattedDate}</TableCell>
-      {!isReadOnly && (
-        <TableCell>
-          <IconButton size="small" onClick={() => deleteTick(index)}>
-            <DeleteIcon />
-          </IconButton>
-        </TableCell>
-      )}
+      <TableCell>
+        <IconButton size="small" onClick={() => deleteTick(index)}>
+          <DeleteIcon />
+        </IconButton>
+      </TableCell>
     </TableRow>
   );
 };
---
+++ src/components/MyTicksPanel/useSortedTable.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
+import { TickRowType } from '../../services/my-ticks/getMyTicks';
+import { t } from '../../services/intl';
+
+function descendingComparator(
+  a: TickRowType,
+  b: TickRowType,
+  orderBy: OrderKey,
+) {
+  return a[orderBy]?.localeCompare(b[orderBy]) ?? 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator(
+  order: Order,
+  orderBy: OrderKey,
+): (a: TickRowType, b: TickRowType) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+type OrderKey = 'name' | 'grade' | 'style' | 'date';
+
+interface HeadCell {
+  id: OrderKey;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'name',
+    numeric: false,
+    label: t('my_ticks.route_name'),
+  },
+  {
+    id: 'grade',
+    numeric: false,
+    label: t('my_ticks.route_grade'),
+  },
+  {
+    id: 'style',
+    numeric: true,
+    label: t('my_ticks.route_style'),
+  },
+  {
+    id: 'date',
+    numeric: true,
+    label: t('my_ticks.route_date'),
+  },
+];
+
+interface EnhancedTableProps {
+  onRequestSort: (event: React.MouseEvent<unknown>, property: OrderKey) => void;
+  order: Order;
+  orderBy: string;
+}
+
+function EnhancedTableHead({
+  order,
+  orderBy,
+  onRequestSort,
+}: EnhancedTableProps) {
+  const createSortHandler =
+    (property: OrderKey) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+export const useSortedTable = (tickRows: TickRowType[]) => {
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<OrderKey>('date');
+
+  const handleRequestSort = (
+    _: React.MouseEvent<unknown>,
+    property: OrderKey,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+  const visibleRows = React.useMemo(
+    () => tickRows.sort(getComparator(order, orderBy)),
+    [order, orderBy, tickRows],
+  );
+
+  const tableHeader = (
+    <EnhancedTableHead
+      order={order}
+      orderBy={orderBy}
+      onRequestSort={handleRequestSort}
+    />
+  );
+  return { visibleRows, tableHeader };
+};
