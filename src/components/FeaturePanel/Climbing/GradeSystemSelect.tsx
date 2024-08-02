ef001e5b1abba2c01ab7fb491ac80f52aece3c18
Jan Václavík <jvaclavik@gmail.com>
2024-08-02 22:13:47 +0200

    climbing: Add grading description (#432)

--- src/components/FeaturePanel/Climbing/GradeSystemSelect.tsx
+++ src/components/FeaturePanel/Climbing/GradeSystemSelect.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { Select, MenuItem } from '@mui/material';
+import { Select, MenuItem, Tooltip } from '@mui/material';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import styled from 'styled-components';
 import { GRADE_SYSTEMS, GradeSystem } from './utils/grades/gradeData';
 
 type Props = {
@@ -8,6 +10,17 @@ type Props = {
   setGradeSystem: (GradeSystem: GradeSystem) => void;
   onClick?: (e: any) => void;
 };
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
+  width: 100%;
+`;
+const StyledInfoOutlinedIcon = styled(InfoOutlinedIcon)`
+  position: relative;
+  top: 2px;
+`;
 
 export const GradeSystemSelect = ({
   selectedGradeSystem,
@@ -23,8 +36,15 @@ export const GradeSystemSelect = ({
       setGradeSystem(event.target.value);
     }}
   >
-    {GRADE_SYSTEMS.map(({ key, name }) => (
-      <MenuItem value={key}>{name}</MenuItem>
+    {GRADE_SYSTEMS.map(({ key, name, description }) => (
+      <MenuItem value={key}>
+        <Row>
+          <div>{name}</div>
+          <Tooltip arrow title={description} placement="right">
+            <StyledInfoOutlinedIcon fontSize="small" color="secondary" />
+          </Tooltip>
+        </Row>
+      </MenuItem>
     ))}
   </Select>
 );
