e389035c6449c33c35b10976f26dbb586a22f06f
Pavel Zbytovský <zbytovsky@gmail.com>
2024-08-15 16:09:39 +0200

    refactor methods outside of component

--- src/components/FeaturePanel/Runways/loadRunways.ts
+++ src/components/FeaturePanel/Runways/loadRunways.ts
@@ -1,5 +1,6 @@
 import { fetchText } from '../../../services/fetch';
 import { OsmApiId } from '../../../services/helpers';
+import { encodeUrl } from '../../../helpers/utils';
 
 export type Runway = {
   id: string;
@@ -10,39 +11,38 @@ export type Runway = {
   surface?: string;
 };
 
-export async function loadRunways({ id, type }: OsmApiId) {
-  const isNode = type === 'node';
+const getNodeQuery = ({ id }: OsmApiId) =>
+  `[out:csv(::id, ::type, ref, width, length, surface; false; '||')];
+    node(${id}) -> .airport;
+    way
+      [aeroway=runway]
+      (around.airport:100);
+    out;`;
 
-  const nodeQuery = `[out:csv(::id, ::type, ref, width, length, surface; false; '||')];
-  node(${id}) -> .airport;
-  way
-    [aeroway=runway]
-    (around.airport:100);
-  out;`;
-  const relationWayQuery = `[out:csv(::id, ::type, ref, width, length, surface; false; '||')];
-  ${type}(${id});
-  map_to_area;
-  way
-    [aeroway=runway]
-    (area);
-  out;`;
+const getWayOrRelationQuery = ({ id, type }: OsmApiId) =>
+  `[out:csv(::id, ::type, ref, width, length, surface; false; '||')];
+    ${type}(${id});
+    map_to_area;
+    way
+      [aeroway=runway]
+      (area);
+    out;`;
 
-  const query = isNode ? nodeQuery : relationWayQuery;
-
-  const response: string = await fetchText(
-    `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`,
-  );
+export async function loadRunways(apiId: OsmApiId) {
+  const isNode = apiId.type === 'node';
+  const query = isNode ? getNodeQuery(apiId) : getWayOrRelationQuery(apiId);
+  const url = encodeUrl`https://overpass-api.de/api/interpreter?data=${query}`;
+  const response: string = await fetchText(url);
 
   return response
     .split('\n')
     .slice(0, -1)
     .map((line): Runway => {
-      const [localId, localType, ref, width, length, surface] =
-        line.split('||');
+      const [id, type, ref, width, length, surface] = line.split('||');
 
       return {
-        id: localId,
-        type: localType,
+        id,
+        type,
         ref: ref || undefined,
         width: width || undefined,
         length: length || undefined,
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * Encodes each variable in a template string with `encodeURIComponent`.
+ * eg. encodeUrl`https://overpass-api.de/api/interpreter?data=${query}`
+ *  -> becomes: `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`
+ */
 export const encodeUrl = (
   strings: TemplateStringsArray,
   ...values: (string | number)[]
