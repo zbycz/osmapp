c374f01644024cefbd95e17498aabff8e6d1b93e
Pavel Zbytovský <zbytovsky@gmail.com>
2024-08-20 15:50:50 +0200

    osmApi: proper 404, add types, use ApiId

--- src/components/App/helpers.ts
+++ src/components/App/helpers.ts
@@ -5,14 +5,16 @@ import { clearFeatureCache, fetchFeature } from '../../services/osmApi';
 import { fetchJson } from '../../services/fetch';
 import { isServer } from '../helpers';
 import { getCoordsFeature } from '../../services/getCoordsFeature';
-import { getApiId, getOsmappLink } from '../../services/helpers';
-import { Feature } from '../../services/types';
+import { getOsmappLink, getShortId } from '../../services/helpers';
+import { Feature, OsmId } from '../../services/types';
+import { View } from '../utils/MapStateContext';
+import { NextPageContext } from 'next';
 
-const DEFAULT_VIEW = [4, 50, 14];
+const DEFAULT_VIEW: View = ['4', '50', '14'];
 
-const isLocalhost = (ip) => ['127.0.0.1', '::1'].includes(ip);
+const isLocalhostOrNgnix = (ip) => ['127.0.0.1', '::1'].includes(ip);
 
-const getViewFromIp = async (ip) => {
+const getViewFromIp = async (ip: string): Promise<View> => {
   try {
     // TODO Currently we dont do rate limiting on our side #83
     // 45 requests per minute from an IP address https://ip-api.com/docs/api:json
@@ -20,7 +22,7 @@ const getViewFromIp = async (ip) => {
     const { status, lat, lon } = await fetchJson(url);
 
     if (status === 'success') {
-      return lat && lon ? [7, lat, lon] : null;
+      return lat && lon ? ['7', lat, lon] : null;
     }
 
     return null;
@@ -31,20 +33,29 @@ const getViewFromIp = async (ip) => {
   }
 };
 
-export const getViewFromRequest = async (req) => {
+export const getViewFromRequest = async (
+  req: NextPageContext['req'],
+): Promise<View> => {
   const remoteIp = req.connection.remoteAddress;
-  const fwdIp = (req.headers['x-forwarded-for'] || '').split(',')[0].trim(); // ngnix: proxy_set_header X-Forwarded-For $remote_addr;
-  const ip = !isLocalhost(remoteIp) ? remoteIp : fwdIp;
+  const fwdIp = ((req.headers['x-forwarded-for'] as string) || '') // ngnix: proxy_set_header X-Forwarded-For $remote_addr;
+    .split(',')[0]
+    .trim();
+  const ip = isLocalhostOrNgnix(remoteIp) ? fwdIp : remoteIp;
   const view = ip ? await getViewFromIp(ip) : null;
   return view ?? DEFAULT_VIEW;
 };
 
-export const getInitialMapView = async (ctx) => {
-  const { mapView } = nextCookies(ctx);
-  return mapView ? mapView.split('/') : getViewFromRequest(ctx.req);
+export const getInitialMapView = async (
+  ctx: NextPageContext,
+): Promise<View> => {
+  const cookies = nextCookies(ctx);
+  const viewCookie = cookies.mapView;
+  return viewCookie
+    ? (viewCookie.split('/') as View)
+    : getViewFromRequest(ctx.req);
 };
 
-const saveLastUrl = (feature: Feature, ctx?) => {
+const saveLastUrl = (feature: Feature, ctx?: NextPageContext) => {
   const url = getOsmappLink(feature);
   if (ctx?.res) {
     ctx.res.setHeader('Set-Cookie', `last-url=${url}; Path=/; Max-Age=86400`);
@@ -53,48 +64,51 @@ const saveLastUrl = (feature: Feature, ctx?) => {
   }
 };
 
-export const getInitialFeature = async (ctx) => {
-  const { all, id } = ctx.query;
+const getValidApiId = (path: string[]): OsmId => {
+  const [type, id] = path;
+  if (type?.match(/^node|way|relation$/) && id?.match(/^\d+$/)) {
+    return { type: path[0], id: parseInt(path[1], 10) } as OsmId;
+  }
+  return null;
+};
+export const getInitialFeature = async (
+  ctx: NextPageContext,
+): Promise<Feature | '404' | null> => {
+  const path = ctx.query.all as string[];
 
-  // url: /
-  if (!all) {
+  // url: "/"
+  if (!path) {
     return null;
   }
 
-  if (all[0].match(/^[-.0-9]+,[-.0-9]+$/)) {
-    const [lat, lon] = all[0].split(',');
+  if (path[0].match(/^[-.0-9]+,[-.0-9]+$/)) {
+    const [lat, lon] = path[0].split(',');
     const coordsFeature = getCoordsFeature([lon, lat]);
     saveLastUrl(coordsFeature, ctx);
     return coordsFeature;
   }
 
-  const [osmtype, osmid] = all;
-  const shortId =
-    osmtype && osmtype.match(/^node|way|relation$/)
-      ? osmtype.substr(0, 1) + osmid
-      : id;
-
-  if (!shortId) {
-    // TODO 404 when !shortId
-    return null;
+  const apiId = getValidApiId(path);
+  if (!apiId) {
+    return '404';
   }
 
   if (isServer()) {
     // we need always fresh OSM element, b/c user can edit a feature and refresh the page
     // (other requests like overpass may be cached)
-    clearFeatureCache(getApiId(shortId));
+    clearFeatureCache(apiId);
   }
 
   const t1 = new Date().getTime();
 
-  const initialFeature = await fetchFeature(shortId);
+  const initialFeature = await fetchFeature(apiId);
   saveLastUrl(initialFeature, ctx);
 
   const t2 = new Date().getTime();
   const time = t2 - t1;
-  console.log(`getInitialFeature()`, shortId, `${time}ms`); // eslint-disable-line no-console
+  console.log(`getInitialFeature()`, getShortId(apiId), `${time}ms`); // eslint-disable-line no-console
   Sentry.metrics.distribution('getInitialFeature', time, {
-    tags: { osmtype, osmid, ssr: isServer() },
+    tags: { ...apiId, ssr: isServer() },
     unit: 'millisecond',
   });
 
