4e6b04c2c0cb4f3f848f53040d140391d3125c22
Pavel Zbytovský <zbytovsky@gmail.com>
2024-08-30 11:12:51 +0200

    remove unneccesary deps

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -6,24 +6,12 @@ import { getExifData } from '../../src/server/upload/getExifData';
 import { uploadToWikimediaCommons } from '../../src/server/upload/uploadToWikimediaCommons';
 import { getApiId } from '../../src/services/helpers';
 import { File } from '../../src/server/upload/types';
-import * as fs from 'node:fs';
-import { Readable } from 'stream';
-import { finished } from 'stream/promises';
 import { setProjectForSSR } from '../../src/services/project';
+import { fetchToFile } from '../../src/server/upload/fetchToFile';
 
 // inspiration: https://commons.wikimedia.org/wiki/File:Drive_approaching_the_Grecian_Lodges_-_geograph.org.uk_-_5765640.jpg
 // https://github.com/multichill/toollabs/blob/master/bot/commons/geograph_uploader.py
 // TODO https://commons.wikimedia.org/wiki/Template:Geograph_from_structured_data
-
-const fetchToFile = async (url: string) => {
-  const filepath = `/tmp/${Math.random()}`;
-  const response = await fetch(url);
-  const inStream = Readable.fromWeb(response.body as unknown as any);
-  const outStream = fs.createWriteStream(filepath);
-  await finished(inStream.pipe(outStream));
-
-  return filepath;
-};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
