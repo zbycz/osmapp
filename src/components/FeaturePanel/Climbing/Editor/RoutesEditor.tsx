1132d1010ba791ce018b84b00dc4c36846852969
Jan Václavík <jvaclavik@gmail.com>
2024-09-01 22:15:49 +0200

    RouteEditor: Preload other images (#495)

--- src/components/FeaturePanel/Climbing/Editor/RoutesEditor.tsx
+++ src/components/FeaturePanel/Climbing/Editor/RoutesEditor.tsx
@@ -6,6 +6,7 @@ import { updateElementOnIndex } from '../utils/array';
 import { PositionPx } from '../types';
 import { getPositionInImageFromMouse } from '../utils/mousePositionUtils';
 import { GalleryControls } from './GalleryControls';
+import { getCommonsImageUrl } from '../../../../services/images/getCommonsImageUrl';
 
 const EditorContainer = styled.div<{ imageHeight: number }>`
   display: flex;
@@ -61,6 +62,7 @@ export const RoutesEditor = ({
     photoPath,
     setLoadedPhotos,
     photoZoom,
+    photoPaths,
   } = useClimbingContext();
   const machine = getMachine();
   const [transformOrigin] = useState({ x: 0, y: 0 }); // @TODO remove ?
@@ -125,12 +127,33 @@ export const RoutesEditor = ({
     onMove(positionInImage);
   };
 
+  const preloadOtherPhotos = () => {
+    const photosToLoad = photoPaths.filter((path) => !loadedPhotos[path]);
+
+    const tempLoadedPhotos = photosToLoad.reduce((acc, otherPhotoPath) => {
+      const img = new Image();
+      const url = getCommonsImageUrl(`File:${otherPhotoPath}`, photoResolution);
+      img.src = url;
+
+      return {
+        ...acc,
+        [otherPhotoPath]: {
+          ...acc[otherPhotoPath],
+          [photoResolution]: true,
+        },
+      };
+    }, loadedPhotos);
+
+    setLoadedPhotos(tempLoadedPhotos);
+  };
+
   const onPhotoLoad = () => {
     setLoadedPhotos({
       ...loadedPhotos,
       [photoPath]: { ...loadedPhotos[photoPath], [photoResolution]: true },
     });
     loadPhotoRelatedData();
+    preloadOtherPhotos();
   };
 
   return (
