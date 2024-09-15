9a217847e2d20596c25dfc6d028173d23306a114
Jan Václavík <jvaclavik@gmail.com>
2024-09-15 22:35:49 +0200

    CragDialog: Delete useEffect in CragMap

--- src/components/FeaturePanel/Climbing/CragMap.tsx
+++ src/components/FeaturePanel/Climbing/CragMap.tsx
@@ -1,15 +1,31 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useState } from 'react';
 import maplibregl, { GeoJSONSource } from 'maplibre-gl';
 import { outdoorStyle } from '../../Map/styles/outdoorStyle';
 import { COMPASS_TOOLTIP } from '../../Map/useAddTopRightControls';
-import { PersistedScaleControl } from '../../Map/behaviour/PersistedScaleControl';
 import styled from '@emotion/styled';
 import { useFeatureContext } from '../../utils/FeatureContext';
 import type { LayerSpecification } from '@maplibre/maplibre-gl-style-spec';
-import { useMobileMode } from '../../helpers';
+import { CircularProgress } from '@mui/material';
+
+const Map = styled.div<{ $isVisible: boolean }>`
+  visibility: ${({ $isVisible }) => ($isVisible ? 'visible' : 'hidden')};
+  height: 100%;
+  width: 100%;
+`;
+
+const LoadingContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Container = styled.div`
+  width: 100%;
   height: 100%;
+  position: relative;
 `;
 
 const emptyGeojson = {
@@ -25,7 +41,6 @@ export const routes: LayerSpecification[] = [
     id: 'climbing-3-routes-circle',
     type: 'circle',
     source: 'climbing',
-
     paint: {
       'circle-color': [
         'case',
@@ -44,7 +59,7 @@ export const routes: LayerSpecification[] = [
       'text-padding': 2,
       'text-font': ['Noto Sans Medium'],
       'text-anchor': 'left',
-      'text-field': '{name} {climbing:grade:uiaa}',
+      'text-field': '{name} {grade}',
       'text-offset': [1, 0],
       'text-size': 14,
       'text-max-width': 9,
@@ -60,41 +75,32 @@ export const routes: LayerSpecification[] = [
   },
 ];
 
-const geolocation = new maplibregl.GeolocateControl({
-  positionOptions: {
-    enableHighAccuracy: true,
-  },
-  fitBoundsOptions: {
-    duration: 4000,
-  },
-  trackUserLocation: true,
-});
-
-const navigation = {
-  mobile: new maplibregl.NavigationControl({
-    showZoom: false,
-    showCompass: true,
-    visualizePitch: true,
-  }),
-  desktop: new maplibregl.NavigationControl({
-    showZoom: true,
-    showCompass: true,
-    visualizePitch: true,
-  }),
-};
-
 const useInitMap = () => {
-  const mapRef = React.useRef(null);
-  const mobileMode = useMobileMode();
-  const [mapInState, setMapInState] = React.useState(null);
+  const containerRef = React.useRef(null);
+  const mapRef = React.useRef<maplibregl.Map>(null);
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
+  const { feature } = useFeatureContext();
 
-  const navControl = mobileMode ? navigation.mobile : navigation.desktop;
+  const getClimbingSource = useCallback(
+    () => mapRef.current.getSource('climbing') as GeoJSONSource | undefined,
+    [],
+  );
 
   React.useEffect(() => {
-    if (!mapRef.current) return undefined;
+    const geolocation = new maplibregl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      fitBoundsOptions: {
+        duration: 4000,
+      },
+      trackUserLocation: true,
+    });
 
+    setIsMapLoaded(false);
+    if (!containerRef.current) return undefined;
     const map = new maplibregl.Map({
-      container: mapRef.current,
+      container: containerRef.current,
       style: {
         ...outdoorStyle,
         layers: [...outdoorStyle.layers, ...routes],
@@ -106,30 +112,44 @@ const useInitMap = () => {
         'NavigationControl.ResetBearing': COMPASS_TOOLTIP,
       },
     });
-    setMapInState(map);
-
-    map.addControl(navControl);
-    map.addControl(PersistedScaleControl as any);
-    map.addControl(geolocation);
 
     map.scrollZoom.setWheelZoomRate(1 / 200); // 1/450 is default, bigger value = faster
+    map.addControl(geolocation);
+    mapRef.current = map;
+
+    mapRef.current.on('load', () => {
+      setIsMapLoaded(true);
+      if (mapRef.current) {
+        mapRef.current.jumpTo({
+          center: feature.center as [number, number],
+          zoom: 18.5,
+        });
+
+        getClimbingSource()?.setData({
+          type: 'FeatureCollection' as const,
+          features: transformMemberFeaturesToGeojson(feature.memberFeatures),
+        });
+      }
+    });
 
     return () => {
       if (map) {
         map.remove();
-        map.removeControl(navControl);
-        map.removeControl(geolocation);
       }
     };
-  }, [mapRef, navControl]);
+  }, [containerRef, feature.center, feature.memberFeatures, getClimbingSource]);
 
-  return [mapInState, mapRef];
+  return { containerRef, isMapLoaded };
 };
 
 const transformMemberFeaturesToGeojson = (features) => {
   return features.map((feature) => ({
     ...feature,
-    properties: { ...feature.properties, name: feature.tags.name },
+    properties: {
+      ...feature.properties,
+      name: feature.tags.name,
+      grade: feature.tags['climbing:grade:uiaa'],
+    },
     geometry:
       feature.osmMeta.type === 'node'
         ? { coordinates: feature.center, type: 'Point' }
@@ -138,30 +158,18 @@ const transformMemberFeaturesToGeojson = (features) => {
 };
 
 const CragMap = () => {
-  const [map, mapRef] = useInitMap();
-  const { feature } = useFeatureContext();
-
-  const getClimbingSource = useCallback(
-    () => map.getSource('climbing') as GeoJSONSource | undefined,
-    [map],
-  );
-
-  useEffect(() => {
-    if (map) {
-      map.jumpTo({ center: feature.center, zoom: 19 });
-
-      getClimbingSource()?.setData({
-        type: 'FeatureCollection' as const,
-        features: transformMemberFeaturesToGeojson(feature.memberFeatures),
-      });
-    }
-  }, [feature.center, feature.memberFeatures, getClimbingSource, map]);
+  const { containerRef, isMapLoaded } = useInitMap();
 
   return (
     <Container>
-      <div ref={mapRef} style={{ height: '100%', width: '100%' }} />
+      {!isMapLoaded && (
+        <LoadingContainer>
+          <CircularProgress color="primary" />
+        </LoadingContainer>
+      )}
+      <Map $isVisible={isMapLoaded} ref={containerRef} />
     </Container>
   );
 };
 
-export default CragMap;
+export default CragMap; // dynamic import
