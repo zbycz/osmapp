ec8499264536710d3683933e758af379221fcc45
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-09-19 16:44:42 +0200

    Refactoring and display routes on the map

--- src/components/FeaturePanel/PublicTransport/requestRoutes.ts
+++ src/components/FeaturePanel/PublicTransport/requestRoutes.ts
@@ -1,8 +1,10 @@
+import groupBy from 'lodash/groupBy';
 import { fetchJson } from '../../../services/fetch';
 import {
   getOverpassUrl,
   overpassGeomToGeojson,
 } from '../../../services/overpassSearch';
+import { intl } from '../../../services/intl';
 
 export interface LineInformation {
   ref: string;
@@ -11,6 +13,26 @@ export interface LineInformation {
   osmType: string;
   osmId: string;
 }
+
+type WithTags = { tags: Record<string, string> };
+
+const filterRoutesByRef = (routes: WithTags[], ref: string) =>
+  routes.filter(({ tags }) => tags.ref === ref);
+
+const getTagValue = (
+  key: string,
+  tags: Record<string, string>,
+  routes: WithTags[],
+) => {
+  if (tags[key]) {
+    return tags[key];
+  }
+  const altElement = routes.find(({ tags }) => tags[key]);
+  if (altElement) {
+    return altElement[key];
+  }
+  return undefined;
+};
 
 export async function requestLines(featureType: string, id: number) {
   const overpassQuery = `[out:json];
@@ -25,29 +47,37 @@ export async function requestLines(featureType: string, id: number) {
       rel(bn.specific_feature)["route"~"bus|train|tram|subway|light_rail|ferry|monorail"];
     ) -> .routes;
     // Get the master relation
-    rel(br.routes);
+    (
+      .routes;
+      rel(br.routes);
+    );
     out geom qt;`;
 
   const overpassGeom = await fetchJson(getOverpassUrl(overpassQuery));
-  const geoJsonFeatures = overpassGeomToGeojson(overpassGeom);
+  const grouped = groupBy(overpassGeom.elements, ({ tags }) => tags.type);
+  const routeMasters = grouped.route_master || [];
+  const routes = grouped.route || [];
+
+  const geoJsonFeatures = overpassGeomToGeojson({ elements: routes });
 
   const geoJson: GeoJSON.GeoJSON = {
     type: 'FeatureCollection',
     features: geoJsonFeatures,
   };
 
-  const allRoutes = geoJsonFeatures
-    .map(
-      ({ properties, osmMeta }): LineInformation => ({
-        ref: `${properties.ref || properties.name}`,
-        colour: properties.colour?.toString() || undefined,
-        service: properties.service?.toString() || undefined,
-        osmId: `${osmMeta.id}`,
-        osmType: osmMeta.type,
-      }),
-    )
-    .sort((a, b) => a.ref.localeCompare(b.ref, undefined, { numeric: true }))
-    .filter(({ ref }) => ref !== '');
+  const allRoutes = routeMasters
+    .map(({ type, id, tags }) => {
+      const directionRouteTags = filterRoutesByRef(routes, tags.ref);
+
+      return {
+        ref: `${tags.ref || tags.name}`,
+        colour: getTagValue('colour', tags, directionRouteTags),
+        service: getTagValue('service', tags, directionRouteTags),
+        osmId: `${id}`,
+        osmType: type,
+      };
+    })
+    .sort((a, b) => a.ref.localeCompare(b.ref, intl.lang, { numeric: true }));
 
   return {
     geoJson,
