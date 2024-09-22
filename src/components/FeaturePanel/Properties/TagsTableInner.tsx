2ddaab8a50c60e277e2f10dade0c875c6537e6fc
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-09-22 18:57:57 +0200

    FeaturePanel: Id tagging scheme internationalization (fixes) (#580)

--- src/components/FeaturePanel/Properties/TagsTableInner.tsx
+++ src/components/FeaturePanel/Properties/TagsTableInner.tsx
@@ -6,7 +6,6 @@ import { InlineEditButton } from '../helpers/InlineEditButton';
 import { buildAddress } from '../../../services/helpers';
 import { ToggleButton } from '../helpers/ToggleButton';
 import { renderValue } from './renderValue';
-import { Position } from '../../../services/types';
 
 const isAddr = (k) => k.match(/^addr:|uir_adr|:addr/);
 const isName = (k) => k.match(/^name(:|$)/);
@@ -60,17 +59,7 @@ const TagsGroup = ({ tags, label, value, hideArrow = false }) => {
 
 // TODO make it dynamic - count how many "first parts before :" are there, and group all >= 2
 
-type TagsTableInnerProps = {
-  tags: Record<string, string>;
-  center: Position;
-  except?: string[];
-};
-
-export const TagsTableInner = ({
-  tags,
-  center,
-  except = [],
-}: TagsTableInnerProps) => {
+export const TagsTableInner = ({ tags, center, except = [] }) => {
   const tagsEntries = Object.entries(tags).filter(([k]) => !except.includes(k));
 
   const addrs = tagsEntries.filter(([k]) => isAddr(k));
--- src/components/FeaturePanel/Properties/renderValue.tsx
+++ src/components/FeaturePanel/Properties/renderValue.tsx
@@ -15,7 +15,7 @@ const getEllipsisHumanUrl = (humanUrl) => {
   });
 };
 
-const getHumanValue = (k: string, v: string, featured: boolean) => {
+const getHumanValue = (k, v, featured: boolean) => {
   const humanValue = v.replace(/^https?:\/\//, '').replace(/^([^/]+)\/$/, '$1');
 
   if (v.startsWith('https://commons.wikimedia.org/wiki/')) {
@@ -42,7 +42,7 @@ const getHumanValue = (k: string, v: string, featured: boolean) => {
   return humanValue;
 };
 
-export const renderValue = (k: string, v: string, featured = false) => {
+export const renderValue = (k, v, featured = false) => {
   const url = getUrlForTag(k, v);
   const humanValue = getHumanValue(k, v, featured);
 
