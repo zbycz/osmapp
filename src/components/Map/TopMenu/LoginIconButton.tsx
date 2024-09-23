5e498db805920cd9740a5e17581ffd3a796ceda5
Pavel Zbytovský <zbytovsky@gmail.com>
2024-09-23 10:55:17 +0200

    UserMenu: show loading indicator while logging in

--- src/components/Map/TopMenu/LoginIconButton.tsx
+++ src/components/Map/TopMenu/LoginIconButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { IconButton } from '@mui/material';
 import { useOsmAuthContext } from '../../utils/OsmAuthContext';
-import { isMobileMode } from '../../helpers';
+import { DotLoader, isMobileMode } from '../../helpers';
 
 const StyledIconButton = styled(IconButton)`
   padding: 12px;
@@ -25,12 +25,16 @@ const StyledUserImg = styled.img`
 `;
 
 export const LoginIconButton = ({ anchorRef, onClick }) => {
-  const { osmUser, userImage } = useOsmAuthContext();
+  const { osmUser, loading, userImage } = useOsmAuthContext();
 
   return (
     <StyledIconButton ref={anchorRef} color="secondary" onClick={onClick}>
       {osmUser ? (
         <StyledUserImg src={userImage} alt={osmUser} />
+      ) : loading ? (
+        <div>
+          <DotLoader />
+        </div>
       ) : (
         <AccountCircleIcon />
       )}
