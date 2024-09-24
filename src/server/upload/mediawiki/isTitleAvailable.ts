b6506f7274bf6ed06a72e127ede721fa67f25fd5
Pavel Zbytovský <zbytovsky@gmail.com>
2024-09-24 16:32:30 +0200

    isTitleAvail type

--- src/server/upload/mediawiki/isTitleAvailable.ts
+++ src/server/upload/mediawiki/isTitleAvailable.ts
@@ -1,11 +1,11 @@
 import fetch from 'isomorphic-unfetch';
 import { FORMAT, WIKI_URL } from './utils';
 
-export const isTitleAvailable = async (title: string) => {
+export const isTitleAvailable = async (title: string): Promise<boolean> => {
   const response = await fetch(WIKI_URL, {
     method: 'POST',
     body: new URLSearchParams({ action: 'query', titles: title, ...FORMAT }),
   });
   const data = await response.json();
-  return data.query.pages[0].missing;
+  return Boolean(data.query.pages[0].missing);
 };
--- src/server/upload/mediawiki/run-cli.ts
+++ src/server/upload/mediawiki/run-cli.ts
@@ -1,5 +1,9 @@
 import { getMediaWikiSession } from './mediawiki';
 import { readFile } from 'node:fs/promises';
+import { isTitleAvailable } from './isTitleAvailable';
+
+// run with:
+//   npx tsx run-cli.ts
 
 (async () => {
   const password = (await readFile('../../../../.env.local', 'utf-8'))
@@ -8,4 +12,6 @@ import { readFile } from 'node:fs/promises';
 
   const session = getMediaWikiSession();
   console.log(await session.login('OsmappBot@osmapp-upload', password)); // eslint-disable-line no-console
+
+  isTitleAvailable('File:Hrana_(Climbing_crag)_-_OsmAPP.jpg').then(console.log); // eslint-disable-line no-console
 })();
