763c13e9e76a14328129ca733a3a0b7c0119f3b1
Jan Václavík <jvaclavik@gmail.com>
2024-10-01 23:47:41 +0200

    structured data: Fix structured data deletion

--- src/components/FeaturePanel/Climbing/ClimbingStructuredData.tsx
+++ src/components/FeaturePanel/Climbing/ClimbingStructuredData.tsx
@@ -1,4 +1,4 @@
-import Script from 'next/script';
+import { useEffect } from 'react';
 import { useFeatureContext } from '../../utils/FeatureContext';
 import {
   getWikimediaCommonsKeys,
@@ -13,105 +13,85 @@ import {
 } from './utils/grades/routeGrade';
 import { useUserSettingsContext } from '../../utils/UserSettingsContext';
 
-export const ClimbingStructuredData = () => {
-  const { feature } = useFeatureContext();
+const generateScriptContent = (feature, userSettings) => {
   const isClimbingArea = feature.tags.climbing === 'area';
   const isClimbingCrag = feature.tags.climbing === 'crag';
   const isClimbingRoute = feature.tags.climbing === 'route_bottom';
   const poiType = getHumanPoiType(feature);
-  const { userSettings } = useUserSettingsContext();
 
   if (isClimbingArea) {
-    return (
-      <Script
-        id="structured-data-climbing-area"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: `{
+    return `{
       "@context": "https://schema.org",
       "@type": "Place",
       "name": "${feature.tags.name} - ${poiType}",
-      ${feature.tags.description ? `"description": "${feature.tags.description}"` : ''},
+      ${feature.tags.description ? `"description": "${feature.tags.description}",` : ''}
       "geo": {
         "@type": "GeoCoordinates",
         "latitude": "${feature.center[1]}",
         "longitude": "${feature.center[0]}"
       }
-    }`,
-        }}
-      />
-    );
+    }`;
   }
 
   if (isClimbingCrag) {
     const photoPaths = getWikimediaCommonsPhotoKeys(feature.tags);
-
-    return (
-      <Script
-        id="structured-data-climbing-crag"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: `{
+    return `{
       "@context": "https://schema.org",
       "@type": "Place",
       "name": "${feature.tags.name} - ${poiType}",
-      ${
-        photoPaths.length > 0
-          ? `"image": [
-        ${photoPaths.map((photoPath) => `"${getCommonsImageUrl(feature.tags[photoPath], 800)}"`).join(', ')}
-      ],`
-          : ''
-      }
-      ${feature.tags.description ? `"description": "${feature.tags.description}"` : ''},
+      ${photoPaths.length > 0 ? `"image": [${photoPaths.map((photoPath) => `"${getCommonsImageUrl(feature.tags[photoPath], 800)}"`).join(', ')}],` : ''}
+      ${feature.tags.description ? `"description": "${feature.tags.description}",` : ''}
       "geo": {
         "@type": "GeoCoordinates",
         "latitude": "${feature.center[1]}",
         "longitude": "${feature.center[0]}"
       }
-    }`,
-        }}
-      />
-    );
+    }`;
   }
 
   if (isClimbingRoute) {
     const photoPaths = getWikimediaCommonsPhotoKeys(feature.tags);
     const selectedRouteSystem = userSettings['climbing.gradeSystem'];
     const routeDifficulties = getDifficulties(feature.tags);
-
     const { routeDifficulty } = findOrConvertRouteGrade(
       routeDifficulties,
       selectedRouteSystem,
     );
 
-    return (
-      <Script
-        id="structured-data-climbing-route"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: `{
+    return `{
       "@context": "https://schema.org",
       "@type": "Route",
       "name": "${feature.tags.name} - ${poiType}",
-      ${
-        photoPaths.length > 0
-          ? `"image": [
-        ${photoPaths.map((photoPath) => `"${getCommonsImageUrl(feature.tags[photoPath], 800)}"`).join(', ')}
-      ],`
-          : ''
-      }
-      ${feature.tags.description ? `"description": "${feature.tags.description}"` : ''},
+      ${photoPaths.length > 0 ? `"image": [${photoPaths.map((photoPath) => `"${getCommonsImageUrl(feature.tags[photoPath], 800)}"`).join(', ')}],` : ''}
+      ${feature.tags.description ? `"description": "${feature.tags.description}",` : ''}
       "difficulty": "${routeDifficulty.grade} (${getGradeSystemName(routeDifficulty.gradeSystem)})",
       "geo": {
         "@type": "GeoCoordinates",
         "latitude": "${feature.center[1]}",
         "longitude": "${feature.center[0]}"
       }
-    }`,
-        }}
-      />
-    );
+    }`;
   }
+
+  return '';
+};
+
+export const ClimbingStructuredData = () => {
+  const { feature } = useFeatureContext();
+  const { userSettings } = useUserSettingsContext();
+
+  useEffect(() => {
+    const scriptId = `structured-data-climbing-${feature.osmMeta.id}`;
+    const scriptTag = document.createElement('script');
+    scriptTag.id = scriptId;
+    scriptTag.type = 'application/ld+json';
+    scriptTag.innerHTML = generateScriptContent(feature, userSettings);
+    document.head.appendChild(scriptTag);
+
+    return () => {
+      document.getElementById(scriptId)?.remove();
+    };
+  }, [feature, userSettings]);
 
   return null;
 };
