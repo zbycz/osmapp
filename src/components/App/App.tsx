116aab512bbac8db89deb3ed6b2d328fd27b5473
Pavel Zbytovský <zbytovsky@gmail.com>
2024-10-02 13:42:38 +0200

    App: react-query client should be static

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -163,6 +163,8 @@ const IndexWithProviders = ({ climbingAreas }: IndexWithProvidersProps) => {
   );
 };
 
+const reactQueryClient = new QueryClient();
+
 type Props = {
   featureFromRouter: Feature | '404' | null;
   initialMapView: View;
@@ -177,7 +179,6 @@ const App: NextPage<Props> = ({
   climbingAreas,
 }) => {
   const mapView = getMapViewFromHash() || initialMapView;
-  const queryClient = new QueryClient();
 
   if (featureFromRouter === '404') {
     return <Error statusCode={404} />;
@@ -194,7 +195,7 @@ const App: NextPage<Props> = ({
             <OsmAuthProvider cookies={cookies}>
               <StarsProvider>
                 <EditDialogProvider /* TODO supply router.query */>
-                  <QueryClientProvider client={queryClient}>
+                  <QueryClientProvider client={reactQueryClient}>
                     <IndexWithProviders climbingAreas={climbingAreas} />
                   </QueryClientProvider>
                 </EditDialogProvider>
