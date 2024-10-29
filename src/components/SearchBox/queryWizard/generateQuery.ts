7e8a790763d1c49e5e1b197b6f07885898b93d58
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-10-29 17:59:35 +0100

    Implement the query generator

--- src/components/SearchBox/queryWizard/generateQuery.ts
+++ src/components/SearchBox/queryWizard/generateQuery.ts
@@ -1,7 +1,43 @@
-import { ASTNode } from './ast';
+import { ASTNode, ASTNodeComparison, ASTNodeExpression } from './ast';
 
-// TODO: Implement it
+const processNode = (node: ASTNode): string[][] => {
+  switch (node.type) {
+    case 'comparison':
+      return [[generateComparison(node)]];
+    case 'group':
+      return processNode(node.expression);
+    case 'expression':
+      return generateExpression(node);
+  }
+};
 
-export const generateQuery = (ast: ASTNode) => {
-  return 'relation';
+const generateComparison = ({ key, value, operator }: ASTNodeComparison) => {
+  // Using switch to simplify adding more operators in the future
+  switch (operator) {
+    case '=':
+      return `["${key}"="${value}"]`;
+  }
+};
+
+const generateExpression = (node: ASTNodeExpression): string[][] => {
+  const subExpressions = node.expressions.map(processNode);
+
+  if (node.operator === 'and') {
+    return subExpressions.reduce(
+      (acc, exprGroup) =>
+        acc.flatMap((accGroup) =>
+          exprGroup.map((expr) => [...accGroup, ...expr]),
+        ),
+      [[]],
+    );
+  }
+  if (node.operator === 'or') {
+    return subExpressions.flat();
+  }
+};
+
+export const generateQuery = (ast: ASTNode): string => {
+  return processNode(ast)
+    .map((p) => `nwr${p.join('')}`)
+    .join(';');
 };
