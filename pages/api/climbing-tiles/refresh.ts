9fa35f888746ebf5de30a668e8f1b87858c7b976
Pavel Zbytovský <zbytovsky@gmail.com>
2024-11-25 16:16:54 +0100

    maxDuration

--- pages/api/climbing-tiles/refresh.ts
+++ pages/api/climbing-tiles/refresh.ts
@@ -2,6 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { climbingTile } from '../../../src/server/climbing-tiles/climbing-tile';
 import { refresh } from '../../../src/server/climbing-tiles/refresh';
 
+export const config = {
+  maxDuration: 60 * 4, // 4 minutes
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     res.writeHead(200, {
