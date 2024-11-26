b24e33260de8b2bf67a200f3f1b101a00fe2b1ca
Pavel Zbytovský <zbytovsky@gmail.com>
2024-11-26 16:41:00 +0100

    geohashing + pg

--- pages/api/climbing-tiles/tile.ts
+++ pages/api/climbing-tiles/tile.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { climbingTile } from '../../../src/server/climbing-tiles/climbing-tile';
+import {
+  climbingTile,
+  climbingTilePg,
+} from '../../../src/server/climbing-tiles/climbing-tile';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const json = await climbingTile();
+    const json = req.query.pg ? await climbingTilePg() : await climbingTile();
 
     res.status(200).json(json);
   } catch (err) {
