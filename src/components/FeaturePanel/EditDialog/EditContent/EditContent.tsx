9668a45598d01ef7ac8afc81af4792f83ad89f9a
Pavel Zbytovský <zbytovsky@gmail.com>
2024-12-03 15:48:45 +0100

    EditDialog: relation editor UI

--- src/components/FeaturePanel/EditDialog/EditContent/EditContent.tsx
+++ src/components/FeaturePanel/EditDialog/EditContent/EditContent.tsx
@@ -7,19 +7,50 @@ import { ContributionInfoBox } from './ContributionInfoBox';
 import { OsmUserLoggedOut } from './OsmUserLoggedOut';
 import { FeatureEditSection } from './FeatureEditSection/FeatureEditSection';
 import { useEditDialogFeature } from '../utils';
+import { useEditContext } from '../EditContext';
 import { getShortId } from '../../../../services/helpers';
+import { fetchSchemaTranslations } from '../../../../services/tagging/translations';
 
 export const EditContent = () => {
   const { feature } = useEditDialogFeature();
-
-  const current = getShortId(feature.osmMeta);
+  const { items, addFeature } = useEditContext();
+  const [current, setCurrent] = React.useState(getShortId(feature.osmMeta));
 
   return (
     <>
       <DialogContent dividers>
         <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
           <OsmUserLoggedOut />
-
+          {items.map(({ shortId }, idx) => (
+            <button key={idx} onClick={() => setCurrent(shortId)}>
+              {shortId}
+            </button>
+          ))}
+          <br />
+          <button
+            onClick={async () => {
+              await fetchSchemaTranslations();
+              const osmApiTestItems = await import(
+                '../../../../services/osmApiTestItems'
+              );
+              addFeature(osmApiTestItems.TEST_CRAG);
+            }}
+          >
+            Add test crag
+          </button>
+          <button
+            onClick={async () => {
+              await fetchSchemaTranslations();
+              const osmApiTestItems = await import(
+                '../../../../services/osmApiTestItems'
+              );
+              addFeature(osmApiTestItems.TEST_NODE);
+            }}
+          >
+            Add test node
+          </button>
+          <br />
+          <br />
           <FeatureEditSection shortId={current} />
           <ContributionInfoBox />
           <CommentField />
