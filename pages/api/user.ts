1a7b1c67616568890e236eb49e5195db3de951cf
Pavel Zbytovský <zbytovsky@gmail.com>
2025-01-05 11:41:29 +0100

    upload: refactor - move osmApiAuthServer (#866)

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,11 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { serverFetchOsmUser } from '../../src/services/osmApiAuthServer';
+import { serverFetchOsmUser } from '../../src/server/osmApiAuthServer';
 
-// TODO upgrade Nextjs and use export async function POST(request: NextRequest) {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { osmAccessToken } = req.cookies;
-    const user = await serverFetchOsmUser({ osmAccessToken });
+    const user = await serverFetchOsmUser(req.cookies.osmAccessToken);
 
     res.status(200).json({ user });
   } catch (err) {
