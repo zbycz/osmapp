b2c3cfccc6a146136bb27b192090896851d6cc85
Pavel Zbytovský <zbytovsky@gmail.com>
2025-01-11 15:22:31 +0100

    MVT clientside + pg pool

---
+++ src/server/climbing-tiles/db.ts
@@ -0,0 +1,27 @@
+import { Client } from 'pg';
+
+if (!global.db) {
+  global.db = { pool: false };
+}
+
+export async function getClient() {
+  if (!global.db.pool) {
+    console.log('No pool available, creating new pool.');
+
+    const client = new Client({
+      user: 'tvgiad',
+      password: 'xau_E0h76BAWwiiGCOqEYZsRoCUQqXEQ3jpM',
+      host: 'us-east-1.sql.xata.sh',
+      port: 5432,
+      database: 'db_with_direct_access:main',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    await client.connect();
+
+    global.db.pool = client;
+  }
+  return global.db.pool;
+}
