1c1065a3f58d893038f280e400d3c66519ee0adb
Dlurak <84224239+Dlurak@users.noreply.github.com>
2025-01-24 11:28:34 +0100

    Remove transparency when a feature is opened

--- src/components/SearchBox/optionsPopper.tsx
+++ src/components/SearchBox/optionsPopper.tsx
@@ -1,11 +1,20 @@
 import styled from '@emotion/styled';
 import { Paper, PaperProps, Popper, PopperProps } from '@mui/material';
+import { useFeatureContext } from '../utils/FeatureContext';
 
-export const OptionsPaper = styled(Paper)<PaperProps>`
-  background-color: ${({ theme }) => theme.palette.background.searchInput};
+const StyledPaper = styled(Paper)<PaperProps & { $solidBg: boolean }>`
+  background-color: ${({ theme, $solidBg }) =>
+    $solidBg
+      ? theme.palette.background.searchInputSolid
+      : theme.palette.background.searchInput};
   -webkit-backdrop-filter: blur(35px);
   backdrop-filter: blur(35px);
 `;
+
+export const OptionsPaper = (props: PaperProps) => {
+  const { feature } = useFeatureContext();
+  return <StyledPaper {...props} $solidBg={!!feature} />;
+};
 
 export const OptionsPopper = styled(Popper)<PopperProps>`
   padding-top: 5px;
