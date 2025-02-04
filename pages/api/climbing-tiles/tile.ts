4836acc11e1968d594dfe778182f8f23ab6a0a64
Pavel Zbytovský <zbytovsky@gmail.com>
2025-02-04 05:50:54 +0400

    allow all CORS origin

--- pages/api/climbing-tiles/tile.ts
+++ pages/api/climbing-tiles/tile.ts
@@ -2,20 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { getClimbingTile } from '../../../src/server/climbing-tiles/getClimbingTile';
 import { Tile } from '../../../src/types';
 
-const CORS_ORIGINS = [
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'https://osmapp.org',
-];
-
 const addCorsHeaders = (req: NextApiRequest, res: NextApiResponse) => {
   const origin = req.headers.origin;
-  if (CORS_ORIGINS.includes(origin)) {
+  if (origin) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   }
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -33,7 +26,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const geojson = await getClimbingTile(tileNumber);
 
-    res.status(200).send(geojson);
+    res.status(200).setHeader('Content-Type', 'application/json').send(geojson);
   } catch (err) {
     console.error(err); // eslint-disable-line no-console
     res.status(err.code ?? 400).send(String(err));
