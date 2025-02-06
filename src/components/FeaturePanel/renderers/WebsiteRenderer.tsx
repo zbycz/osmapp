9979fce4b26ceb33f1153037da21d2355755a741
Pavel Zbytovský <zbytovsky@gmail.com>
2025-02-06 11:27:22 +0400

    FeaturePanel: display decoded link (#938)

--- src/components/FeaturePanel/renderers/WebsiteRenderer.tsx
+++ src/components/FeaturePanel/renderers/WebsiteRenderer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Language from '@mui/icons-material/Language';
 
 const protocol = /^\w+:\/\//;
-const fixHttp = (url) => (url.match(protocol) ? url : `http://${url}`);
-const displayForm = (url) =>
-  url.replace(protocol, '').replace(/([^/]+)\/$/, '$1');
+const fixHttp = (url: string) => (url.match(protocol) ? url : `http://${url}`);
+const displayForm = (url: string) =>
+  decodeURI(url.replace(protocol, '').replace(/([^/]+)\/$/, '$1'));
 
 const WebsiteRenderer = ({ v }) => (
   <>
