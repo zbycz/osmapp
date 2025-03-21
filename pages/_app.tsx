4aa0848f2360eb7f0e467fa7ba3e01e20a3ccec1
Pavel Zbytovský <zbytovsky@gmail.com>
2025-03-21 11:26:57 +0100

    vercel.json rewrites 404 -> 407

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,8 @@ import { UserThemeProvider } from '../src/helpers/theme';
 import { GlobalStyle } from '../src/helpers/GlobalStyle';
 import { doShortenerRedirect } from '../src/services/helpers';
 import { PROJECT_ID, PROJECT_NAME } from '../src/services/project';
-import { GoogleAnalytics } from './api/google-analytics';
-import { Umami } from './api/umami';
+import { GoogleAnalytics } from '../src/components/App/google-analytics';
+import { Umami } from '../src/components/App/umami';
 import { SnackbarProvider } from '../src/components/utils/SnackbarContext';
 import { UserSettingsProvider } from '../src/components/utils/UserSettingsContext';
 import {
@@ -55,87 +55,89 @@ type OwnProps = {
 
 type Props = AppProps & EmotionCacheProviderProps & OwnProps;
 
-export default class MyApp extends App<Props> {
-  componentDidMount() {
-    // setTimeout(() => {
-    //   // TODO find a way to load both pages only once (they contain same code)
-    //   //  OR maybe split the different next pages to contain just specific Panel (and move App.tsx to _app.tsx)
-    //   Router.prefetch('/'); // works only in PROD
-    //   Router.prefetch('/[osmtype]/[osmid]');
-    // }, 500);
-  }
+// export default class MyApp extends App<Props> {
+//   componentDidMount() {
+//     // setTimeout(() => {
+//     //   // TODO find a way to load both pages only once (they contain same code)
+//     //   //  OR maybe split the different next pages to contain just specific Panel (and move App.tsx to _app.tsx)
+//     //   Router.prefetch('/'); // works only in PROD
+//     //   Router.prefetch('/[osmtype]/[osmid]');
+//     // }, 500);
+//   }
+//
+//   render() {
 
-  render() {
-    const {
-      Component,
-      pageProps,
-      emotionCache,
-      userThemeCookie,
-      initialMapView,
-      cookies,
-      featureFromRouter,
-    } = this.props;
-    const mapView = getMapViewFromHash() || initialMapView;
+const MyApp = (props: Props) => {
+  const {
+    Component,
+    pageProps,
+    emotionCache,
+    userThemeCookie,
+    initialMapView = ['4', '50', '14'],
+    cookies = {},
+    featureFromRouter = null,
+  } = props;
+  const mapView = getMapViewFromHash() || initialMapView;
 
-    return (
-      <>
-        <Head>
-          <title>{PROJECT_NAME}</title>
-          <meta
-            name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1"
-          />
-        </Head>
-        <AppCacheProvider emotionCache={emotionCache}>
-          <UserThemeProvider userThemeCookie={userThemeCookie}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+  return (
+    <>
+      <Head>
+        <title>{PROJECT_NAME}</title>
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1"
+        />
+      </Head>
+      <AppCacheProvider emotionCache={emotionCache}>
+        <UserThemeProvider userThemeCookie={userThemeCookie}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
 
-            <SnackbarProvider initialToast={undefined}>
-              <UserSettingsProvider>
-                <FeatureProvider
-                  featureFromRouter={
-                    featureFromRouter === '404' ? null : featureFromRouter
-                  }
-                  cookies={cookies}
-                >
-                  <MapStateProvider initialMapView={mapView}>
-                    <OsmAuthProvider cookies={cookies}>
-                      <StarsProvider>
-                        <EditDialogProvider /* TODO supply router.query */>
-                          <QueryClientProvider client={reactQueryClient}>
-                            <Loading />
-                            <SearchBox />
-                            <ResponsiveFeaturePanel />
-                            <HomepagePanel />
-                            <Climbing />
-                            <Map />
-                            <TitleAndMetaTags />
+          <SnackbarProvider initialToast={undefined}>
+            <UserSettingsProvider>
+              <FeatureProvider
+                featureFromRouter={
+                  featureFromRouter === '404' ? null : featureFromRouter
+                }
+                cookies={cookies}
+              >
+                <MapStateProvider initialMapView={mapView}>
+                  <OsmAuthProvider cookies={cookies}>
+                    <StarsProvider>
+                      <EditDialogProvider /* TODO supply router.query */>
+                        <QueryClientProvider client={reactQueryClient}>
+                          <Loading />
+                          <SearchBox />
+                          <ResponsiveFeaturePanel />
+                          <HomepagePanel />
+                          <Climbing />
+                          <Map />
+                          <TitleAndMetaTags />
 
-                            <Component {...pageProps} />
-                          </QueryClientProvider>
-                        </EditDialogProvider>
-                      </StarsProvider>
-                    </OsmAuthProvider>
-                  </MapStateProvider>
-                </FeatureProvider>
-              </UserSettingsProvider>
-            </SnackbarProvider>
+                          <Component {...pageProps} />
+                        </QueryClientProvider>
+                      </EditDialogProvider>
+                    </StarsProvider>
+                  </OsmAuthProvider>
+                </MapStateProvider>
+              </FeatureProvider>
+            </UserSettingsProvider>
+          </SnackbarProvider>
 
-            <GlobalStyle />
-          </UserThemeProvider>
-        </AppCacheProvider>
-        {PROJECT_ID === 'openclimbing' && (
-          <>
-            <GoogleAnalytics />
-            <Umami />
-          </>
-        )}
-      </>
-    );
-  }
-}
+          <GlobalStyle />
+        </UserThemeProvider>
+      </AppCacheProvider>
+      {PROJECT_ID === 'openclimbing' && (
+        <>
+          <GoogleAnalytics />
+          <Umami />
+        </>
+      )}
+    </>
+  );
+};
+// }
 
 MyApp.getInitialProps = async ({ ctx, Component }) => {
   if (doShortenerRedirect(ctx)) {
@@ -169,3 +171,5 @@ MyApp.getInitialProps = async ({ ctx, Component }) => {
     pageProps,
   };
 };
+
+export default MyApp;
