88386bd6ae1ca43cde4bec74664394e0c6baec76
Pavel Zbytovský <zbytovsky@gmail.com>
2025-03-26 13:55:35 +0100

    add PageDrawer for standalone page

--- pages/sentry-example-page.tsx
+++ pages/sentry-example-page.tsx
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/nextjs';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
+import { MobilePageDrawer } from '../src/components/utils/MobilePageDrawer';
 
 const throwError = async () => {
   await Sentry.startSpan(
@@ -22,22 +23,24 @@ const captureMessage = () =>
   Sentry.captureMessage('Sentry Example captureMessage');
 
 const Page = () => (
-  <main style={{ textAlign: 'center' }}>
-    <h1>Sentry Test Page</h1>
-    <p>Works only in NODE_ENV=production:</p>
-    <button onClick={throwError}>Throw error!</button>
-    <button onClick={captureMessage}>Capture Message</button>
-    <br />
-    <a href="https://osmapp.sentry.io/issues/?project=1858591&query=is%3Aunresolved&statsPeriod=1h">
-      Issues
-    </a>
-    <h1>404 test</h1>
-    <Link href="/xxx">xxx</Link> <br />
-    <Link href="/node/xxx">node/xxx</Link>
-    <Head>
-      <meta name="robots" content="noindex" />
-    </Head>
-  </main>
+  <MobilePageDrawer className="sentry-example-page">
+    <main style={{ textAlign: 'center' }}>
+      <h1>Sentry Test Page</h1>
+      <p>Works only in NODE_ENV=production:</p>
+      <button onClick={throwError}>Throw error!</button>
+      <button onClick={captureMessage}>Capture Message</button>
+      <br />
+      <a href="https://osmapp.sentry.io/issues/?project=1858591&query=is%3Aunresolved&statsPeriod=1h">
+        Issues
+      </a>
+      <h1>404 test</h1>
+      <Link href="/xxx">xxx</Link> <br />
+      <Link href="/node/xxx">node/xxx</Link>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+    </main>
+  </MobilePageDrawer>
 );
 
 export default Page;
