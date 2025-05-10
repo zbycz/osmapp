335ec664fb2c831eb1048c746a0d001311daa5a2
Pavel Zbytovský <zbytovsky@gmail.com>
2025-05-10 14:19:38 +0200

    EditDialog: refactor MembersEditor - extract inner components

--- src/components/FeaturePanel/EditDialog/EditContent/FeatureEditSection/MembersEditor.tsx
+++ src/components/FeaturePanel/EditDialog/EditContent/FeatureEditSection/MembersEditor.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCurrentItem } from './CurrentContext';
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
   Chip,
-  Icon,
   List,
   Stack,
   Typography,
@@ -18,58 +17,61 @@ import { useGetHandleClick } from '../helpers';
 import { AddMemberForm } from './AddMemberForm';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 import { CragIcon } from '../../../Climbing/CragIcon';
+import { Setter } from '../../../../../types';
 
-export const MembersEditor = () => {
-  const { members, tags, nodeLonLat } = useCurrentItem();
+const SectionName = () => {
   const theme = useTheme();
-  const isClimbingCrag = tags.climbing === 'crag';
-  const [isExpanded, setIsExpanded] = React.useState(false);
-  const handleClick = useGetHandleClick({ setIsExpanded });
+  const { tags } = useCurrentItem();
 
-  const SectionName = () => {
-    const isClimbingArea = tags.climbing === 'area';
-
-    if (isClimbingArea) {
-      return (
-        <Stack direction="row" gap={1}>
-          <CragIcon
-            fill={theme.palette.text.primary}
-            stroke={theme.palette.text.primary}
-            height={24}
-            width={24}
-          />
-          <Typography variant="button">
-            {t('editdialog.climbing_crags')}{' '}
-            <Typography variant="caption" color="secondary">
-              ({t('editdialog.members')})
-            </Typography>
+  if (tags.climbing === 'area') {
+    return (
+      <Stack direction="row" gap={1}>
+        <CragIcon
+          fill={theme.palette.text.primary}
+          stroke={theme.palette.text.primary}
+          height={24}
+          width={24}
+        />
+        <Typography variant="button">
+          {t('editdialog.climbing_crags')}{' '}
+          <Typography variant="caption" color="secondary">
+            ({t('editdialog.members')})
           </Typography>
-        </Stack>
-      );
-    }
-    if (isClimbingCrag) {
-      return (
-        <Stack direction="row" gap={1}>
-          <ShowChartIcon />
-          <Typography variant="button">
-            {t('editdialog.climbing_routes')}{' '}
-            <Typography variant="caption" color="secondary">
-              ({t('editdialog.members')})
-            </Typography>
-          </Typography>
-        </Stack>
-      );
-    }
-    return <Typography variant="button">{t('editdialog.members')}</Typography>;
-  };
+        </Typography>
+      </Stack>
+    );
+  }
 
-  const AccordionComponent = ({
-    children,
-    membersLength,
-  }: {
-    children: React.ReactNode;
-    membersLength?: number;
-  }) => (
+  if (tags.climbing === 'crag') {
+    return (
+      <Stack direction="row" gap={1}>
+        <ShowChartIcon />
+        <Typography variant="button">
+          {t('editdialog.climbing_routes')}{' '}
+          <Typography variant="caption" color="secondary">
+            ({t('editdialog.members')})
+          </Typography>
+        </Typography>
+      </Stack>
+    );
+  }
+  return <Typography variant="button">{t('editdialog.members')}</Typography>;
+};
+
+const AccordionComponent = ({
+  children,
+  membersLength,
+  isExpanded,
+  setIsExpanded,
+}: {
+  children: React.ReactNode;
+  membersLength?: number;
+  isExpanded?: boolean;
+  setIsExpanded?: Setter<boolean>;
+}) => {
+  const theme = useTheme();
+
+  return (
     <Accordion disableGutters elevation={0} square expanded={isExpanded}>
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
@@ -98,6 +100,14 @@ export const MembersEditor = () => {
       </AccordionDetails>
     </Accordion>
   );
+};
+
+export const MembersEditor = () => {
+  const { members, tags, nodeLonLat } = useCurrentItem();
+  const theme = useTheme();
+  const isClimbingCrag = tags.climbing === 'crag';
+  const [isExpanded, setIsExpanded] = useState(false);
+  const handleClick = useGetHandleClick({ setIsExpanded });
 
   const selectedPresetKey = isClimbingCrag
     ? 'climbing/route_bottom'
@@ -107,7 +117,11 @@ export const MembersEditor = () => {
   if (!isClimbingCrag && hasNoMembers) return null;
 
   return (
-    <AccordionComponent membersLength={members?.length}>
+    <AccordionComponent
+      membersLength={members?.length}
+      isExpanded={isExpanded}
+      setIsExpanded={setIsExpanded}
+    >
       {members?.map((member) => {
         return (
           <FeatureRow
