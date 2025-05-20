a8e2afa1c333227a3f97425c9a56fc7e78625ed5
Jan Václavík <jvaclavik@gmail.com>
2025-05-20 13:41:22 +0200

    SEO: improve SEO 3 (#1059)

--- src/helpers/TitleAndMetaTags.tsx
+++ src/helpers/TitleAndMetaTags.tsx
@@ -6,7 +6,6 @@ import { useFeatureContext } from '../components/utils/FeatureContext';
 import { getFullOsmappLink, getShortId } from '../services/helpers';
 import { getLabel, getParentLabel } from './featureLabel';
 import {
-  PROJECT_DECRIPTION,
   PROJECT_ID,
   PROJECT_NAME,
   PROJECT_OG_IMAGE,
@@ -15,44 +14,7 @@ import {
 } from '../services/project';
 import { t } from '../services/intl';
 
-type MetaTagsProps = {
-  title: string;
-  url: string;
-  ogImage?: string;
-  description?: string;
-};
-
 const isOpenClimbing = PROJECT_ID === 'openclimbing';
-
-const MetaTags = ({ title, url, ogImage, description }: MetaTagsProps) => (
-  <>
-    {/* Open Graph (Facebook, LinkedIn, Slack) */}
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content={url} />
-    <meta property="og:title" content={title} />
-    <meta property="og:site_name" content={PROJECT_NAME} />
-    {ogImage && <meta property="og:image" content={ogImage} />}
-    <meta
-      property="og:description"
-      content={description || t(PROJECT_DECRIPTION)}
-    />
-
-    {/* SEO description */}
-    <meta
-      name="description"
-      content={description || t(PROJECT_SERP_DESCRIPTION)}
-    />
-
-    {/* Twitter Cards */}
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={title} />
-    <meta
-      name="twitter:description"
-      content={description || t(PROJECT_DECRIPTION)}
-    />
-    {ogImage && <meta name="twitter:image" content={ogImage} />}
-  </>
-);
 
 const getCustomLabel = (feature: Feature) => {
   switch (feature.tags.climbing) {
@@ -76,42 +38,61 @@ const getTitleLabel = (feature: Feature) => {
 export const TitleAndMetaTags = () => {
   const { feature } = useFeatureContext();
 
-  if (!feature) {
+  if (feature) {
+    const url = getFullOsmappLink(feature);
+    const titleLabel = getTitleLabel(feature);
+    const title = `${titleLabel} | ${PROJECT_NAME}`;
+    const description = feature.tags.description || t(PROJECT_SERP_DESCRIPTION);
+
+    const image = feature.imageDefs?.length
+      ? `${PROJECT_URL}/api/og-image?id=${getShortId(feature.osmMeta)}`
+      : undefined;
     return (
       <Head>
-        <title>
-          {PROJECT_NAME}
-          {isOpenClimbing
-            ? ` | ${t('project.openclimbing.climbing_guide')}`
-            : ''}
-        </title>
-        <MetaTags
-          title={PROJECT_NAME}
-          url={PROJECT_URL}
-          ogImage={PROJECT_OG_IMAGE}
-        />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content={PROJECT_NAME} />
+        {image && <meta property="og:image" content={image} />}
+        <meta property="og:description" content={description} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta property="twitter:url" content={url} />
+        {image && <meta name="twitter:image" content={image} />}
       </Head>
     );
   }
 
-  const osmappLink = getFullOsmappLink(feature);
-  const titleLabel = getTitleLabel(feature);
-  const title = `${titleLabel} | ${PROJECT_NAME}`;
-
-  const ogImage = feature.imageDefs?.length
-    ? `${PROJECT_URL}/api/og-image?id=${getShortId(feature.osmMeta)}`
-    : undefined;
+  const title = PROJECT_NAME;
+  const url = PROJECT_URL;
+  const image = PROJECT_OG_IMAGE;
+  const description = t(PROJECT_SERP_DESCRIPTION);
 
   return (
     <Head>
-      <title>{title}</title>
+      <title>
+        {PROJECT_NAME}
+        {isOpenClimbing ? ` | ${t('project.openclimbing.climbing_guide')}` : ''}
+      </title>
+      <meta name="description" content={description} />
 
-      <MetaTags
-        title={title}
-        description={feature.tags.description}
-        url={osmappLink}
-        ogImage={ogImage}
-      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content={PROJECT_NAME} />
+      {image && <meta property="og:image" content={image} />}
+      <meta property="og:description" content={description} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta property="twitter:url" content={url} />
+      {image && <meta name="twitter:image" content={image} />}
     </Head>
   );
 };
