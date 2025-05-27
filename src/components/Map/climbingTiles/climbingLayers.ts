4fca45ff5146d19bd8b2d1c12b4d8e6a6e61d596
Pavel Zbytovský <zbytovsky@gmail.com>
2025-05-27 14:23:07 +0200

    climbing: tweak `type=route` Lines in climbingTiles (#1079)

--- src/components/Map/climbingTiles/climbingLayers.ts
+++ src/components/Map/climbingTiles/climbingLayers.ts
@@ -103,23 +103,87 @@ const step = (
 
 export const routes: LayerSpecification[] = [
   {
+    id: 'climbing route (line) - casing',
+    metadata: { clickableWithOsmId: true },
+    type: 'line',
+    source: CLIMBING_TILES_SOURCE,
+    minzoom: 13,
+    filter: ['all', ['==', 'type', 'route']],
+    layout: {
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': '#f8f4f0',
+      'line-width': 4,
+    },
+  },
+  {
     id: 'climbing route (line)',
     metadata: { clickableWithOsmId: true },
     type: 'line',
     source: CLIMBING_TILES_SOURCE,
-    minzoom: 16,
+    minzoom: 13,
     filter: ['all', ['==', 'type', 'route']],
+    layout: {
+      'line-cap': 'round',
+    },
     paint: {
+      'line-color': '#ea5540',
       'line-width': 2,
     },
   },
+  {
+    id: 'climbing route (line) - hover',
+    metadata: { clickableWithOsmId: true },
+    type: 'line',
+    source: CLIMBING_TILES_SOURCE,
+    minzoom: 13,
+    filter: ['all', ['==', 'type', 'route']],
+    layout: {
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': '#4150a0',
+      'line-opacity': [
+        'case',
+        ['boolean', ['feature-state', 'hover'], false],
+        1,
+        0,
+      ],
+      'line-width': 2,
+    },
+  },
+  {
+    id: 'climbing route (line) - text',
+    type: 'symbol',
+    source: CLIMBING_TILES_SOURCE,
+    filter: ['all', ['==', 'type', 'route']],
+    layout: {
+      'symbol-placement': 'line-center',
+      'text-font': ['Noto Sans Regular'],
+      'text-field': '{name}',
+      'text-size': 12,
+      'text-rotation-alignment': 'map',
+    },
+    paint: {
+      'text-color': '#000000',
+      'text-halo-width': 1.5,
+      'text-halo-color': 'rgba(255,255,255,0.7)',
+      'text-opacity': [
+        'case',
+        ['boolean', ['feature-state', 'hover'], false],
+        0.5,
+        1,
+      ],
+    },
+  } as LayerSpecification,
   {
     id: 'climbing route (circle)',
     metadata: { clickableWithOsmId: true },
     type: 'circle',
     source: CLIMBING_TILES_SOURCE,
     minzoom: 16,
-    filter: ['all', ['==', 'type', 'route']],
+    filter: ['all', ['==', 'type', 'route'], ['==', '$type', 'Point']],
     paint: {
       'circle-color': [
         'case',
