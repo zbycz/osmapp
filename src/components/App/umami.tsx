0e4572bc425968ce7eed83f411024258dcfde694
Alexander Menk <a.menk@imi.de>
2025-06-02 12:10:46 +0200

    general: Move telemetry website IDs to env (Umami, GTM) as well as OSM Test server (#1077)

--- src/components/App/umami.tsx
+++ src/components/App/umami.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import Script from 'next/script';
 import { PROJECT_ID } from '../../services/project';
 
-const UMAMI_ID_OSMAPP = '8eb1792b-ba57-468b-a2cf-c8b050e11bb3';
-const UMAMI_ID_OPENCLIMBING = '85c41d25-1a2a-4168-9786-a442a92e6171';
-
 export const Umami = () => {
   const isOpenClimbing = PROJECT_ID === 'openclimbing';
 
@@ -13,7 +10,11 @@ export const Umami = () => {
     <Script
       defer
       src="https://cloud.umami.is/script.js"
-      data-website-id={isOpenClimbing ? UMAMI_ID_OPENCLIMBING : UMAMI_ID_OSMAPP}
+      data-website-id={
+        isOpenClimbing
+          ? process.env.NEXT_PUBLIC_UMAMI_ID_OPENCLIMBING
+          : process.env.NEXT_PUBLIC_UMAMI_ID_OSMAPP
+      }
     />
   );
 };
