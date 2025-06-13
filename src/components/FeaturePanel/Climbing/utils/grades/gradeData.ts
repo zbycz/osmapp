4ec4f297958caeb7b2fcfd29039eac53f2d05e65
Jan Václavík <jvaclavik@gmail.com>
2025-06-13 09:55:32 +0200

    Climbing: Add polish grading system (#1107)

--- src/components/FeaturePanel/Climbing/utils/grades/gradeData.ts
+++ src/components/FeaturePanel/Climbing/utils/grades/gradeData.ts
@@ -10,85 +10,86 @@ type GradeTable = Record<GradeSystem, Array<string>>;
 // Source of this table is: https://wiki.openstreetmap.org/wiki/Climbing#Grading
 // UIAA                        YDS                     UK ADJ                    WI
 //       French                        V Grade                 FB                       Mixed
-//               Saxon                          UK Tech                 Norwegian
+//               Saxon                          UK Tech                 Norwegian               Polish
 export const gradeTableString = `UIAA|Germany, French, Saxon|Swiss, YDS|YDS_class, V Grade, UK Tech, UK ADJ, FB|French British, Norwegian|Scandinavian, WI, Mixed
-1-,      1,      I,            5,      VB-,     1,     M,      1,       1-,       WI2,   M2
-1,       1,      I,            5,      VB-,     1,     M,      1,       1,        WI2,   M2
-1+,      1,      I,            5,      VB-,     1,     M,      1,       1+,       WI2,   M2
-2-,      2,      II,           5.1,    VB-,     2,     M/D,    1,       2-,       WI2,   M2
-2,       2,      II,           5.1,    VB-,     2,     M/D,    1,       2,        WI2,   M2
-2+,      2,      II,           5.2,    VB-,     2,     M/D,    1,       2+,       WI2,   M2
-3-,      3-,     III,          5.2,    VB-,     3,     D,      1/2,     3-,       WI2,   M2
-3,       3,      III,          5.3,    VB-,     3,     D,      1/2,     3,        WI3,   M3
-3+,      3+,     III,          5.3,    VB-,     3,     D,      1/2,     3+,       WI3,   M3
-4-,      4a,     IV,           5.4,    VB-,     4a,    D/VD,   2,       4-,       WI3,   M3
-4,       4b,     IV,           5.5,    VB-,     4a,    D/VD,   2,       4,        WI3,   M3
-4+,      4c,     V,            5.6,    VB-,     4a,    VD,     2,       4,        WI3,   M3
-5-,      4c,     V,            5.7,    VB-/VB,  4a/4b, S,      2/3,     4+,       WI3,   M3
-5,       5a,     VI,           5.8,    VB,      4b,    HS,     3,       4+,       WI4,   M4
-5+,      5a,     VI,           5.9,    VB/V0-,  4c,    HS/VS,  4a,      5-,       WI5,   M5
-6-,      5b,     VIIa,         5.10a,  V0-,     4c/5a, VS,     4a/4b,   5/5+,     WI6,   M6
-6,       5c,     VIIb,         5.10b,  V0-/V0,  5a,    HVS,    4b,      5,        WI6,   M6
-6,       5c,     VIIb,         5.10b,  V0-/V0,  5a,    HVS,    4b,      5+,       WI6,   M6
-6,       5c,     VII,          5.10b,  V0-/V0,  5a,    HVS,    4b,      5+/6-,    WI6,   M6
-6+,      5c+,    VIIc,         5.10c,  V0,      5a/5b, E1,     4c,      6-,       WI6,   M6
-6+/7-,   6a,     VIIc/VIIIa,   5.10c,  V0,      5a/5b, E1,     4c,      6-,       WI6,   M6
-7-,      6a+,    VIIIa,        5.10d,  V0+,     5b,    E1/E2,  5a,      6-/6,     WI6,   M6
-7-/7,    6a+/6b, VIIIa/VIIIb,  5.10c,  V0,      5a/5b, E1,     4c,      6,        WI6,   M6
-7,       6b,     VIIIb,        5.11a,  V1,      5b/5c, E2,     5b,      6,        WI7,   M7
-7/7+,    6b/6b+, VIIIb/VIIIc,  5.11a,  V1,      5b/5c, E2,     5b,      6,        WI7,   M7
-7+,      6b+,    VIIIc,        5.11b,  V1/V2,   5c,    E2/E3,  5c,      6/6+,     WI7,   M7
-7+/8-,   6c,     VIIIc/IXa,    5.11b,  V1/V2,   5c,    E2/E3,  5c,      6+,       WI7,   M7
-8-,      6c/6c+, VIIIc/IXa,    5.11c,  V2,      5c/6a, E3,     6a,      6+/7-,    WI8,   M8
-8-,      6c+,    IXa,          5.11c,  V2,      5c/6a, E3,     6a,      7-,       WI8,   M8
-8-/8,    6c+/7a, IXa/IXb,      5.11c,  V2,      5c/6a, E3,     6a,      7-/7,     WI8,   M8
-8,       7a,     IXb,          5.11d,  V3,      6a,    E4,     6b,      7,        WI8,   M8
-8,       7a,     IXb,          5.11d,  V3,      6a,    E4,     6b,      7/7+,     WI8,   M8
-8/8+,    7a/7a+, IXb/IXc,      5.11d,  V3,      6a,    E4,     6b,      7+,       WI8,   M8
-8/8+,    7a/7a+, IXb/IXc,      5.11d,  V3,      6a,    E4,     6b,      7+/8-,    WI8,   M8
-8+,      7a+,    IXc,          5.12a,  V3/V4,   6a,    E4/E5,  6b+,     8-,       WI8,   M8
-8+/9-,   7b,     IXc/Xa,       5.12a,  V3/V4,   6a,    E4/E5,  6b+,     8-,       WI8,   M8
-8+/9-,   7b,     IXc/Xa,       5.12a,  V3/V4,   6a,    E4/E5,  6b+,     8-/8,     WI8,   M8
-9-,      7b/7b+, IXc/Xa,       5.12b,  V4,      6b,    E5,     6c,      8,        WI8,   M8
-9-,      7b+,    Xa,           5.12b,  V4,      6b,    E5,     6c,      8,        WI8,   M8
-9-/9,    7b+/7c, Xa/Xb,        5.12b,  V4,      6b,    E5,     6c,      8,        WI8,   M8
-9,       7c,     Xb,           5.12c,  V4/V5,   6b/6c, E6,     6c+,     8/8+,     WI9,   M9
-9/9+,    7c/7c+, Xb/Xc,        5.12c,  V4/V5,   6b/6c, E6,     6c+,     8+,       WI9,   M9
-9+,      7c+,    Xc,           5.12d,  V5,      6c,    E6/E7,  7a,      8+,       WI9,   M9
-9+/10-,  8a,     Xc/XIa,       5.12d,  V5,      6c,    E6/E7,  7a,      8+/9-,    WI9,   M9
-10-,     8a/8a+, Xc/XIa,       5.13a,  V6,      6c,    E7,     7a+,     9-,       WI9,   M9
-10-,     8a+,    XIa,          5.13a,  V6,      6c,    E7,     7a+,     9-/9,     WI9,   M9
-10-/10,  8a+/8b, XIa/XIb,      5.13a,  V6,      6c,    E7,     7a+,     9,        WI9,   M9
-10,      8b,     XIb,          5.13b,  V6/V7,   6c/7a, E7/E8,  7a+/7b,  9,        WI9,   M9
-10/10+,  8a/8a+, XIb/XIc,      5.13b,  V6/V7,   6c/7a, E7/E8,  7a+/7b,  9,        WI9,   M9
-10+,     8b+,    XIc,          5.13c,  V7,      7a,    E8,     7b,      9/9+,     WI10,  M10
-10+/11-, 8b+/8c, XIc/XIIa,     5.13c,  V7,      7a,    E8,     7b,      9+,       WI10,  M10
-11-,     8c,     XIIa,         5.13d,  V8,      7a,    E9,     7b+,     9+,       WI10,  M10
-11-,     8c/8c+, XIIa,         5.13d,  V8,      7a,    E9,     7b+,     9+/10-,   WI10,  M10
-11-/11,  8c+,    XIIa/XIIb,    5.13d,  V8,      7a,    E9,     7b+,     10-,      WI10,  M10
-11-/11,  8c+/9a, XIIa/XIIb,    5.13d,  V8,      7a,    E9,     7b+,     10-,      WI10,  M10
-11,      9a,     XIIb,         5.14a,  V9,      7a/7b, E9/E10, 7c,      10-/10,   WI10,  M10
-11/11+,  9a/9a+, XIIb/XIIc,    5.14a,  V9,      7a/7b, E9/E10, 7c,      10,       WI10,  M10
-11+,     9a+,    XIIc,         5.14b,  V10,     7b,    E10,    7c+,     10,       WI10,  M10
-11+,     9a+/9b, XIIc/XIIIa,   5.14b,  V10,     7b,    E10,    7c+,     10,       WI10,  M10
-11+/12-, 9b,     XIIIa,        5.14b,  V10,     7b,    E10,    7c+,     10/10+,   WI10,  M10
-11+/12-, 9b/9b+, XIIIa/XIIIb,  5.14b,  V10,     7b,    E10,    7c+,     10+,      WI10,  M10
-12-,     9b+,    XIIIb,        5.14c,  V10/V11, 7b,    E11,    7c+/8a,  10+/11-,  WI11,  M11
-12-/12,  9b+/9c, XIIIb/XIIIc,  5.14c,  V10/V11, 7b,    E11,    7c+/8a,  11-,      WI11,  M11
-12,      9c,     XIIIc,        5.14d,  V11,     7b,    E11,    8a,      11-,      WI11,  M11
-12,      9c,     XIIIc,        5.14d,  V11,     7b,    E11,    8a,      11-/11+,  WI11,  M11
-12/12+,  9c/9c+, XIIIc,        5.14d,  V11,     7b,    E11,    8a,      11,       WI11,  M11
-12+,     9c+,    XIIIc,        5.15a,  V12,     >7b,   >E11,   8a+/8b,  11,       WI11,  M11
-12+/13-, 10a,    >XIIIc,       5.15a,  V12,     >7b,   >E11,   8a+/8b,  11/11+,   WI11,  M11
-13-,     >10a,   >XIIIc,       5.15a,  V13,     >7b,   >E11,   8b,      11+,      WI11,  M11
-13-/13,  >10a,   >XIIIc,       5.15a,  V13,     >7b,   >E11,   8b,      11+,      WI11,  M11
-13,      >10a,   >XIIIc,       5.15b,  V14,     >7b,   >E11,   8b+,     11+,      WI12,  M12
-13/13+,  >10a,   >XIIIc,       5.15b,  V14,     >7b,   >E11,   8b+,     11+/12-,  WI12,  M12
-13+,     >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12-,      WI13,  M13
-13+,     >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12-/12,   WI13,  M13
-13+/14-, >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12,       WI13,  M13
-13+/14-, >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12/12+,   WI13,  M13
-14-,     >10a,   >XIIIc,       5.15d,  V15,     >7b,   >E11,   8c+,     12+,      WI13,  M13`;
+1-,      1,      I,            5,      VB-,     1,     M,      1,       1-,       WI2,   M2,     I-
+1,       1,      I,            5,      VB-,     1,     M,      1,       1,        WI2,   M2,     I
+1+,      1,      I,            5,      VB-,     1,     M,      1,       1+,       WI2,   M2,     I+
+2-,      2,      II,           5.1,    VB-,     2,     M/D,    1,       2-,       WI2,   M2,     II-
+2,       2,      II,           5.1,    VB-,     2,     M/D,    1,       2,        WI2,   M2,     II
+2+,      2,      II,           5.2,    VB-,     2,     M/D,    1,       2+,       WI2,   M2,     II+
+3-,      3-,     III,          5.2,    VB-,     3,     D,      1/2,     3-,       WI2,   M2,     III-
+3,       3,      III,          5.3,    VB-,     3,     D,      1/2,     3,        WI3,   M3,     III
+3+,      3+,     III,          5.3,    VB-,     3,     D,      1/2,     3+,       WI3,   M3,     III+
+4-,      4a,     IV,           5.4,    VB-,     4a,    D/VD,   2,       4-,       WI3,   M3,     IV-
+4,       4b,     IV,           5.5,    VB-,     4a,    D/VD,   2,       4,        WI3,   M3,     IV
+4+,      4c,     V,            5.6,    VB-,     4a,    VD,     2,       4,        WI3,   M3,     IV+
+5-,      4c,     V,            5.7,    VB-/VB,  4a/4b, S,      2/3,     4+,       WI3,   M3,     V-
+5,       5a,     VI,           5.8,    VB,      4b,    HS,     3,       4+,       WI4,   M4,     V
+5+,      5a,     VI,           5.9,    VB/V0-,  4c,    HS/VS,  4a,      5-,       WI5,   M5,     V
+5+,      5a,     VI,           5.9,    VB/V0-,  4c,    HS/VS,  4a,      5-,       WI5,   M5,     V+
+6-,      5b,     VIIa,         5.10a,  V0-,     4c/5a, VS,     4a/4b,   5/5+,     WI6,   M6,     V+
+6,       5c,     VIIb,         5.10b,  V0-/V0,  5a,    HVS,    4b,      5,        WI6,   M6,     VI-
+6,       5c,     VIIb,         5.10b,  V0-/V0,  5a,    HVS,    4b,      5+,       WI6,   M6,     VI-
+6,       5c,     VII,          5.10b,  V0-/V0,  5a,    HVS,    4b,      5+/6-,    WI6,   M6,     VI-
+6+,      5c+,    VIIc,         5.10c,  V0,      5a/5b, E1,     4c,      6-,       WI6,   M6,     VI
+6+/7-,   6a,     VIIc/VIIIa,   5.10c,  V0,      5a/5b, E1,     4c,      6-,       WI6,   M6,     VI
+7-,      6a+,    VIIIa,        5.10d,  V0+,     5b,    E1/E2,  5a,      6-/6,     WI6,   M6,     VI+
+7-/7,    6a+/6b, VIIIa/VIIIb,  5.10c,  V0,      5a/5b, E1,     4c,      6,        WI6,   M6,     VI+
+7,       6b,     VIIIb,        5.11a,  V1,      5b/5c, E2,     5b,      6,        WI7,   M7,     VI.1
+7/7+,    6b/6b+, VIIIb/VIIIc,  5.11a,  V1,      5b/5c, E2,     5b,      6,        WI7,   M7,     VI.1+
+7+,      6b+,    VIIIc,        5.11b,  V1/V2,   5c,    E2/E3,  5c,      6/6+,     WI7,   M7,     VI.2
+7+/8-,   6c,     VIIIc/IXa,    5.11b,  V1/V2,   5c,    E2/E3,  5c,      6+,       WI7,   M7,     VI.2
+8-,      6c/6c+, VIIIc/IXa,    5.11c,  V2,      5c/6a, E3,     6a,      6+/7-,    WI8,   M8,     VI.2+
+8-,      6c+,    IXa,          5.11c,  V2,      5c/6a, E3,     6a,      7-,       WI8,   M8,     VI.2+
+8-/8,    6c+/7a, IXa/IXb,      5.11c,  V2,      5c/6a, E3,     6a,      7-/7,     WI8,   M8,     VI.2+
+8,       7a,     IXb,          5.11d,  V3,      6a,    E4,     6b,      7,        WI8,   M8,     VI.3
+8,       7a,     IXb,          5.11d,  V3,      6a,    E4,     6b,      7/7+,     WI8,   M8,     VI.3
+8/8+,    7a/7a+, IXb/IXc,      5.11d,  V3,      6a,    E4,     6b,      7+,       WI8,   M8,     VI.3+
+8/8+,    7a/7a+, IXb/IXc,      5.11d,  V3,      6a,    E4,     6b,      7+/8-,    WI8,   M8,     VI.3+
+8+,      7a+,    IXc,          5.12a,  V3/V4,   6a,    E4/E5,  6b+,     8-,       WI8,   M8,     VI.3+
+8+/9-,   7b,     IXc/Xa,       5.12a,  V3/V4,   6a,    E4/E5,  6b+,     8-,       WI8,   M8,     VI.4
+8+/9-,   7b,     IXc/Xa,       5.12a,  V3/V4,   6a,    E4/E5,  6b+,     8-/8,     WI8,   M8,     VI.4
+9-,      7b/7b+, IXc/Xa,       5.12b,  V4,      6b,    E5,     6c,      8,        WI8,   M8,     VI.4
+9-,      7b+,    Xa,           5.12b,  V4,      6b,    E5,     6c,      8,        WI8,   M8,     VI.4
+9-/9,    7b+/7c, Xa/Xb,        5.12b,  V4,      6b,    E5,     6c,      8,        WI8,   M8,     VI.4+
+9,       7c,     Xb,           5.12c,  V4/V5,   6b/6c, E6,     6c+,     8/8+,     WI9,   M9,     VI.4+
+9/9+,    7c/7c+, Xb/Xc,        5.12c,  V4/V5,   6b/6c, E6,     6c+,     8+,       WI9,   M9,     VI.5
+9+,      7c+,    Xc,           5.12d,  V5,      6c,    E6/E7,  7a,      8+,       WI9,   M9,     VI.5
+9+/10-,  8a,     Xc/XIa,       5.12d,  V5,      6c,    E6/E7,  7a,      8+/9-,    WI9,   M9,     VI.5+
+10-,     8a/8a+, Xc/XIa,       5.13a,  V6,      6c,    E7,     7a+,     9-,       WI9,   M9,     VI.5+
+10-,     8a+,    XIa,          5.13a,  V6,      6c,    E7,     7a+,     9-/9,     WI9,   M9,     VI.5+
+10-/10,  8a+/8b, XIa/XIb,      5.13a,  V6,      6c,    E7,     7a+,     9,        WI9,   M9,     VI.5+
+10,      8b,     XIb,          5.13b,  V6/V7,   6c/7a, E7/E8,  7a+/7b,  9,        WI9,   M9,     VI.6
+10/10+,  8a/8a+, XIb/XIc,      5.13b,  V6/V7,   6c/7a, E7/E8,  7a+/7b,  9,        WI9,   M9,     VI.6
+10+,     8b+,    XIc,          5.13c,  V7,      7a,    E8,     7b,      9/9+,     WI10,  M10,    VI.6+
+10+/11-, 8b+/8c, XIc/XIIa,     5.13c,  V7,      7a,    E8,     7b,      9+,       WI10,  M10,    VI.6+
+11-,     8c,     XIIa,         5.13d,  V8,      7a,    E9,     7b+,     9+,       WI10,  M10,    VI.7
+11-,     8c/8c+, XIIa,         5.13d,  V8,      7a,    E9,     7b+,     9+/10-,   WI10,  M10,    VI.7
+11-/11,  8c+,    XIIa/XIIb,    5.13d,  V8,      7a,    E9,     7b+,     10-,      WI10,  M10,    VI.7
+11-/11,  8c+/9a, XIIa/XIIb,    5.13d,  V8,      7a,    E9,     7b+,     10-,      WI10,  M10,    VI.7
+11,      9a,     XIIb,         5.14a,  V9,      7a/7b, E9/E10, 7c,      10-/10,   WI10,  M10,    VI.7+
+11/11+,  9a/9a+, XIIb/XIIc,    5.14a,  V9,      7a/7b, E9/E10, 7c,      10,       WI10,  M10,    VI.7+
+11+,     9a+,    XIIc,         5.14b,  V10,     7b,    E10,    7c+,     10,       WI10,  M10,    VI.8
+11+,     9a+/9b, XIIc/XIIIa,   5.14b,  V10,     7b,    E10,    7c+,     10,       WI10,  M10,    VI.8
+11+/12-, 9b,     XIIIa,        5.14b,  V10,     7b,    E10,    7c+,     10/10+,   WI10,  M10,    VI.8
+11+/12-, 9b/9b+, XIIIa/XIIIb,  5.14b,  V10,     7b,    E10,    7c+,     10+,      WI10,  M10,    VI.8
+12-,     9b+,    XIIIb,        5.14c,  V10/V11, 7b,    E11,    7c+/8a,  10+/11-,  WI11,  M11,    VI.8+
+12-/12,  9b+/9c, XIIIb/XIIIc,  5.14c,  V10/V11, 7b,    E11,    7c+/8a,  11-,      WI11,  M11,    VI.8+
+12,      9c,     XIIIc,        5.14d,  V11,     7b,    E11,    8a,      11-,      WI11,  M11,    VI.9
+12,      9c,     XIIIc,        5.14d,  V11,     7b,    E11,    8a,      11-/11+,  WI11,  M11,    VI.9
+12/12+,  9c/9c+, XIIIc,        5.14d,  V11,     7b,    E11,    8a,      11,       WI11,  M11,    VI.9
+12+,     9c+,    XIIIc,        5.15a,  V12,     >7b,   >E11,   8a+/8b,  11,       WI11,  M11,    VI.9+
+12+/13-, 10a,    >XIIIc,       5.15a,  V12,     >7b,   >E11,   8a+/8b,  11/11+,   WI11,  M11,    VI.9+
+13-,     >10a,   >XIIIc,       5.15a,  V13,     >7b,   >E11,   8b,      11+,      WI11,  M11,    >VI.9+
+13-/13,  >10a,   >XIIIc,       5.15a,  V13,     >7b,   >E11,   8b,      11+,      WI11,  M11,    >VI.9+
+13,      >10a,   >XIIIc,       5.15b,  V14,     >7b,   >E11,   8b+,     11+,      WI12,  M12,    >VI.9+
+13/13+,  >10a,   >XIIIc,       5.15b,  V14,     >7b,   >E11,   8b+,     11+/12-,  WI12,  M12,    >VI.9+
+13+,     >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12-,      WI13,  M13,    >VI.9+
+13+,     >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12-/12,   WI13,  M13,    >VI.9+
+13+/14-, >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12,       WI13,  M13,    >VI.9+
+13+/14-, >10a,   >XIIIc,       5.15c,  V15,     >7b,   >E11,   8c,      12/12+,   WI13,  M13,    >VI.9+
+14-,     >10a,   >XIIIc,       5.15d,  V15,     >7b,   >E11,   8c+,     12+,      WI13,  M13,    >VI.9+`;
 
 export const gradeColors = {
   '1-': { light: '#048F28', dark: '#00c132' },
