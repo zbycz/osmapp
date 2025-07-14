8bf17ce6cd0438fa9683e0d7a9cac7eb4ca28a96
glemco <32201227+glemco@users.noreply.github.com>
2025-07-14 21:33:43 +0300

    PublicTransport: Improve query routes robustness
    The query to gather routes including the clicked object is making
    assumptions, for instance:
    * the object is a node
    * the routes have the object as a "stop"
    
    This is often not the case, the object could be a way (platform as in
    https://github.com/zbycz/osmapp/issues/1145) or the route could be using
    "stop_position", "bus_stop" or "station".
    
    Extend the query to allow those scenarios.

--- src/components/FeaturePanel/PublicTransport/routes/requestRoutes.ts
+++ src/components/FeaturePanel/PublicTransport/routes/requestRoutes.ts
@@ -61,13 +61,20 @@ export async function requestLines(featureType: string, id: number) {
     // Try to find stop_area relations containing the specific node and get their stops
     (
       rel(bn.specific_feature)["public_transport"="stop_area"];
+      rel(bw.specific_feature)["public_transport"="stop_area"];
       rel(r._)["public_transport"="stop_area"] -> .stop_areas;
     ) -> .stop_areas;
-    node(r.stop_areas: "stop") -> .stops;
+    (
+        node(r.stop_areas: "stop");
+        node(r.stop_areas: "stop_position");
+        node(r.stop_areas: "station");
+        node(r.stop_areas: "bus_stop");
+    ) -> .stops;
     (
       rel(bn.stops)["route"~"bus|train|tram|subway|light_rail|ferry|monorail"];
-      // If no stop_area, find routes that directly include the specific node
+      // If no stop_area, find routes that directly include the specific node/way
       rel(bn.specific_feature)["route"~"bus|train|tram|subway|light_rail|ferry|monorail"];
+      rel(bw.specific_feature)["route"~"bus|train|tram|subway|light_rail|ferry|monorail"];
     ) -> .routes;
     // Get the master relation
     (
