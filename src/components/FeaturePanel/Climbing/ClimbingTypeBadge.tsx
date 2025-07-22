3c733832079fd068791e9c284c3a4cc3bc0a53c2
Jan Václavík <jvaclavik@gmail.com>
2025-07-22 18:59:05 +0200

    Climbing: Add climbing type badges

--- src/components/FeaturePanel/Climbing/ClimbingTypeBadge.tsx
+++ src/components/FeaturePanel/Climbing/ClimbingTypeBadge.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Feature } from '../../../services/types';
 import styled from '@emotion/styled';
-import { Chip } from '@mui/material';
+import { Chip, Stack } from '@mui/material';
 
 const StyledChip = styled(Chip)`
   font-size: 10px;
@@ -17,15 +17,35 @@ type Props = {
   feature: Feature;
 };
 
+const types = [
+  { label: 'boulder', value: 'climbing:boulder' },
+  { label: 'trad', value: 'climbing:trad' },
+  { label: 'speed', value: 'climbing:speed' },
+  { label: 'sport', value: 'climbing:sport' },
+  { label: 'multipitch', value: 'climbing:multipitch' },
+  { label: 'ice', value: 'climbing:ice' },
+  { label: 'mixed', value: 'climbing:mixed' },
+  { label: 'deepwater', value: 'climbing:deepwater' },
+  { label: 'top rope', value: 'climbing:toprope' },
+];
+
 export const ClimbingTypeBadge = ({ feature }: Props) => {
-  if (feature.tags?.['climbing:boulder'] === 'yes')
-    return (
-      <StyledChip
-        label="boulder"
-        size="small"
-        color="primary"
-        variant="filled"
-      />
-    );
-  return null;
+  return (
+    <Stack direction="row" spacing={0.5} flexWrap="wrap">
+      {types.map((type) => {
+        if (feature.tags?.[type.value] === 'yes') {
+          return (
+            <StyledChip
+              key={type.value}
+              label={type.label}
+              size="small"
+              color="primary"
+              variant="filled"
+            />
+          );
+        }
+        return null;
+      })}
+    </Stack>
+  );
 };
