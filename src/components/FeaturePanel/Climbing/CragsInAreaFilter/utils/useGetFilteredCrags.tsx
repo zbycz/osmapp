54355d12a9a6cb8d94326d62268fef54470233c3
Pavel Zbytovský <zbytovsky@gmail.com>
2025-07-28 13:32:04 +0200

    climbingFilter: simplify filterCrag

---
+++ src/components/FeaturePanel/Climbing/CragsInAreaFilter/utils/useGetFilteredCrags.tsx
@@ -0,0 +1,30 @@
+import { getGradeIndexFromTags } from '../../../../../services/tagging/climbing/routeGrade';
+import { Feature } from '../../../../../services/types';
+import { useFeatureContext } from '../../../../utils/FeatureContext';
+import { useUserSettingsContext } from '../../../../utils/userSettings/UserSettingsContext';
+
+const useGetMemberCrags = () => {
+  const { feature } = useFeatureContext();
+  return feature.memberFeatures.filter(({ tags }) => tags.climbing === 'crag');
+};
+
+export const useGetFilteredCrags = (): Feature[] => {
+  const crags = useGetMemberCrags();
+  const { climbingFilter } = useUserSettingsContext();
+  const { gradeInterval, minimumRoutesInInterval, isDefaultFilter } =
+    climbingFilter;
+  const [minIndex, maxIndex] = gradeInterval;
+
+  if (isDefaultFilter) {
+    return crags;
+  }
+
+  return crags.filter((crag) => {
+    const routes = crag.memberFeatures;
+    const filteredRoutes = routes
+      .map((route) => getGradeIndexFromTags(route.tags))
+      .filter((gradeIndex) => gradeIndex >= minIndex && gradeIndex <= maxIndex);
+
+    return filteredRoutes.length >= minimumRoutesInInterval;
+  });
+};
