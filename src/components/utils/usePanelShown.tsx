a59f8e0ef181a1cfacf19f465a4af5b0b4d8499f
Pavel Zbytovský <zbytovsky@gmail.com>
2025-08-07 13:46:46 +0200

    SearchBox: fix hiding it when directions shown (#1230)

--- src/components/utils/usePanelShown.tsx
+++ src/components/utils/usePanelShown.tsx
@@ -4,10 +4,6 @@ import { useRouter } from 'next/router';
 export const usePanelShown = () => {
   const { featureShown, homepageShown } = useFeatureContext();
   const router = useRouter();
-  if (router.asPath.startsWith('/directions')) {
-    return null;
-  }
-
   const otherPageShown = router.pathname !== '/'; // TODO there was a bug in nextjs which sometimes gave some nonsense pathname like `?nxtPall` – CHECK!
 
   return router.pathname.match(/^\/(node|way|relation)\//)
