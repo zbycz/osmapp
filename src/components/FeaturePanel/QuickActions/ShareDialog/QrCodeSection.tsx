013b7b617f1fb13ae6f29da11ca300df2bd7b7f8
Pavel Zbytovský <zbytovsky@gmail.com>
2025-08-15 11:30:08 +0200

    ShareDialog: fix logo in QR code osmapp/openclimbing (#1284)

--- src/components/FeaturePanel/QuickActions/ShareDialog/QrCodeSection.tsx
+++ src/components/FeaturePanel/QuickActions/ShareDialog/QrCodeSection.tsx
@@ -2,6 +2,14 @@ import { QRCodeSVG } from 'qrcode.react';
 import { useFeatureContext } from '../../../utils/FeatureContext';
 import { getFullOsmappLink } from '../../../../services/helpers';
 import { useTheme } from '@emotion/react';
+import { PROJECT_ID } from '../../../../services/project';
+
+const getLogoSrc = () => {
+  if (PROJECT_ID === 'openclimbing') {
+    return '/openclimbing/logo/openclimbing_64.png';
+  }
+  return '/osmapp/logo/osmapp_64.png';
+};
 
 export const QrCodeSection = () => {
   const { feature } = useFeatureContext();
@@ -16,7 +24,7 @@ export const QrCodeSection = () => {
       bgColor="transparent"
       fgColor={theme.palette.text.primary}
       imageSettings={{
-        src: 'https://openclimbing.org/openclimbing/logo/openclimbing_64.png',
+        src: getLogoSrc(),
         width: 32,
         height: 32,
         excavate: true,
