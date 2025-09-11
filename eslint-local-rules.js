3389c587e0712a42461c3fab04eff871ab187007
Pavel Zbytovský <zbytovsky@gmail.com>
2025-09-11 08:01:40 +0200

    general: local eslint rule for max-lines - only changed functions (#1369)

--- eslint-local-rules.js
+++ eslint-local-rules.js
@@ -1,5 +1,5 @@
 module.exports = {
   'no-styled-missing-transient-props': require('./src/helpers/eslint-local-rules/ruleNoStyledMissingTransientProps'),
   'no-react-prefixed-hooks': require('./src/helpers/eslint-local-rules/ruleNoReactPrefixedHooks'),
-  'max-lines-per-function-LOCAL': require('./src/helpers/eslint-local-rules/ruleMaxLinesPerFuction'),
+  'max-lines-per-function-LOCAL': require('./src/helpers/eslint-local-rules/ruleMaxLinesPerFunction'),
 };
