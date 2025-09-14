d8bb681396291fe128ee069dcd3e113496dcd034
Pavel Zbytovský <zbytovsky@gmail.com>
2025-09-14 07:09:09 +0200

    order by timestamp, ids in xata are random

--- src/server/climbing-tiles/getClimbingStats.ts
+++ src/server/climbing-tiles/getClimbingStats.ts
@@ -2,7 +2,7 @@ import { ClimbingStatsResponse } from '../../types';
 import { xataRestQuery } from './db';
 
 export const getClimbingStats = async (): Promise<ClimbingStatsResponse> => {
-  const query = `SELECT * FROM climbing_tiles_stats ORDER BY id DESC LIMIT 1`;
+  const query = `SELECT * FROM climbing_tiles_stats ORDER BY timestamp DESC LIMIT 1`;
   const result = await xataRestQuery(query);
 
   if (result.rows.length === 0) {
