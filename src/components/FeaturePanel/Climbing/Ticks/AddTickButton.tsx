53eb302e4e3fea4bfa8d2fb4036ab549e12a4344
Pavel Zbytovský <zbytovsky@gmail.com>
2025-09-16 08:51:37 +0200

    ClimbingTicks: addTickToDb + AddTickButton (#1400)

--- src/components/FeaturePanel/Climbing/Ticks/AddTickButton.tsx
+++ src/components/FeaturePanel/Climbing/Ticks/AddTickButton.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
-import { isTicked } from '../../../../services/my-ticks/ticks';
+import { useSnackbar } from '../../../utils/SnackbarContext';
+import { useOsmAuthContext } from '../../../utils/OsmAuthContext';
 import { useTicksContext } from '../../../utils/TicksContext';
 import { useFeatureContext } from '../../../utils/FeatureContext';
 import { getShortId } from '../../../../services/helpers';
 
 export const AddTickButton = () => {
-  const { addTick } = useTicksContext();
+  const { addTickToDb } = useTicksContext();
   const { feature } = useFeatureContext();
-  const shortId = getShortId(feature.osmMeta);
-  const ticked = isTicked(shortId);
+  const { loggedIn } = useOsmAuthContext();
+  const { showToast } = useSnackbar();
+  const [loading, setLoading] = useState(false);
+  const ticked = false; //TODO
+
+  const onClick = async () => {
+    if (!loggedIn) {
+      showToast('Please log in to add tick.', 'warning');
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await addTickToDb(getShortId(feature.osmMeta));
+    } catch (e) {
+      showToast(`Error: ${e.message ?? e}`, 'error');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
       <Button
-        onClick={() => addTick(shortId)}
+        onClick={onClick}
         color="secondary"
         size="small"
         variant="text"
         endIcon={<CheckIcon color={ticked ? 'primary' : undefined} />}
+        loading={loading}
       >
         Add tick
       </Button>
