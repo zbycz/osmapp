7413bfa29607a4a67ca3081c57d990540a1d8ba8
Pavel Zbytovský <zbytovsky@gmail.com>
2025-10-02 22:15:51 +0200

    FilterPopover: show warning for lower zooms (#1425)

--- src/components/FeaturePanel/Climbing/Filter/FilterPopover.tsx
+++ src/components/FeaturePanel/Climbing/Filter/FilterPopover.tsx
@@ -1,12 +1,13 @@
-import { Backdrop, Button, Stack, Box } from '@mui/material';
+import { Backdrop, Button, Stack, Box, Typography } from '@mui/material';
 import React from 'react';
-import { t } from '../../../../services/intl';
+import { t, Translation } from '../../../../services/intl';
 import { MinimumRoutesFilter } from './MinimumRoutesFilter';
 import { GradeFilter } from './GradeFilter';
 import { PopperWithArrow } from '../../../utils/PopperWithArrow';
 import { useUserSettingsContext } from '../../../utils/userSettings/UserSettingsContext';
 import { Placement } from '@popperjs/core';
 import { Setter } from '../../../../types';
+import { useMapStateContext } from '../../../utils/MapStateContext';
 
 const ResetButton = (props: { onClick: () => void }) => (
   <Button onClick={props.onClick} size="small" color="secondary">
@@ -25,6 +26,32 @@ const DoneButton = (props: { onClick: () => void }) => (
   </Button>
 );
 
+const ZoomWarning = () => {
+  // First zoom level without omitted pois due to "optimization to grid"
+  // - see climbingTileSource#updateData() loads tile level 9 for mapzoom >= 10
+  // - see getClimbingTile() to which zoom levels "isOptimizedToGrid"
+  const ZOOM_LEVEL = 10;
+
+  const [zoom, lat, lon] = useMapStateContext().view;
+  if (parseFloat(zoom) >= ZOOM_LEVEL) {
+    return null;
+  }
+  return (
+    // TODO the FilterPopover should be probably fixed to maxWidth. But maybe there is a reason.
+    <Box mx={2} pb={1} sx={{ maxWidth: '300px' }}>
+      <Typography variant="body2">
+        <Translation
+          id="crag_filter.zoom_in"
+          values={{ zoom: `${ZOOM_LEVEL}+ (~3 km)` }}
+          tags={{
+            link: 'a href="https://community.openclimbing.org/d/12-map-filtering-is-in-beta" target="_blank"',
+          }}
+        />
+      </Typography>
+    </Box>
+  );
+};
+
 type FilterPopoverProps = {
   anchorEl: null | HTMLElement;
   open: boolean;
@@ -40,12 +67,9 @@ export const FilterPopover = ({
   placement,
   offset,
 }: FilterPopoverProps) => {
-  const { climbingFilter } = useUserSettingsContext();
-  const { reset, isDefaultFilter } = climbingFilter;
+  const { reset, isDefaultFilter } = useUserSettingsContext().climbingFilter;
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const handleClose = () => setOpen(false);
 
   return (
     <Backdrop
@@ -75,6 +99,7 @@ export const FilterPopover = ({
           <Box>
             <GradeFilter />
             <MinimumRoutesFilter />
+            <ZoomWarning />
           </Box>
         </PopperWithArrow>
       </Box>
