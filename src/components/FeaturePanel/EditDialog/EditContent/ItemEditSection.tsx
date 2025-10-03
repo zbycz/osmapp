b4ff808d837a23b1489c84b1b59d7fc07d70dd3b
Pavel Zbytovský <zbytovsky@gmail.com>
2025-10-03 14:42:33 +0200

    EditDialog: refactor - MemberEditor item (#1430)

--- src/components/FeaturePanel/EditDialog/EditContent/ItemEditSection.tsx
+++ src/components/FeaturePanel/EditDialog/EditContent/ItemEditSection.tsx
@@ -6,7 +6,7 @@ import { MajorKeysEditor } from './FeatureEditSection/MajorKeysEditor';
 import { TagsEditor } from './FeatureEditSection/TagsEditor/TagsEditor';
 import { LocationEditor } from './FeatureEditSection/LocationEditor/LocationEditor';
 import { ParentsEditor } from './FeatureEditSection/ParentsEditor';
-import { MembersEditor } from './FeatureEditSection/MembersEditor';
+import { MembersEditor } from './FeatureEditSection/MembersEditor/MembersEditor';
 import React from 'react';
 import { ClimbingEditor } from './FeatureEditSection/ClimbingEditor/ClimbingEditor';
 import { Box } from '@mui/material';
