d92c7ac1287d751079c08849ddd9d57286e08978
Jan Václavík <jvaclavik@gmail.com>
2026-02-19 21:52:09 +0100

    overpass: add more hosts + retry if busy (#1460)
    Co-authored-by: Pavel Zbytovský <zbytovsky@gmail.com>

--- src/services/overpass/fetchOverpass.ts
+++ src/services/overpass/fetchOverpass.ts
@@ -1,10 +1,49 @@
 import { fetchJson } from '../fetch';
+import { FetchError } from '../helpers';
 
 // TODO add proper overpass types from refreshClimbingTilesHelpers.ts
 
-export const fetchOverpass = async (fullQuery: string) =>
-  await fetchJson(
-    `https://overpass-api.de/api/interpreter?data=${encodeURIComponent(
-      fullQuery,
-    )}`,
+const OVERPASS_HOSTS = [
+  'overpass.private.coffee', // alternative instance (minutely synced), we prefer it for bigger HW & less known
+  'overpass-api.de', // main instance (minutely synced)
+  'maps.mail.ru/osm/tools/overpass', // last alternative (minutely synced)
+];
+
+// Overpass lately experiences a lot of wierd issues ~ February 2026, see https://community.openstreetmap.org/t/overpass-api-performance-issues/140598
+const isRetryableError = (e: FetchError) => {
+  return (
+    (e instanceof Error && e.message.includes('fetchJson: parse error')) ||
+    (e instanceof FetchError &&
+      (e.code === '429' ||
+        e.code === '500' ||
+        e.code === '502' ||
+        e.code === '503' ||
+        e.code === '504'))
   );
+};
+
+export const fetchOverpass = async (query: string) => {
+  const LAST_INDEX = OVERPASS_HOSTS.length - 1;
+
+  for (let i = 0; i < OVERPASS_HOSTS.length; i++) {
+    const host = OVERPASS_HOSTS[i];
+
+    try {
+      const url = `https://${host}/api/interpreter?data=${encodeURIComponent(query)}`;
+      const response = await fetchJson(url);
+
+      return response;
+    } catch (e) {
+      if (i === LAST_INDEX) {
+        throw e;
+      }
+
+      if (isRetryableError(e)) {
+        console.info(`Overpass ${host} failed, will try next host.`, e); // eslint-disable-line no-console
+        continue;
+      }
+
+      throw e;
+    }
+  }
+};
