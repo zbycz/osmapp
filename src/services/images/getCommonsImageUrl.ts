63f7808d00a10f8b93faa2dac591c1e231090f6f
Pavel Zbytovský <zbytovsky@gmail.com>
2026-02-21 22:34:32 +0100

    images: getCommonsImageUrl() - constrain width with types

--- src/services/images/getCommonsImageUrl.ts
+++ src/services/images/getCommonsImageUrl.ts
@@ -1,21 +1,44 @@
 import { md5 } from 'js-md5';
 import { encodeUrl } from '../../helpers/utils';
 
+// requests for other widths works, but may be blocked for some users #1463
+// from https://www.mediawiki.org/wiki/Common_thumbnail_sizes
+export type CommonsAllowedWidth =
+  | 20
+  | 40
+  | 60
+  | 120
+  | 250
+  | 330
+  | 500
+  | 960
+  | 1280
+  | 1920
+  | 3840;
+
 export const getCommonsImageUrl = (
   photoName: string,
-  width: number,
+  width: CommonsAllowedWidth | 'original',
 ): string | null => {
-  if (!photoName) return null;
-  if (!photoName.startsWith('File:')) {
-    // eslint-disable-next-line no-console
-    console.warn('Invalid Commons photo name without "File:":', photoName);
+  if (!photoName) {
     return null;
   }
+
+  if (!photoName.startsWith('File:')) {
+    console.warn('Invalid Commons photo name without "File:":', photoName); // eslint-disable-line no-console
+    return null;
+  }
+
   const fileName = decodeURI(photoName)
     .replace(/^.*File:/, '')
     .replace(/ /g, '_');
   const hash = md5(fileName);
   const part1 = hash[0];
   const part2 = hash.substring(0, 2);
+
+  if (width === 'original') {
+    return encodeUrl`https://upload.wikimedia.org/wikipedia/commons/${part1}/${part2}/${fileName}`;
+  }
+
   return encodeUrl`https://upload.wikimedia.org/wikipedia/commons/thumb/${part1}/${part2}/${fileName}/${width}px-${fileName}`;
 };
