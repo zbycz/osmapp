b4a24348a2f85a781d36457cc32fdcc42a502c70
Pavel Zbytovský <zbytovsky@gmail.com>
2026-02-21 16:44:21 +0100

    Revert "fix: Fix allowed wikimedia thumb resolutions (#1463)"
    This reverts commit 4311993ee377b8d08424f10081ca3a2265c33e48.

--- src/services/images/getCommonsImageUrl.ts
+++ src/services/images/getCommonsImageUrl.ts
@@ -1,18 +1,6 @@
 import { md5 } from 'js-md5';
 import { encodeUrl } from '../../helpers/utils';
 
-/** Standard Wikimedia Commons thumbnail widths (https://www.mediawiki.org/wiki/Common_thumbnail_sizes) */
-const ALLOWED_WIDTHS = [120, 150, 180, 200, 220, 250, 300, 400] as const;
-
-const MAX_ALLOWED_WIDTH = 400;
-
-/** Returns smallest allowed width >= requested, or null if full resolution needed (> 400px) */
-export const roundUpToAllowedWidth = (width: number): number | null => {
-  if (width > MAX_ALLOWED_WIDTH) return null;
-  const match = ALLOWED_WIDTHS.find((w) => w >= width);
-  return match ?? null;
-};
-
 export const getCommonsImageUrl = (
   photoName: string,
   width: number,
@@ -29,12 +17,5 @@ export const getCommonsImageUrl = (
   const hash = md5(fileName);
   const part1 = hash[0];
   const part2 = hash.substring(0, 2);
-
-  const thumbnailWidth = roundUpToAllowedWidth(width);
-
-  if (thumbnailWidth === null) {
-    return encodeUrl`https://upload.wikimedia.org/wikipedia/commons/${part1}/${part2}/${fileName}`;
-  }
-
-  return encodeUrl`https://upload.wikimedia.org/wikipedia/commons/thumb/${part1}/${part2}/${fileName}/${thumbnailWidth}px-${fileName}`;
+  return encodeUrl`https://upload.wikimedia.org/wikipedia/commons/thumb/${part1}/${part2}/${fileName}/${width}px-${fileName}`;
 };
