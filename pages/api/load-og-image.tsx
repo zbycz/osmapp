611ea32ffa45c3a4437dfb2350ab2f5ad04ee917
Pavel Zbytovský <zbytovsky@gmail.com>
2026-02-25 13:23:12 +0100

    images: fix html escaped og-image (#1465)

--- pages/api/load-og-image.tsx
+++ pages/api/load-og-image.tsx
@@ -1,11 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { PROJECT_NAME, PROJECT_URL } from '../../src/services/project';
+import { PROJECT_URL } from '../../src/services/project';
 
 // Abort after approximately 200kb
 const READ_LIMIT = 200_000;
 
-const regex1 = /<meta\s+property="og:image"\s+content="([^"]+)"/;
-const regex2 = /<meta\s+content="([^"]+)"\s+property="og:image"/;
+const decodeHtmlEntities = (str) =>
+  str.replace(/&#x([0-9a-fA-F]+);/g, (_match, hex) =>
+    String.fromCharCode(parseInt(hex, 16)),
+  );
+
+// TODO use proper (minimal) HTML parser, this doesn't handle all combinations (apostrophes, etc)
+const regex1 = /<meta\s+property="og:image"\s+content="([^"]+)"/i;
+const regex2 = /<meta\s+content="([^"]+)"\s+property="og:image"/i;
 
 const loadOgImage = async (url: string): Promise<string | null> => {
   const abortController = new AbortController();
@@ -14,24 +20,21 @@ const loadOgImage = async (url: string): Promise<string | null> => {
     signal: abortController.signal,
     headers: {
       Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'User-Agent': `${PROJECT_NAME} (+${PROJECT_URL})`,
+      'User-Agent': `OsmAPP ${process.env.osmappVersion} (+${PROJECT_URL}; load-og-image.tsx)`,
     },
   });
 
   const reader = response.body.getReader();
-
   const decoder = new TextDecoder('utf-8');
   let buffer = '';
-
   while (true) {
     const { value, done } = await reader.read();
-
     buffer += decoder.decode(value, { stream: true });
 
     const match = buffer.match(regex1) ?? buffer.match(regex2);
     if (match) {
       abortController.abort();
-      return match[1];
+      return decodeHtmlEntities(match[1]);
     }
 
     if (done || buffer.length > READ_LIMIT) {
