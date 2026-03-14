574fa2bd4f6da94f2194009e9b454fd366345a12
Pavel Zbytovský <zbytovsky@gmail.com>
2026-03-14 12:19:00 +0100

    ShareDialog: id editor link with maxzoom 16

--- src/components/FeaturePanel/helpers/externalLinks.ts
+++ src/components/FeaturePanel/helpers/externalLinks.ts
@@ -4,10 +4,13 @@ import { View } from '../../utils/MapStateContext';
 import { OSM_WEBSITE } from '../../../services/osm/consts';
 
 export const getIdEditorLink = (feature: Feature, view?: View) => {
+  const [z, lon, lat] = view.map(parseFloat);
+  const constrainedView = [z > 16 ? z : 16, lon, lat];
+
   const query = feature?.osmMeta?.id
     ? `?${feature.osmMeta.type}=${feature.osmMeta.id}`
     : '';
-  const hash = view ? `#map=${view.join('/')}` : '';
+  const hash = constrainedView ? `#map=${constrainedView.join('/')}` : '';
   return `${OSM_WEBSITE}/edit${query}${hash}`;
 };
 
