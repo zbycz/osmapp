bd61d0b47efe66358eadabc0da55d7c9c6d7b465
Pavel Zbytovský <zbytovsky@gmail.com>
2026-06-08 20:23:31 +0200

    Revert "EditDialog: fix deeplink in hacky-static-export (#1492)"
    This reverts commit 2ba6bb4743974190546fa4694ba1e4494b046e89.

--- src/components/FeaturePanel/helpers/EditDialogContext.tsx
+++ src/components/FeaturePanel/helpers/EditDialogContext.tsx
@@ -17,33 +17,9 @@ type EditDialogType = {
 
 const EditDialogContext = createContext<EditDialogType>(undefined);
 
-const EDIT_DEEPLINK_REGEX =
-  /^\/(?:[a-z]{2}\/)?(?:node|way|relation)\/\d+\/edit\/?$/i;
-
-const isEditDeeplink = () => {
-  if (Router.query.all?.[2] === 'edit') {
-    return true;
-  }
-  // hacky static export:
-  return EDIT_DEEPLINK_REGEX.test(window.location.pathname);
-};
-
-const removeEditDeeplink = () => {
-  if (!isBrowser()) {
-    return;
-  }
-  const { pathname, search, hash } = window.location;
-  if (EDIT_DEEPLINK_REGEX.test(pathname)) {
-    const cleanPathname = pathname.replace(/\/edit\/?$/, '');
-    Router.replace(`${cleanPathname}${search}${hash}`, undefined, {
-      shallow: true,
-    });
-  }
-};
-
 // lives in App.tsx because the context is needed in SSR
 export const EditDialogProvider = ({ children }) => {
-  const initialState = isEditDeeplink();
+  const initialState = isBrowser() ? Router.query.all?.[2] === 'edit' : false;
   const [opened, setOpened] = useState<boolean | Tag>(initialState);
   const [redirectOnClose, setRedirectOnClose] = useState<string | undefined>();
 
@@ -52,8 +28,6 @@ export const EditDialogProvider = ({ children }) => {
       Router.replace('/').then(() => {
         Router.replace(redirectOnClose); // to reload the panel, we need two redirects, see https://github.com/zbycz/osmapp/pull/685
       });
-    } else {
-      removeEditDeeplink();
     }
     setOpened(false);
     setRedirectOnClose(undefined);
